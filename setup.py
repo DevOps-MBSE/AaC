a051758f301316e5c5340363d98443c775be784a
Alex Monnet <alexcmonnet@gmail.com>
2021-10-08 21:22:12 +0000

    formatting changes

--- setup.py
+++ setup.py
@@ -1,45 +1,43 @@
 from setuptools import setup, find_packages
 
 runtime_dependencies = [
-    'attrs == 21.2.0',
-    'coverage == 6.0',
-    'jsonschema == 4.0.1',
-    'pyrsistent == 0.18.0',
-    'PyYAML == 5.4.1',
-    'six == 1.16.0',
-    'click == 8.0.1',
-    'pathspec == 0.9.0',
-    'platformdirs == 2.4.0',
-    'pyrsistent == 0.18.0',
-    'regex == 2021.9.30',
-    'typing-extensions == 3.10.0.2',
+    "attrs == 21.2.0",
+    "coverage == 6.0",
+    "jsonschema == 4.0.1",
+    "pyrsistent == 0.18.0",
+    "PyYAML == 5.4.1",
+    "six == 1.16.0",
+    "click == 8.0.1",
+    "pathspec == 0.9.0",
+    "platformdirs == 2.4.0",
+    "pyrsistent == 0.18.0",
+    "regex == 2021.9.30",
+    "typing-extensions == 3.10.0.2",
+    "pluggy == 1.0.0",
 ]
 
 development_dependencies = [
-    'flake8 == 3.9.2',
-    'black == 21.9b0',
-    'mccabe == 0.6.1',
-    'mypy-extensions == 0.4.3',
-    'pycodestyle == 2.7.0',
-    'pyflakes == 2.3.1',
+    "flake8 == 3.9.2",
+    "black == 21.9b0",
+    "mccabe == 0.6.1",
+    "mypy-extensions == 0.4.3",
+    "pycodestyle == 2.7.0",
+    "pyflakes == 2.3.1",
 ]
 
-test_dependencies = [
-    'nose2 == 0.10.0',
-    'coverage == 6.0'
-]
+test_dependencies = ["nose2 == 0.10.0", "coverage == 6.0"]
 
 setup(
-    name = 'Jellyfish',
-    version = '3.0.0',
-    packages = find_packages(where="jellyfish", exclude="tests"),
-    package_dir = {"": "jellyfish"},
-    install_requires = runtime_dependencies,
-    setup_requires = development_dependencies,
-    tests_require = test_dependencies,
-    extras_require = {
-        'test' : test_dependencies,
-        'dev' : development_dependencies,
-        'all' : test_dependencies + development_dependencies
-    }
+    name="jellyfish",
+    version="3.0.0",
+    packages=find_packages(where="jellyfish", exclude="tests"),
+    package_dir={"": "jellyfish"},
+    install_requires=runtime_dependencies,
+    setup_requires=development_dependencies,
+    tests_require=test_dependencies,
+    extras_require={
+        "test": test_dependencies,
+        "dev": development_dependencies,
+        "all": test_dependencies + development_dependencies,
+    },
 )
