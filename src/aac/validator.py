3280d90bf559ee0ef9ede205d4cd29dcdc5235db
Cameron Chaparro <cameron.chaparro@zadentech.com>
2021-10-15 00:33:45 -0500

    Add tests and validate data (partially)
    We still aren't validating required parameters.

--- src/aac/validator.py
+++ src/aac/validator.py
@@ -1,3 +1,5 @@
+from iteration_utilities import flatten
+
 from aac import util
 
 
@@ -15,19 +17,77 @@ def get_all_errors(model: dict) -> list:
     :return: Return a list of all the validation errors found for MODEL. If the MODEL is valid,
     return an empty list.
     """
-    errors = []
+    return get_all_enum_errors(model) + get_all_data_errors(model)
+
+
+def get_all_enum_errors(model: dict) -> list:
+    """Return all validation errors for the enumeration MODEL.
+
+    :return: Return a list of all the validation errors found for the enumeration MODEL. If the
+    enumeration MODEL is valid, return an empty list.
+    """
+    if not is_enum_model(model):
+        return []
+
     enum = model["enum"]
+    required_properties = ["name", "values"]
+    property_types = [str, list]
 
-    if "name" not in enum.keys():
-        errors.append('missing required field property: "name"')
+    property_errors = map(
+        lambda p: get_error_if_missing_required_property(enum, p), required_properties
+    )
+    type_errors = map(
+        lambda p, t: get_error_if_property_has_wrong_type(enum, p, t),
+        required_properties,
+        property_types,
+    )
 
-    if "values" not in enum.keys():
-        errors.append('missing required field property: "values"')
+    return filter_out_empty_strings(property_errors, type_errors)
 
-    if "name" in enum.keys() and not isinstance(enum["name"], str):
-        errors.append('wrong type for field property: "values"')
 
-    if "values" in enum.keys() and not isinstance(enum["values"], list):
-        errors.append('wrong type for field property: "name"')
+def is_enum_model(model: dict) -> bool:
+    """Determine if the MODEL represents an enumeration model."""
+    return "enum" in model
 
-    return errors
+
+def get_error_if_missing_required_property(model: dict, key: str) -> str:
+    """Return an error message if the model is missing the specifed property."""
+    if key not in model.keys():
+        return 'missing required field property: "{}"'.format(key)
+    return ""
+
+
+def get_error_if_property_has_wrong_type(model: dict, key: str, instance: type) -> str:
+    if key in model.keys() and not isinstance(model[key], instance):
+        return 'wrong type for field property: "{}"'.format(key)
+    return ""
+
+
+def filter_out_empty_strings(*xs: list) -> list:
+    """Return XS with all empty strings removed."""
+    return list(filter(lambda x: x != "", flatten(xs)))
+
+
+def get_all_data_errors(model: dict) -> list:
+    if not is_data_model(model):
+        return []
+
+    data = model["data"]
+    required_properties = ["name", "fields"]
+    property_types = [str, list]
+
+    property_errors = map(
+        lambda p: get_error_if_missing_required_property(data, p), required_properties
+    )
+    type_errors = map(
+        lambda p, t: get_error_if_property_has_wrong_type(data, p, t),
+        required_properties + ["required"],
+        property_types + [list],
+    )
+
+    return filter_out_empty_strings(property_errors, type_errors)
+
+
+def is_data_model(model: dict) -> bool:
+    """Determine if the MODEL represents a data model."""
+    return "data" in model
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -24,7 +24,6 @@ def assert_no_errors(errors):
 
 def assert_errors_contain(errors, pattern):
     """Assert that at least one error in ERRORS matches PATTERN."""
-    print(errors)
     for e in errors:
         assert re.search(pattern, e)
 
@@ -62,4 +61,43 @@ class ValidatorTest(TestCase):
         assert_model_is_invalid(enum(name="test"), ".*missing.*required.*values.*")
         assert_model_is_invalid(enum(values=[]), ".*missing.*required.*name.*")
         assert_model_is_invalid(enum(name=1, values=2), ".*wrong.*type.*(name|values).*")
+
+    def test_can_validate_data(self):
+        def field(name=None, type=None):
+            field = {}
+            if name is not None:
+                field["name"] = name
+            if type is not None:
+                field["type"] = type
+            return field
+
+        def data(name=None, fields=None, required=None):
+            data = {}
+            if name is not None:
+                data["name"] = name
+            if fields is not None:
+                data["fields"] = fields
+            if required is not None:
+                data["required"] = required
+            return {"data": data}
+
+        one_field = [field(name="x", type="int")]
+        two_fields = one_field + [field(name="y", type="int")]
+
+        assert_model_is_valid(data(name="test", fields=[]))
+        assert_model_is_valid(data(name="test", fields=one_field))
+        assert_model_is_valid(data(name="test", fields=two_fields))
+
+        assert_model_is_valid(data(name="test", fields=[], required=[]))
+        assert_model_is_valid(data(name="test", fields=one_field, required=["x"]))
+        assert_model_is_valid(data(name="test", fields=two_fields, required=["x"]))
+        assert_model_is_valid(data(name="test", fields=two_fields, required=["x", "y"]))
+
+        assert_model_is_invalid(data(), ".*missing.*required.*(name|fields).*")
+        assert_model_is_invalid(data(name="test"), ".*missing.*required.*fields.*")
+        assert_model_is_invalid(data(fields=[]), ".*missing.*required.*name.*")
+
+        assert_model_is_invalid(data(name=1, fields=2), ".*wrong.*type.*(name|fields).*")
+        assert_model_is_invalid(
+            data(name=1, fields=2, required=3), ".*wrong.*type.*(name|fields|required).*"
         )
