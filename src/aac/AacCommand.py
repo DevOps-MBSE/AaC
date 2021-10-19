949c7a9b465e470fbfd20a193917f3be71fc0f88
Alex Monnet <alexcmonnet@gmail.com>
2021-10-19 21:38:07 +0000

    Made AacCommandArguments optional

--- src/aac/AacCommand.py
+++ src/aac/AacCommand.py
@@ -5,24 +5,6 @@ import attr
 
 
 @attr.s
-class AacCommand:
-    """
-    A class used as a struct to hold a command name, command description, and callback.
-    The command name is added to the argument parser with help set to teh command description.
-    The callback is invoked if the user specifies the command.
-
-    Attributes:
-        command_name: A string with the name of the command_description
-        command_description: a string with the command description -- will be provided with the help command
-        callback: A Callable function that's executed when the user runs the AaC command
-    """
-
-    command_name = attr.ib()
-    command_description = attr.ib()
-    callback = attr.ib()
-
-
-@attr.s
 class AacCommandArgument:
     """
     A class used as a struct to hold a command argument name and description.
@@ -35,3 +17,23 @@ class AacCommandArgument:
 
     name = attr.ib()
     description = attr.ib()
+
+
+@attr.s
+class AacCommand:
+    """
+    A class used as a struct to hold a command name, command description, and callback.
+    The command name is added to the argument parser with help set to teh command description.
+    The callback is invoked if the user specifies the command.
+
+    Attributes:
+        command_name: A string with the name of the command_description
+        command_description: a string with the command description -- will be provided with the help command
+        callback: A Callable function that's executed when the user runs the AaC command
+        arguments: A List of AacCommandArgument containing argument information about the command
+    """
+
+    command_name = attr.ib()
+    command_description = attr.ib()
+    callback = attr.ib()
+    arguments = attr.ib(default=[])
