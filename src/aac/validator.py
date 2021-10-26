c59fe477441bdca40117b387d21f62bc4f99caa2
Cameron Chaparro <cameron.chaparro@zadentech.com>
2021-10-26 08:45:54 -0500

    Rename _is_valid_type -> _is_defined_type

--- src/aac/validator.py
+++ src/aac/validator.py
@@ -195,10 +195,10 @@ def _set_valid_types(model: dict) -> None:
 
 def _get_error_messages_if_invalid_type(name: str, types: list) -> list:
     """Get a list of error messages if any types are unrecognized."""
-    return [f"unrecognized type {t} used in {name}" for t in types if not _is_valid_type(t)]
+    return [f"unrecognized type {t} used in {name}" for t in types if not _is_defined_type(t)]
 
 
-def _is_valid_type(type: str) -> bool:
+def _is_defined_type(type: str) -> bool:
     """Determine whether the type is valid, or not."""
     return type.strip("[]") in DEFINED_TYPES + REFERENCED_TYPES_IN_MODEL
 
