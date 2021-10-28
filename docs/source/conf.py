7afea3c2529da5c4503b60e741d57277681f504e
Cameron Chaparro <cameron.chaparro@zadentech.com>
2021-10-27 20:19:45 -0500

    Update author for API docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath("."))
 
 project = "Architecture-as-Code"
 copyright = "2021, JD Black, Alex Monnet, Cameron Chaparro"
-author = "JD Black, Alex Monnet, Cameron Chaparro"
+author = "Northrop Grumman Corporation"
 
 # The full version, including alpha/beta/rc tags
 release = "0.0.4"
