857a5048b709792ccf4e435183e5c24a2054eed5
Cameron Chaparro <cameron.chaparro@zadentech.com>
2021-10-31 16:40:00 -0500

    Define the project root directory

--- src/aac/__init__.py
+++ src/aac/__init__.py
@@ -1,11 +1,14 @@
+from os.path import abspath
+
 from pluggy import HookimplMarker, HookspecMarker
 
 PLUGIN_PROJECT_NAME = "aac"
+PROJECT_ROOT_DIR = abspath("../../")
 
 hookimpl = HookimplMarker(PLUGIN_PROJECT_NAME)
 hookspec = HookspecMarker(PLUGIN_PROJECT_NAME)
 
-__all__ = ("run_cli", "hookimpl", "hookspec")
+__all__ = ("run_cli", "hookimpl", "hookspec", "PROJECT_ROOT_DIR")
 
 # note:  must come last to avoid circular import
 from aac.cli import run_cli
