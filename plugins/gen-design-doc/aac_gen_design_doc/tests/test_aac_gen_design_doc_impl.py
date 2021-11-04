f1b1a71ec4adbfce7fb6d72d7f38a7f3b07ec533
Cameron Chaparro <cameron.chaparro@zadentech.com>
2021-11-04 18:52:29 -0500

    Move tests to the right directory

--- plugins/gen-design-doc/aac_gen_design_doc/tests/test_aac_gen_design_doc_impl.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from aac_gen_design_doc.aac_gen_design_doc_impl import gen_design_doc
-
-
-class TestGenerateDesignDocumentPlugin(TestCase):
-    def test_can_generate_design_doc_with_models():
-        return
