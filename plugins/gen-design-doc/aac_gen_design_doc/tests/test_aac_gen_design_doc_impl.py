637686f354414423cad1e0b0c95ac6837470db60
Cameron Chaparro <cameron.chaparro@zadentech.com>
2021-11-04 13:01:27 -0500

    Update tests

--- plugins/gen-design-doc/aac_gen_design_doc/tests/test_aac_gen_design_doc_impl.py
+++ plugins/gen-design-doc/aac_gen_design_doc/tests/test_aac_gen_design_doc_impl.py
@@ -1,9 +1,8 @@
 from unittest import TestCase
-from nose2.tools import params
 
-from aac_gen_design_doc.aac_gen_design_doc_impl import *
+from aac_gen_design_doc.aac_gen_design_doc_impl import gen_design_doc
 
 
-class TestGenerateProtobufPlugin(TestCase):
-    def test_example():
+class TestGenerateDesignDocumentPlugin(TestCase):
+    def test_can_generate_design_doc_with_models():
         return
