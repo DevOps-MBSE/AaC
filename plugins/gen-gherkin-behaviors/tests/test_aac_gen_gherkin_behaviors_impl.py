7742fff78fffacd333d28726640bb835c1b81705
Alex Monnet <alexcmonnet@gmail.com>
2021-11-10 21:33:59 +0000

    Fixed directory structure

---
+++ plugins/gen-gherkin-behaviors/tests/test_aac_gen_gherkin_behaviors_impl.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from aac_gen_gherkin_behavior.aac_gen_gherkin_behaviors_impl import gen_gherkin_behaviors
+
+
+class TestGenerateGherkinBehaviorsPlugin(TestCase):
+
+    def test(self):
+        self.assertTrue(True)
