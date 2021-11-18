08deecf0ba9e7fa393f18eef9607f220f02c45aa
Alex Monnet <alexcmonnet@gmail.com>
2021-11-18 03:58:56 +0000

    Added tests for each validation case.

--- plugins/aac-spec/tests/test_aac_spec_impl.py
+++ plugins/aac-spec/tests/test_aac_spec_impl.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from tempfile import NamedTemporaryFile
 
-from aac_spec.aac_spec_impl import _do_validate, spec_validate, AacSpecValidationException
+from aac_spec.aac_spec_impl import spec_validate, AacSpecValidationException
 
 
 class TestAacSpec(TestCase):
@@ -17,7 +17,7 @@ class TestAacSpec(TestCase):
             except AacSpecValidationException as e:
                 self.fail("validate() raised AacSpecValidationException with error: {0}".format(e))
 
-    def test_spec_validate_fails(self):
+    def test_spec_validate_fails_with_missing_abbrv(self):
 
         with NamedTemporaryFile("w") as temp_spec:
             temp_spec.write(INVALID_SPEC_MISSING_ABRV)
@@ -25,6 +25,33 @@ class TestAacSpec(TestCase):
 
             with self.assertRaises(AacSpecValidationException) as context_manager:
                 spec_validate(temp_spec.name)
+
+                validation_exception = context_manager.exception
+                self.assertIn("Spec name Subsystem must have 1 abbrv", validation_exception.message)
+
+    def test_spec_validate_fails_with_bad_id_ref(self):
+
+        with NamedTemporaryFile("w") as temp_spec:
+            temp_spec.write(INVALID_SPEC_BAD_ID_REFERENCE)
+            temp_spec.seek(0)
+
+            with self.assertRaises(AacSpecValidationException) as context_manager:
+                spec_validate(temp_spec.name)
+
+                validation_exception = context_manager.exception
+                self.assertIn("Invalid requirement id 3 reference in", validation_exception.message)
+
+    def test_spec_validate_fails_with_bad_abbrv_ref(self):
+
+        with NamedTemporaryFile("w") as temp_spec:
+            temp_spec.write(INVALID_SPEC_BAD_ID_REFERENCE)
+            temp_spec.seek(0)
+
+            with self.assertRaises(AacSpecValidationException) as context_manager:
+                spec_validate(temp_spec.name)
+
+                validation_exception = context_manager.exception
+                self.assertIn("Invalid requirement abbreviation NOTSUB reference in", validation_exception.message)
 
 
 VALID_SPEC = """
@@ -81,7 +108,7 @@ spec:
           value: Test
 """
 
-INVALID_SPEC_2 = """
+INVALID_SPEC_BAD_ID_REFERENCE = """
 spec:
   name: Subsystem
   abbrv: SUB
@@ -108,7 +135,7 @@ spec:
           value: Test
 """
 
-VALID_MODEL = """
+INVALID_SPEC_BAD_ABBRV_VALUE = """
 spec:
   name: Subsystem
   abbrv: SUB
@@ -120,137 +147,17 @@ spec:
         - name: TADI
           value: Test
 ---
-model:
-  name: Test
-  description: This is a test model.
-  behavior:
-    - name: do_stuff
-      type: pub-sub
-      requirements:
-        - abbrv: SUB
+spec:
+  name: Module
+  abbrv: MOD
+  description:  This is a representative module requirement specification.
+  requirements:
+    - id: 1
+      shall:  When receiving a message, the module shall respond with a value.
+      parent:
+        - abbrv: NOTSUB
           ids: 1
-"""
-
-INVALID_MODEL_1 = """
-spec:
-  name: Subsystem
-  abbrv: SUB
-  description:  This is a representative subsystem requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the subsystem shall respond with a value.
       attributes:
         - name: TADI
           value: Test
----
-model:
-  name: Test
-  description: This is a test model.
-  behavior:
-    - name: do_stuff
-      type: pub-sub
-      requirements:
-        - abbrv: SUB_SYS
-          ids: 1
-"""
-
-INVALID_MODEL_2 = """
-spec:
-  name: Subsystem
-  abbrv: SUB
-  description:  This is a representative subsystem requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the subsystem shall respond with a value.
-      attributes:
-        - name: TADI
-          value: Test
----
-model:
-  name: Test
-  description: This is a test model.
-  behavior:
-    - name: do_stuff
-      type: pub-sub
-      requirements:
-        - abbrv: SUB
-          ids: 9
-"""
-
-VALID_DATA = """
-spec:
-  name: Subsystem
-  abbrv: SUB
-  description:  This is a representative subsystem requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the subsystem shall respond with a value.
-      attributes:
-        - name: TADI
-          value: Test
----
-data:
-  name: Message
-  requirements:
-    - abbrv: SUB
-      ids: 1
-  fields:
-    - name: header
-      type: string
-    - name: body
-      type: string
-  required:
-    - header
-"""
-
-INVALID_DATA_1 = """
-spec:
-  name: Subsystem
-  abbrv: SUB
-  description:  This is a representative subsystem requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the subsystem shall respond with a value.
-      attributes:
-        - name: TADI
-          value: Test
----
-data:
-  name: Message
-  requirements:
-    - abbrv: SUB_SYS
-      ids: 1
-  fields:
-    - name: header
-      type: string
-    - name: body
-      type: string
-  required:
-    - header
-"""
-
-INVALID_DATA_2 = """
-spec:
-  name: Subsystem
-  abbrv: SUB
-  description:  This is a representative subsystem requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the subsystem shall respond with a value.
-      attributes:
-        - name: TADI
-          value: Test
----
-data:
-  name: Message
-  requirements:
-    - abbrv: SUB
-      ids: 10
-  fields:
-    - name: header
-      type: string
-    - name: body
-      type: string
-  required:
-    - header
 """
