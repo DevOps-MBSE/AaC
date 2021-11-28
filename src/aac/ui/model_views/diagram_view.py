0b99edd9d68bf39de56ffb0ce7f9756def122ea8
Alex Monnet <alexcmonnet@gmail.com>
2021-11-28 23:26:47 +0000

    Added selection list boxes and labels

--- src/aac/ui/model_views/diagram_view.py
+++ src/aac/ui/model_views/diagram_view.py
@@ -1,4 +1,4 @@
-from tkinter import font, PanedWindow, Canvas, Text, Button, Frame, Label, LEFT, RIGHT, END, TOP
+from tkinter import font, PanedWindow, Canvas, Text, Button, Listbox, Frame, Label, LEFT, RIGHT, END, TOP, BOTTOM, RIDGE
 from tkinter.ttk import Notebook
 from tkinter.font import BOLD
 
@@ -60,42 +60,73 @@ def _get_properties_tab_frame(root_notebook: Notebook) -> Frame:
     properties_components_frame = Frame(properties_frame)
     properties_components_frame.pack(fill="x", expand=True, padx=25)
 
-    properties_components_label = Label(properties_components_frame, text="Components")
+    properties_components_insert_new_frame = Frame(properties_components_frame)
+    properties_components_insert_new_frame.pack(side=TOP, fill="x", expand=True)
+
+    properties_components_label = Label(properties_components_insert_new_frame, text="Components")
     properties_components_label.pack(side=LEFT, padx=(0, 20))
 
     properties_components_plus_button_font = font.Font(size=11, weight=BOLD)
-    properties_components_plus_button = Button(properties_components_frame, text="+", command=not_implemented, font=properties_components_plus_button_font)
+    properties_components_plus_button = Button(properties_components_insert_new_frame, text="+", command=not_implemented, font=properties_components_plus_button_font)
     properties_components_plus_button.pack(side=RIGHT)
 
-    properties_components_text = Text(properties_components_frame, height=1)
+    properties_components_text = Text(properties_components_insert_new_frame, height=1)
     properties_components_text.insert(END, "Component 3")
     properties_components_text.pack(side=RIGHT, fill="x", expand=True, padx=(0, 20))
+
+    properties_components_listbox_frame = Frame(properties_components_frame, borderwidth=3, relief=RIDGE)
+    properties_components_listbox_frame.pack(side=BOTTOM, fill="x", expand=True)
+
+    properties_components_listbox_frame_label = Label(properties_components_listbox_frame, text="Model Components")
+    properties_components_listbox_frame_label.pack(side=TOP)
+
+    properties_components_listbox = Listbox(properties_components_listbox_frame)
+    properties_components_listbox.insert(1, "Component 1")
+    properties_components_listbox.insert(2, "Component 2")
+    properties_components_listbox.pack(side=BOTTOM, fill="x", expand=True)
 
     # Properties Behavior Frame #
     properties_behavior_frame = Frame(properties_frame)
     properties_behavior_frame.pack(fill="x", expand=True, padx=25)
 
-    properties_behavior_name_label = Label(properties_behavior_frame, text="Behavior")
+    properties_behavior_insert_new_frame = Frame(properties_behavior_frame)
+    properties_behavior_insert_new_frame.pack(side=TOP, fill="x", expand=True)
+
+    properties_behavior_name_label = Label(properties_behavior_insert_new_frame, text="Behavior")
     properties_behavior_name_label.pack(side=LEFT, padx=(0, 20))
 
     properties_behavior_plus_button_font = font.Font(size=11, weight=BOLD)
-    properties_behavior_plus_button = Button(properties_behavior_frame, text="+", command=not_implemented, font=properties_behavior_plus_button_font)
+    properties_behavior_plus_button = Button(properties_behavior_insert_new_frame, text="+", command=not_implemented, font=properties_behavior_plus_button_font)
     properties_behavior_plus_button.pack(side=RIGHT)
 
-    properties_behavior_name_text = Text(properties_behavior_frame, height=1)
+    properties_behavior_name_text = Text(properties_behavior_insert_new_frame, height=1)
     properties_behavior_name_text.insert(END, "Behavior 2")
     properties_behavior_name_text.pack(side=RIGHT, fill="x", expand=True, padx=(0, 20))
+
+    properties_behavior_listbox_frame = Frame(properties_behavior_frame, borderwidth=3, relief=RIDGE)
+    properties_behavior_listbox_frame.pack(side=BOTTOM, fill="x", expand=True)
+
+    properties_behavior_listbox_frame_label = Label(properties_behavior_listbox_frame, text="Model Behaviors")
+    properties_behavior_listbox_frame_label.pack(side=TOP)
+
+    properties_behavior_listbox = Listbox(properties_behavior_listbox_frame)
+    properties_behavior_listbox.insert(1, "Behavior 1")
+    properties_behavior_listbox.pack(fill="x", expand=True)
 
     return properties_frame
 
 
 def _get_imports_tab_frame(root_notebook: Notebook) -> Frame:
-    imports_frame = Frame(root_notebook)
-    imports_frame.pack()
+    imports_tab_frame = Frame(root_notebook)
+    imports_tab_frame.pack()
+
+    # Imports Frame #
+    imports_frame = Frame(imports_tab_frame)
+    imports_frame.pack(anchor="n", fill="x", expand=True, padx=40, pady=20)
 
     # Imports Name Frame #
     imports_name_frame = Frame(imports_frame)
-    imports_name_frame.pack(side=TOP, fill="x", expand=True, padx=25)
+    imports_name_frame.pack(side=TOP, fill="x", expand=True)
 
     imports_name_label = Label(imports_name_frame, text="Name")
     imports_name_label.pack(side=LEFT, padx=(0, 20))
@@ -108,7 +139,19 @@ def _get_imports_tab_frame(root_notebook: Notebook) -> Frame:
     imports_name_text.insert(END, "")
     imports_name_text.pack(side=RIGHT, fill="x", expand=True, padx=(0, 20))
 
-    return imports_frame
+    # Imports Listbox Frame #
+    imports_listbox_frame = Frame(imports_frame, borderwidth=3, relief=RIDGE)
+    imports_listbox_frame.pack(side=BOTTOM, fill="x", expand=True)
+
+    imports_listbox_frame_label = Label(imports_listbox_frame, text="Imports")
+    imports_listbox_frame_label.pack(side=TOP)
+
+    imports_listbox = Listbox(imports_listbox_frame)
+    imports_listbox.insert(1, "Imported Data")
+    imports_listbox.insert(2, "Imported Enum")
+    imports_listbox.pack(fill="x", expand=True)
+
+    return imports_tab_frame
 
 
 def not_implemented():
