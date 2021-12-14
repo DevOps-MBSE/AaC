eac906624c511ef366fc1d18157ed8e275443608
Cameron Chaparro <cvchaparro@users.noreply.github.com>
2021-12-14 14:49:39 -0600

    Update src/aac/templates/genplug/__init__.py

--- src/aac/templates/genplug/__init__.py
+++ src/aac/templates/genplug/__init__.py
@@ -1,5 +1,5 @@
 """Templates module for use by the gen-plugin plugin.
 
-These templates are what `gen-plug` uses to generate a plugin when the `aac gen-plugin` command is
+These templates are what `gen-plugin` uses to generate a plugin when the `aac gen-plugin` command is
 executed.
 """
