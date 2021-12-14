2abf3e15814498db480a6b3414cd45de83f2f202
Alex Monnet <alexcmonnet@gmail.com>
2021-12-14 17:51:28 +0000

    Implemented a new module and function for easier access to package resources.

---
+++ src/aac/package_resources.py
@@ -0,0 +1,18 @@
+"""Module that contains helper functions for interacting with package resources."""
+
+from importlib import resources
+
+
+def get_resource_file_contents(package: str, resource: str) -> str:
+    """
+    Returns the contents of a package resource as a string.
+
+    Args:
+        package (str): the package containing the resource
+        resource (str): the resource's filename
+
+    Returns:
+        A string containing the entire file content of the package resource.
+    """
+    with resources.open_text(package, resource) as resource_file:
+        return resource_file.read()
