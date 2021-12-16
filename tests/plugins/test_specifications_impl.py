e97a1caf4f088e19a5940336e363e5d3cf920f21
Alex Monnet <alexcmonnet@gmail.com>
2021-12-16 21:18:23 +0000

    Updated tests

--- tests/plugins/test_specifications_impl.py
+++ tests/plugins/test_specifications_impl.py
@@ -17,146 +17,94 @@ class TestSpecifications(TestCase):
             except AacSpecValidationException as e:
                 self.fail("validate() raised AacSpecValidationException with error: {0}".format(e))
 
-    def test_spec_validate_fails_with_missing_abbrv(self):
+    def test_spec_validate_fails_with_missing_requirement(self):
 
         with NamedTemporaryFile("w") as temp_spec:
-            temp_spec.write(INVALID_SPEC_MISSING_ABRV)
+            temp_spec.write(INVALID_SPEC_MISSING_REQ_ID)
             temp_spec.seek(0)
 
             with self.assertRaises(AacSpecValidationException) as context_manager:
                 spec_validate(temp_spec.name)
 
-                validation_exception = context_manager.exception
-                self.assertIn("Spec name Subsystem must have 1 abbrv", validation_exception.message)
-
-    def test_spec_validate_fails_with_bad_id_ref(self):
-
-        with NamedTemporaryFile("w") as temp_spec:
-            temp_spec.write(INVALID_SPEC_BAD_ID_REFERENCE)
-            temp_spec.seek(0)
-
-            with self.assertRaises(AacSpecValidationException) as context_manager:
-                spec_validate(temp_spec.name)
-
-                validation_exception = context_manager.exception
-                self.assertIn("Invalid requirement id 3 reference in", validation_exception.message)
-
-    def test_spec_validate_fails_with_bad_abbrv_ref(self):
-
-        with NamedTemporaryFile("w") as temp_spec:
-            temp_spec.write(INVALID_SPEC_BAD_ID_REFERENCE)
-            temp_spec.seek(0)
-
-            with self.assertRaises(AacSpecValidationException) as context_manager:
-                spec_validate(temp_spec.name)
-
-                validation_exception = context_manager.exception
-                self.assertIn("Invalid requirement abbreviation NOTSUB reference in", validation_exception.message)
+            validation_exception = context_manager.exception
+            self.assertIn("Invalid requirement id \"SUB-3\"", validation_exception.message)
 
 
 VALID_SPEC = """
 spec:
   name: Subsystem
-  abbreviation: SUB
   description:  This is a representative subsystem requirement specification.
   requirements:
-    - id: 1
+    - id: "SUB-1"
       shall:  When receiving a message, the subsystem shall respond with a value.
       attributes:
         - name: TADI
           value: Test
+
+  sections:
+    - name: Other Requirements
+      description:  Other requirements.
+      requirements:
+        - id: "SUB-2"
+          shall: Do things.
 ---
 spec:
   name: Module
-  abbreviation: MOD
   description:  This is a representative module requirement specification.
   requirements:
-    - id: 1
+    - id: "MOD-1"
       shall:  When receiving a message, the module shall respond with a value.
       parent:
-        - abbreviation: SUB
-          ids: 1
+        ids:
+          - "SUB-1"
+      attributes:
+        - name: TADI
+          value: Test
+    - id: "MOD-2"
+      shall:  When receiving a message do things.
+      parent:
+        ids:
+          - "SUB-2"
       attributes:
         - name: TADI
           value: Test
 """
 
-INVALID_SPEC_MISSING_ABRV = """
+INVALID_SPEC_MISSING_REQ_ID = """
 spec:
   name: Subsystem
-  abbreviation:
   description:  This is a representative subsystem requirement specification.
   requirements:
-    - id: 1
+    - id: "SUB-1"
       shall:  When receiving a message, the subsystem shall respond with a value.
       attributes:
         - name: TADI
           value: Test
----
-spec:
-  name: Module
-  abbreviation: MOD
-  description:  This is a representative module requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the module shall respond with a value.
-      parent:
-        - abbreviation: SUB_SYS
-          ids: 1
-      attributes:
-        - name: TADI
-          value: Test
-"""
 
-INVALID_SPEC_BAD_ID_REFERENCE = """
-spec:
-  name: Subsystem
-  abbreviation: SUB
-  description:  This is a representative subsystem requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the subsystem shall respond with a value.
-      attributes:
-        - name: TADI
-          value: Test
+  sections:
+    - name: Other Requirements
+      description:  Other requirements.
+      requirements:
+        - id: "SUB-2"
+          shall: Do things.
 ---
 spec:
   name: Module
-  abbreviation: MOD
   description:  This is a representative module requirement specification.
   requirements:
-    - id: 1
+    - id: "MOD-1"
       shall:  When receiving a message, the module shall respond with a value.
       parent:
-        - abbreviation: SUB
-          ids: 3
+        ids:
+          - "SUB-3"
       attributes:
         - name: TADI
           value: Test
-"""
-
-INVALID_SPEC_BAD_ABBRV_VALUE = """
-spec:
-  name: Subsystem
-  abbreviation: SUB
-  description:  This is a representative subsystem requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the subsystem shall respond with a value.
-      attributes:
-        - name: TADI
-          value: Test
----
-spec:
-  name: Module
-  abbreviation: MOD
-  description:  This is a representative module requirement specification.
-  requirements:
-    - id: 1
-      shall:  When receiving a message, the module shall respond with a value.
+    - id: "MOD-2"
+      shall:  When receiving a message do things.
       parent:
-        - abbreviation: NOTSUB
-          ids: 1
+        ids:
+          - "SUB-2"
       attributes:
         - name: TADI
           value: Test
