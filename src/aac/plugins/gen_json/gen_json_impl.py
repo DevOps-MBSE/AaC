bdf4b77acc20bb2a44ce5dd0c04667e88ce32c85
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-01-05 15:14:04 -0600

    Address some linting stuff

--- src/aac/plugins/gen_json/gen_json_impl.py
+++ src/aac/plugins/gen_json/gen_json_impl.py
@@ -1,4 +1,5 @@
 """A plugin to print JSON schema of AaC model files."""
+
 import json
 
 from aac.parser import parse_file
@@ -6,8 +7,7 @@ from aac.validator import validation
 
 
 def print_json(architecture_files: list[str]) -> None:
-    """Prints the parsed_models from the parsed architecture_files values in JSON format."""
-
+    """Print the parsed_models from the parsed architecture_files values in JSON format."""
     for architecture_file in architecture_files:
         print(f"File: {architecture_file}")
         with validation(parse_file, architecture_file) as parsed_model:
