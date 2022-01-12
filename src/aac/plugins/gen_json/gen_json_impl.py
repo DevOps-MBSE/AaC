5e930ca8d025582dc7c3867bee427e83c43c9e39
BryanBailey11 <bryan.bailey@ngc.com>
2022-01-12 20:11:58 +0000

    fully functional optional output from gen_json

--- src/aac/plugins/gen_json/gen_json_impl.py
+++ src/aac/plugins/gen_json/gen_json_impl.py
@@ -15,23 +15,29 @@ from aac.validator import validation
 plugin_name = "gen-json"
 
 
-def print_json(architecture_files: list[str], output_directory) -> PluginExecutionResult:
-    """Print or write to file the parsed_models from the parsed architecture_files values in JSON format."""
+def print_json(architecture_files: list[str], output_directory: str) -> PluginExecutionResult:
+    """Prints the parsed_models from the parsed architecture_files values in JSON format.
+
+       (optional) if output directory is given to the cli, parsed_models with be writen to
+        a new file in user defined directory
+
+    """
 
     def to_json(architecture_file: str, output_directory: str) -> str:
-        file_name = os.path.splitext(architecture_file)[0]
+        file_name = os.path.basename(architecture_file)
+        file_ext = os.path.splitext(file_name)[1]
         with validation(parse_file, architecture_file) as result:
 
-            if output_directory == None:
+            if output_directory is None:
                 return f"File: {architecture_file}\n{json.dumps(result.model, indent=2)}\n"
 
             else:
-                output_file_path = output_directory + file_name + '.json'
-                print(output_file_path)
+                file_name = file_name.replace(file_ext, '.json')
+                output_file_path = os.path.join(output_directory, file_name)
                 outFile = open(output_file_path, "w")
                 outFile.write(json.dumps(result.model, indent=2))
                 outFile.close()
-                return file_name + '.json file writen to directory'
+                return f"Successfully added {file_name} to directory"
 
     status = PluginExecutionStatusCode.SUCCESS
     messages = []
