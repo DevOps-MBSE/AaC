6cd53c89ddf306c7050a6dab0b29fd40f682f564
Alex <60155541+Coffee2Bits@users.noreply.github.com>
2022-02-02 01:41:38 -0600

    VSCode Extension MVP (#192)
    Closes #186
    * Organized the repo into python and vscode_extension projects
    * Added a github workflow for the VSCode Extension
    * Compiles, lints, tests, and packages the extension
    * Packaged extension is accessible as a pipeline artifact
    * Updated existing github workflows
    * Accounts for the new python and vscode_extension directory structure
    * Updated vscode launch configurations
    * Implemented basic vscode_extension project setup
    * Implemented basic use of the task provider
    * Due to limitations on the extension contribution points, I was unable to add user input for the tasks. Only AaC commands without arguments will work because of this
    * https://code.visualstudio.com/docs/editor/variables-reference#_input-variables can be declared by users, not extensions because there are no extension contribution points for "inputs"
    * Moved vscode-settings and tasks to specific Workspace files
    * One for the AaC Python Package
    * One for the AaC Typescript VSCode Extension
    * Switched the gitpod image to lighten the development container and reduce notification blasts for open ports

---
+++ vscode_extension/src/extension.ts
@@ -0,0 +1,14 @@
+import * as vscode from 'vscode';
+import { AacTaskProvider } from './AacTaskProvider';
+
+let aacTaskProvider: vscode.Disposable | undefined;
+
+export function activate() {
+    aacTaskProvider = vscode.tasks.registerTaskProvider(AacTaskProvider.aacType, new AacTaskProvider());
+}
+
+export function deactivate(): void {
+    if (aacTaskProvider) {
+        aacTaskProvider.dispose();
+    }
+}
