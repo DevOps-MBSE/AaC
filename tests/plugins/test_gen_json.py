0e334048fb4640d473c50d6922b350c73b533a72
BryanBailey11 <94871172+BryanBailey11@users.noreply.github.com>
2022-02-07 08:56:23 -0600

    Update tests/plugins/test_gen_json.py
    Co-authored-by: Cameron Chaparro <cvchaparro@users.noreply.github.com>

--- tests/plugins/test_gen_json.py
+++ tests/plugins/test_gen_json.py
@@ -13,10 +13,12 @@ class Testgen_json(TestCase):
             temp_arch_file.write(TEST_ARCH_YAML_STRING)
             temp_arch_file.seek(0)
 
-            with plugin_result("", print_json, [temp_arch_file.name], temp_dir) as result:
-                print(result.messages)
-                print(result.status_code)
-                self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+            result = print_json([temp_arch_file.name], temp_dir):
+            # TODO: Delete these once testing is complete
+            print(result.messages)
+            print(result.status_code)
+            self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+            self.assertIn("Wrote JSON to", "\n".join(result.messages))
 
     def test_gen_json_output_to_cli(self):
 
