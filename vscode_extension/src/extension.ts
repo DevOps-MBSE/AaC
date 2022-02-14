c4f542d74150387e2c2ee33005b2cc4763c137e6
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-02-13 20:07:35 -0600

    Update the VSCode extension
    Start the language server from the VSCode extension.

--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -1,38 +1,37 @@
 import * as fs from "fs";
-import { Disposable, ExtensionContext, tasks, window } from "vscode";
-import { LanguageClient } from "vscode-languageclient/node";
+import { Disposable, ExtensionContext, tasks, workspace } from "vscode";
+import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
 import { AacTaskProvider, execShell } from "./AacTaskProvider";
 import { ensureTrue, getConfigurationItem } from "./helpers";
 
 const MIN_REQUIRED_PYTHON_VERSION = "3.9";
+const DEFAULT_LSP_SERVER_HOST = "127.0.0.1";
+const DEFAULT_LSP_SERVER_PORT = 8080;
 
 let aacTaskProvider: Disposable | undefined;
-let aacLspClient: LanguageClient | undefined;
+let aacLspClient: LanguageClient;
 
 export function activate(context: ExtensionContext) {
     aacTaskProvider = tasks.registerTaskProvider(AacTaskProvider.aacType, new AacTaskProvider());
     startLanguageServer(context);
 }
 
-function startLanguageServer(_context: ExtensionContext) {
+function startLanguageServer(context: ExtensionContext) {
     ensureAacToolIsAvailable();
-    aacLspClient?.start();
+    ensureLspServerIsReady(context);
 }
 
 function ensureAacToolIsAvailable(): void {
-    const pythonPath = getPython3Path();
+    const pythonPath = getConfigurationItemFile("pythonPath");
     ensureCorrectPythonVersionIsInstalled(pythonPath);
     ensureCorrectAacVersionIsInstalled(pythonPath);
-
-    // TODO: Actually start the server
 }
 
-function getPython3Path(): string {
-    const pythonPath: string = getConfigurationItem("pythonPath") ?? "";
-    ensureTrue("Cannot start Language Server; 'pythonPath' is not configured!", pythonPath.length > 0);
-    ensureTrue(`Cannot use ${pythonPath} as it does not exist!`, fs.existsSync(pythonPath));
-
-    return pythonPath;
+function getConfigurationItemFile(name: string): string {
+    const item: string = getConfigurationItem(name);
+    ensureTrue(`Cannot start Language Server; '${item}' is not configured!`, item.length > 0);
+    ensureTrue(`Cannot use ${item} as it does not exist!`, fs.existsSync(item));
+    return item;
 }
 
 async function ensureCorrectPythonVersionIsInstalled(pythonPath: string): Promise<void> {
@@ -56,7 +55,50 @@ async function ensureCorrectAacVersionIsInstalled(pythonPath: string): Promise<v
     ensureTrue(`The installed aac version is ${actualAacVersion} which is unsupported.`, !!actualAacVersion);
 }
 
+function ensureLspServerIsReady(context: ExtensionContext): void {
+    const aacPath: string = getConfigurationItemFile("aacPath");
+    startLspClient(
+        context,
+        aacPath,
+        getConfigurationItem("lspServerHost") ?? DEFAULT_LSP_SERVER_HOST,
+        getConfigurationItem("lspServerPort") ?? DEFAULT_LSP_SERVER_PORT,
+    );
+}
+
+function startLspClient(context: ExtensionContext, aacPath: string, host: string, port: number): void {
+    aacLspClient = new LanguageClient(
+        aacPath,
+        getServerOptions(aacPath, "start-lsp", "--host", host, "--port", `${port}`),
+        getClientOptions()
+    );
+    context.subscriptions.push(aacLspClient.start());
+}
+
+function getServerOptions(command: string, ...args: any[]): ServerOptions {
+    return {
+        args,
+        command,
+    };
+}
+
+function getClientOptions(): LanguageClientOptions {
+    return {
+        documentSelector: [
+            { scheme: "file", language: "aac" },
+            { scheme: "untitled", language: "aac" },
+        ],
+        outputChannelName: "[aac] Architecture-as-Code Language Server",
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+        }
+    };
+}
+
 export function deactivate(): void {
     aacTaskProvider?.dispose();
+    shutdownServer();
+}
+
+function shutdownServer(): void {
     aacLspClient?.stop();
 }
