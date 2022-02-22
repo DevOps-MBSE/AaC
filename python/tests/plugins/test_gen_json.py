8e77dc2b7450b98a64c02ba0d417f45996e9f119
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-02-21 18:39:05 -0600

    Increase code coverage
    Since the newly added server.py has full code coverage, update test_gen_json.py
    to use the new temporary_test_file context manager and add a test to check the
    parser result for an unparsable file.

--- python/tests/plugins/test_gen_json.py
+++ python/tests/plugins/test_gen_json.py
@@ -1,40 +1,38 @@
+import os
+
 from unittest import TestCase
 from tempfile import TemporaryDirectory, NamedTemporaryFile
 
 from aac.plugins.gen_json.gen_json_impl import print_json
 from aac.plugins.plugin_execution import PluginExecutionStatusCode
 
+from tests.helpers.io import temporary_test_file
+
 
 class TestGenJson(TestCase):
 
     def test_print_json_with_output_directory(self):
-
-        with TemporaryDirectory() as temp_dir, NamedTemporaryFile("w") as temp_arch_file:
-            temp_arch_file.write(TEST_ARCH_YAML_STRING)
-            temp_arch_file.seek(0)
-
+        with temporary_test_file(TEST_ARCH_YAML_STRING) as temp_arch_file:
+            temp_dir = os.path.dirname(temp_arch_file.name)
             result = print_json([temp_arch_file.name], temp_dir)
             self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
             self.assertIn("Wrote JSON to", "\n".join(result.messages))
 
     def test_gen_json_output_to_cli(self):
-
-        with TemporaryDirectory(), NamedTemporaryFile("w") as temp_arch_file:
-            temp_arch_file.write(TEST_ARCH_YAML_STRING)
-            temp_arch_file.seek(0)
-
+        with temporary_test_file(TEST_ARCH_YAML_STRING) as temp_arch_file:
             result = print_json([temp_arch_file.name])
             self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
             self.assertIn('"name": "Test_model"', "\n".join(result.messages))
 
     def test_gen_json_with_invalid_arch_file(self):
-
-        with TemporaryDirectory(), NamedTemporaryFile("w") as temp_arch_file:
-            temp_arch_file.write(BAD_YAML_STRING)
-            temp_arch_file.seek(0)
-
+        with temporary_test_file(BAD_YAML_STRING) as temp_arch_file:
             result = print_json([temp_arch_file.name])
             self.assertEqual(result.status_code, PluginExecutionStatusCode.VALIDATION_FAILURE)
+
+    def test_gen_json_with_unparsable_file(self):
+        with temporary_test_file("model: ][") as temp_arch_file:
+            result = print_json([temp_arch_file.name])
+            self.assertEqual(result.status_code, PluginExecutionStatusCode.PARSER_FAILURE)
 
 
 TEST_ARCH_YAML_STRING = """
@@ -48,7 +46,6 @@ model:
       input:
         - name: architecture_file
           type: string
-
 """
 
 BAD_YAML_STRING = """
@@ -62,5 +59,4 @@ model:
       input:
         - name: architecture
           type: string
-
 """
