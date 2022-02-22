8334a97b649cddb8226b5ec5eed696f5e6852faa
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-02-22 17:46:49 -0600

    Add documentation to LspTestClient and it's methods
    Also, I extracted the common functionality for creating a server and server
    thread into a single, more generic function.

--- python/tests/lang/LspTestClient.py
+++ python/tests/lang/LspTestClient.py
@@ -1,3 +1,5 @@
+"""An LSP client used for testing the Architecture-as-Code LSP server."""
+
 import os
 import asyncio
 
@@ -14,44 +16,90 @@ default_timeout = 3
 
 
 class LspTestClient:
+    """An AaC LSP test client.
+
+    The test client handles starting a test instance of the AaC LSP server and communicating with it.
+
+    Attributes:
+        server (LanguageServer): A test version of the AaC LSP server.
+        client (LanguageServer): The client used for communicating with the AaC LSP server.
+    """
+
+    server: LanguageServer
+    client: LanguageServer
+
+    _server_thread: Thread
+    _client_thread: Thread
+
     def __init__(self):
         """Create an LSP test client."""
         client_server_reader, client_server_writer = os.pipe()
         server_client_reader, server_client_writer = os.pipe()
 
-        self._setup_server(client_server_reader, server_client_writer)
-        self._setup_client(server_client_reader, client_server_writer)
+        self.server, self._server_thread = self._configure_ls(client_server_reader, server_client_writer)
+        self.client, self._client_thread = self._configure_ls(server_client_reader, client_server_writer)
 
-    def _setup_server(self, reader, writer):
-        self.server = LanguageServer(asyncio.new_event_loop())
-        self.server_thread = Thread(target=self.server.start_io, args=(os.fdopen(reader, "rb"), os.fdopen(writer, "wb")))
-        self.server_thread.daemon = True
+    def _configure_ls(self, reader: int, writer: int):
+        """Create, configure, and return a new LanguageServer and it's associated thread.
 
-    def _setup_client(self, reader, writer):
-        self.client = LanguageServer(asyncio.new_event_loop())
-        self.client_thread = Thread(target=self.client.start_io, args=(os.fdopen(reader, "rb"), os.fdopen(writer, "wb")))
-        self.client_thread.daemon = True
+        Args:
+            reader (int): The file descriptor used for reading standard input.
+            writer (int): The file descriptor used for writing standard output.
+
+        Returns:
+            The newly created LanguageServer and it's associated thread.
+        """
+        ls = LanguageServer(asyncio.new_event_loop())
+        thread = Thread(target=ls.start_io, args=(os.fdopen(reader, "rb"), os.fdopen(writer, "wb")))
+        thread.daemon = True
+        return ls, thread
 
     def start(self):
-        self.server_thread.start()
-        self.server.thread_id = self.server_thread.ident
+        """Start the test LSP server and client."""
+        self._server_thread.start()
+        self.server.thread_id = self._server_thread.ident
         setup_features(self.server)
 
-        self.client_thread.start()
+        self._client_thread.start()
 
     def stop(self):
-        shutdown_response = self.client.lsp.send_request(methods.SHUTDOWN).result(timeout=default_timeout)
+        """Stop the test LSP server and client."""
+        shutdown_response = self.send_request(methods.SHUTDOWN).result(timeout=default_timeout)
         assert shutdown_response is None
 
-        self.client.lsp.notify(methods.EXIT)
-        self.server_thread.join()
+        self.send_notification(methods.EXIT)
+        self._server_thread.join()
 
         self.client._stop_event.set()
         try:
             self.client.loop._signal_handlers.clear()
         except AttributeError:
             pass
-        self.client_thread.join()
+        self._client_thread.join()
 
-    def send_request(self, method: str, params: Model, timeout: int = default_timeout):
-        return self.client.lsp.send_request(method, params).result(timeout=timeout)
+    def send_request(self, method: str, params: Model = None, timeout: int = default_timeout):
+        """Send a request to the server.
+
+        Args:
+            method (str): The LSP method to use for the request.
+            params (Model): The parameters to send with the request. (optional)
+            timeout (int): The timeout to use when waiting for a result. If not passed,
+                           default_timeout is used.
+
+        Returns:
+            The LSP response for the sent request.
+        """
+        if params:
+            return self.client.lsp.send_request(method, params).result(timeout=timeout)
+        return self.client.lsp.send_request(method)
+
+    def send_notification(self, method: str):
+        """Send a notification to the server.
+
+        Args:
+            method (str): The LSP method to use for the notification.
+
+        Returns:
+            The LSP response for the sent notification.
+        """
+        return self.client.lsp.notify(method)
