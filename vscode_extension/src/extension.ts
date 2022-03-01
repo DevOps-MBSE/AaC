294296abaeeef1de677167b67ff9078458f0bfc9
Alex Monnet <60155541+Coffee2Bits@users.noreply.github.com>
2022-03-01 12:53:39 -0600

    Update vscode_extension/src/extension.ts
    Co-authored-by: Cameron Chaparro <cvchaparro@users.noreply.github.com>

--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -16,8 +16,7 @@ export function activate(context: ExtensionContext) {
                 commands.registerCommand(EXECUTE_AAC_COMMAND_NAME, executeAacCommand)
             );
         } else {
-            const missingAacMessage = `Please install AaC locally to activate
-                these plugin features.\n 'pip install aac'`;
+            const missingAacMessage = "Please install AaC locally to activate these plugin features.\n 'pip install aac'";
 
             commands.registerCommand(EXECUTE_AAC_COMMAND_NAME, () => {
                 window.showErrorMessage(missingAacMessage);
