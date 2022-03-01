87a19520becbd34e835fb5c530a6941e039a9260
Alex Monnet <60155541+Coffee2Bits@users.noreply.github.com>
2022-03-01 14:27:33 -0600

    VSCode AaC Command User Input (#233)
    * Changed from Tasks to Commands
    * New command prompt the user to input arguments for AaC commands

---
+++ vscode_extension/src/aacExecutableWrapper.ts
@@ -0,0 +1,157 @@
+import { execShell } from "./helpers";
+import { getOutputChannel } from "./outputChannel";
+import { window, QuickPickOptions, InputBoxOptions, OpenDialogOptions, Uri } from 'vscode';
+
+const outputChannel = getOutputChannel();
+
+interface CommandArgument {
+    name: string;
+    description: string;
+    userResponse?: string;
+}
+
+/**
+ * Prompts the user for input then executes the AaC plugin with user-provided
+ * information and arguments.
+ */
+export async function executeAacCommand(): Promise<void> {
+
+    let availableCommands: string[] = await getAacCommandNames();
+
+    const quickPickOptions: QuickPickOptions = {
+        canPickMany: false,
+    };
+
+    window.showQuickPick(availableCommands, quickPickOptions).then(commandName => {
+        if (commandName) {
+            getAacCommandArgs(commandName).then(async (commandArguments) => {
+                await getCommandArgUserInput(commandArguments);
+                execAacShellCommand(commandName, commandArguments).then(output => {
+                    outputChannel.appendLine(output);
+                    outputChannel.show();
+                });
+            });
+        }
+    });
+}
+
+/**
+ * Gets the version of the currently installed AaC package
+ * @returns string of the version number or null if not installed.
+ */
+export async function getAaCVersion(): Promise<string | null> {
+
+    let aacVersionOutput: string = "";
+    try {
+        aacVersionOutput = await execAacShellCommand("version");
+    } catch (exception) {
+        // TODO: Change to logging or alternative
+    }
+
+    const regExp = /([0-9]+\.*){3}/;
+    const versionMatch = regExp.exec(aacVersionOutput);
+    return versionMatch ? versionMatch[0] : null;
+}
+
+async function getCommandArgUserInput(commandArguments: CommandArgument[]) {
+
+    // TODO: remove filter for optional arguments.
+    let argumentsWithoutUserResponse: CommandArgument[] = commandArguments.filter(argument => !argument.userResponse && !argument.name.startsWith("-"));
+
+    if (argumentsWithoutUserResponse.length > 0) {
+        let argumentToPromptUserFor = argumentsWithoutUserResponse[0];
+
+        if (argumentToPromptUserFor.description.toLowerCase().includes("path")) {
+            const dialogBoxOptions: OpenDialogOptions = {
+                title: argumentToPromptUserFor.name,
+                canSelectMany: false
+            };
+
+            let fileUri: Uri[] | undefined = await window.showOpenDialog(dialogBoxOptions);
+            argumentToPromptUserFor.userResponse = fileUri ? fileUri[0]?.path : "";
+
+        } else {
+            const inputBoxOptions: InputBoxOptions = {
+                title: argumentToPromptUserFor.name,
+                prompt: argumentToPromptUserFor.description,
+            };
+
+            argumentToPromptUserFor.userResponse = await window.showInputBox(inputBoxOptions);
+        }
+    } else if (argumentsWithoutUserResponse.length > 1) {
+        getCommandArgUserInput(commandArguments);
+    }
+}
+
+async function getAacCommandNames(): Promise<string[]> {
+    const aacHelpOutput = await execAacShellCommand("-h");
+    return parseTaskNamesFromHelpCommand(aacHelpOutput);
+}
+
+/**
+ * @returns list of Command arguments
+ */
+async function getAacCommandArgs(aacCommandName: string): Promise<CommandArgument[]> {
+    const aacHelpOutput = await execAacShellCommand(`${aacCommandName} -h`);
+    return parseTaskArgsFromHelpCommand(aacHelpOutput);
+}
+
+/**
+ * Executes AaC commands with arguments.
+ * @param command - AaC command name
+ * @param commandArgs - command arguments and
+ * @returns
+ */
+async function execAacShellCommand(command: string, commandArgs: CommandArgument[] = []): Promise<string> {
+    const commandArgsArray = ["aac", command, ...(commandArgs.map(argument => argument.userResponse))];
+    try {
+        const { stdout, stderr } = await execShell(commandArgsArray.join(" "), {});
+        const stringOutput = stderr.length > 0 ? stderr : stdout;
+        return stringOutput;
+    } catch (error: any) {
+        let errorMessage = error.stderr || error.stdout || "urecognized error";
+
+        outputChannel.appendLine(`Failed to execute AaC command:\n${errorMessage}`);
+        outputChannel.show(true);
+        throw error;
+    }
+}
+
+/**
+ * Parses command names from the AaC help message.
+ * @param aacHelpOutput - the output to parse
+ * @returns array of available command names
+ */
+function parseTaskNamesFromHelpCommand(aacHelpOutput: string): string[] {
+
+    const regExp = /\{(?<commandNames>.*?)\}/;
+    const commandNamesMatch = regExp.exec(aacHelpOutput);
+
+    let commandNames: string[] = [];
+    if (commandNamesMatch?.groups?.commandNames) {
+        commandNames = commandNamesMatch.groups.commandNames.split(",");
+    }
+
+    return commandNames;
+}
+
+/**
+ * Parses argument names and descriptions from command help output
+ * @param aacHelpOutput - the output to parse
+ * @returns array of CommandArgument objects
+ */
+function parseTaskArgsFromHelpCommand(aacHelpOutput: string): CommandArgument[] {
+
+    const regExp = /^  (?<argName>\S+)\s+(?<argDescription>.*)$/gm;
+    const commandArgumentsMatch = regExp.exec(aacHelpOutput);
+
+    let commandArguments: CommandArgument[] = [];
+    if (commandArgumentsMatch) {
+        commandArguments.push({
+            name: commandArgumentsMatch.groups?.argName ?? "<argument name>",
+            description: commandArgumentsMatch.groups?.argDescription ?? "<argument description>"
+        });
+    }
+
+    return commandArguments;
+}
--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -1,16 +1,30 @@
-import { Disposable, ExtensionContext, tasks } from "vscode";
+import { ExtensionContext, window, commands } from "vscode";
 import { AacLanguageServerClient } from "./AacLanguageServer";
-import { AacTaskProvider } from "./AacTaskProvider";
+import { executeAacCommand, getAaCVersion } from "./aacExecutableWrapper";
+import { getOutputChannel } from "./outputChannel";
 
-let aacTaskProvider: Disposable | undefined;
 let aacLspClient: AacLanguageServerClient = AacLanguageServerClient.getLspClient();
 
+const EXECUTE_AAC_COMMAND_NAME = "aac.execute";
+
 export function activate(context: ExtensionContext) {
-    aacTaskProvider = tasks.registerTaskProvider(AacTaskProvider.aacType, new AacTaskProvider());
-    aacLspClient.startLanguageServer(context);
+
+    getAaCVersion().then(installedAaCVersion => {
+        if (installedAaCVersion) {
+            aacLspClient.startLanguageServer(context);
+            context.subscriptions.push(
+                commands.registerCommand(EXECUTE_AAC_COMMAND_NAME, executeAacCommand)
+            );
+        } else {
+            const missingAacMessage = "Please install AaC locally to activate these plugin features.\n 'pip install aac'";
+
+            commands.registerCommand(EXECUTE_AAC_COMMAND_NAME, () => window.showErrorMessage(missingAacMessage));
+            window.showErrorMessage(missingAacMessage);
+        }
+    });
 }
 
 export function deactivate(): void {
-    aacTaskProvider?.dispose();
     aacLspClient.shutdownServer();
+    getOutputChannel().dispose();
 }
