e85a6f9daf6daf6caad86752358863f1ee05686e
Alex Monnet <alexcmonnet@gmail.com>
2022-03-14 05:01:28 +0000

    Expanded the validator plugin class and added test

--- python/src/aac/validation/Validator_Plugin.py
+++ python/src/aac/validation/Validator_Plugin.py
@@ -16,13 +16,30 @@ class Validator_Plugin:
     """
 
     name = attrib(validator=validators.instance_of(str))
-    validation_definition = attrib(validator=validators.instance_of(str))
-    validation_function = attrib(validator=validators.instance_of(callable))
+    validation_definition = attrib(validator=validators.instance_of(dict))
+    validation_function = attrib(default=None, validator=validators.instance_of((type(callable), type(None))))
     validation_arguments = attrib(default=None, validator=validators.instance_of((list, type(None))))
 
     @staticmethod
     def from_definition(validation_definition: dict) -> Validator_Plugin:
-        new_validator_plugin = Validator_Plugin()
-        new_validator_plugin.name = "Name"
-        new_validator_plugin.validation_definition = "definition"
-        return new_validator_plugin
+        """
+        Parse a validation definition and return a new instance of Validator_Plugin populated from the validation definition.
+
+        Args:
+            validation_definition: The definition used to populate the returned class instance
+
+        Returns:
+            A instance of Validator_Plugin
+        """
+        def is_validation_definition(definition):
+            return "validation" in definition
+
+        validation_definitions = list(filter(is_validation_definition, validation_definition.values()))
+
+        if len(validation_definitions) != 1:
+            raise RuntimeError(f"Expected one and only one validation defintion.\nValidation Definitions:\n{validation_definitions}")
+
+        plugin_definition = validation_definitions[0].get("validation")
+        plugin_name = plugin_definition.get("name")
+
+        return Validator_Plugin(name=plugin_name, validation_definition=validation_definition)
---
+++ python/tests/validation/test_Validator_Plugin.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from aac import parser
+from aac.validation.Validator_Plugin import Validator_Plugin
+
+
+class TestValidatorPlugin(TestCase):
+    def test_from_definition(self):
+        definition = parser.parse_str("validator_plugin_tests", TEST_VALIDATION_DEFINITION_STRING)
+
+        expected_result = Validator_Plugin(name=TEST_VALIDATION_NAME, validation_definition=definition)
+        actual_result = Validator_Plugin.from_definition(definition)
+
+        self.assertEqual(expected_result.name, actual_result.name)
+        self.assertEqual(expected_result.validation_definition, actual_result.validation_definition)
+
+
+TEST_VALIDATION_NAME = "Test Validator"
+TEST_VALIDATION_DEFINITION_STRING = f"""
+validation:
+  name: {TEST_VALIDATION_NAME}
+  description: Verifies that every validation definition has a corresponding python plugin implementation
+"""
