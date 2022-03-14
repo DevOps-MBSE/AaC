5c5b4a52b074f05fb1efebc2039124091e048969
Alex Monnet <alexcmonnet@gmail.com>
2022-03-14 05:01:04 +0000

    Added some basic logging config at the root

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -1,2 +1,12 @@
 """The Architecture-as-Code tool."""
+import logging
+
 __version__ = "0.0.5"
+
+logging.basicConfig(
+    format="%(asctime)s::%(name)s::%(levelname)s::%(message)s",
+    filename="aac.log",  # TODO: make the log naming and destination configurable
+    encoding="utf-8",
+    level=logging.DEBUG,
+    datefmt="%m/%d/%Y %H:%M:%S",
+)
