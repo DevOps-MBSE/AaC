03dfc8d09eba28c1266075ee1c1ac802073bbf05
Alex Monnet <alexcmonnet@gmail.com>
2022-03-15 17:20:23 +0000

    Added scaffolding for the other two initial validation plugin

---
+++ python/src/aac/plugins/validators/required_fields/__init__.py
@@ -0,0 +1,32 @@
+"""Validation plugin to ensure that each validation definition has a corresponding validation implementation."""
+
+from aac.package_resources import get_resource_file_contents
+from aac.plugins import hookimpl
+from aac.plugins.validators import get_validation_definition_from_plugin_definitions
+from aac.validation.ValidatorPlugin import ValidatorPlugin
+
+PLUGIN_YAML_FILE = "required_fields.yaml"
+
+
+@hookimpl
+def get_plugin_aac_definitions() -> str:
+    """
+    Return the plugins Aac definitions.
+
+    Returns:
+         string representing yaml extensions and data definitions employed by the plugin
+    """
+
+    return get_resource_file_contents(__package__, PLUGIN_YAML_FILE)
+
+
+@hookimpl
+def register_validators() -> ValidatorPlugin:
+    """
+    Returns the information about the validation plugin necessary to execute validation.
+
+    Returns:
+        A collection of data necessary to manage and execute validation plugins.
+    """
+    validation_definition = get_validation_definition_from_plugin_definitions(PLUGIN_YAML_FILE, get_plugin_aac_definitions())
+    return ValidatorPlugin.from_definition(validation_definition)
