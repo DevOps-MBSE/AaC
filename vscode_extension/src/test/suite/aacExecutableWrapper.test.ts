d72420722567cad8dab0991ae404b7e5c2c59506
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-03-19 11:16:08 -0500

    Get the tests to work (and fail when expected)

--- vscode_extension/src/test/suite/aacExecutableWrapper.test.ts
+++ vscode_extension/src/test/suite/aacExecutableWrapper.test.ts
@@ -4,21 +4,21 @@ import * as aacWrapper from "../../aacExecutableWrapper";
 import * as helpers from "../../helpers";
 
 suite("AaC Executable Wrapper Test Suite", () => {
-    test("we can get version of aac tool", () => {
-        aacWrapper.getAaCVersion().then((value) => {
+    test("we can get version of aac tool", async () => {
+        await aacWrapper.getAaCVersion().then((value) => {
             assert.strictEqual(value, helpers.getConfigurationItem("version"));
             assert.fail(`this should fail! -- value is: ${value}`);
         });
     });
 
-    test("we can execute a command with no arguments", () => {
-        aacWrapper.executeCommandWithArguments("version")
+    test("we can execute a command with no arguments", async () => {
+        await aacWrapper.executeCommandWithArguments("version")
             .then((output: string) => output.includes(helpers.getConfigurationItem("version")))
             .catch(reason => assert.fail(reason));
     });
 
-    test("we can execute a command with only required arguments", () => {
-        aacWrapper.executeCommandWithArguments("validate")
+    test("we can execute a command with only required arguments", async () => {
+        await aacWrapper.executeCommandWithArguments("validate")
             .then((output: string) => output.includes("is valid"))
             .catch(reason => assert.fail(reason));
     });
