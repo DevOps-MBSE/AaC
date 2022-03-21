986fb70901ab3408ad91615206a94fae5ac3965c
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-03-21 13:00:38 -0500

    Make withTestFile asynchronous and add a synchronous version

--- vscode_extension/src/test/suite/helpers/io.ts
+++ vscode_extension/src/test/suite/helpers/io.ts
@@ -7,7 +7,7 @@ import * as fs from "fs";
  * @param data - The data to be written to the file.
  * @param action - A thunk to run after the file has been created.
  */
-export function withTestFile(filespec: string, data: string, action: Function): void {
+export function withTestFileSync(filespec: string, data: string, action: Function): void {
     const cleanUpFile = () => {
         if (fs.existsSync(filespec)) {
             fs.rmSync(filespec);
@@ -17,6 +17,29 @@ export function withTestFile(filespec: string, data: string, action: Function): 
     try {
         fs.writeFileSync(filespec, data);
         action();
+        cleanUpFile();
+    } catch (error) {
+        cleanUpFile();
+    }
+}
+
+/**
+ * Create `filespec` and then await the specified `action`, then delete `filespec`.
+ *
+ * @param filespec - The name of the file to write.
+ * @param data - The data to be written to the file.
+ * @param action - An asynchronous thunk to run after the file has been created.
+ */
+export async function withTestFile(filespec: string, data: string, action: Function) {
+    const cleanUpFile = () => {
+        if (fs.existsSync(filespec)) {
+            fs.rmSync(filespec);
+        }
+    };
+
+    try {
+        fs.writeFileSync(filespec, data);
+        await action();
         cleanUpFile();
     } catch (error) {
         cleanUpFile();
