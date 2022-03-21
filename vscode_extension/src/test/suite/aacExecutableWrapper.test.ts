e7df80e01c9d5f4b9a2ee5dfca795ad4c298dba9
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-03-21 12:02:38 -0500

    Mock vscode.window.showOpenDialog and fix last test

--- vscode_extension/src/test/suite/aacExecutableWrapper.test.ts
+++ vscode_extension/src/test/suite/aacExecutableWrapper.test.ts
@@ -1,7 +1,12 @@
+import { ImportMock } from "ts-mock-imports";
+
 import * as assert from "assert";
+import * as vscode from "vscode";
 
 import * as aacWrapper from "../../aacExecutableWrapper";
 import * as helpers from "../../helpers";
+
+import * as io from "./helpers/io";
 
 suite("AaC Executable Wrapper Test Suite", () => {
     test("we can get version of aac tool", async () => {
@@ -15,8 +20,22 @@ suite("AaC Executable Wrapper Test Suite", () => {
     });
 
     test("we can execute a command with only required arguments", async () => {
-        await aacWrapper.executeCommandWithArguments("validate")
-            .then((output: string) => output.includes("is valid"))
-            .catch(reason => assert.fail(reason));
+        const filespec = "test.yaml";
+        const model = "\
+data:\n\
+  name: Person\n\
+  fields:\n\
+  - name: name\n\
+    type: string\n\
+  required:\n\
+  - name\n";
+
+        io.withTestFile(filespec, model, async () => {
+            const stub = ImportMock.mockFunction(vscode.window, "showOpenDialog", vscode.Uri.parse(filespec));
+            await aacWrapper.executeCommandWithArguments("validate")
+                .then((output: string) => assert.ok(output.includes(`${filespec} is valid`)))
+                .catch(reason => assert.fail(reason));
+            stub.restore();
+        });
     });
 });
---
+++ vscode_extension/src/test/suite/helpers/io.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+
+/**
+ * Create `filespec` and then run the specified `action`, then delete `filespec`.
+ *
+ * @param filespec - The name of the file to write.
+ * @param data - The data to be written to the file.
+ * @param action - A thunk to run after the file has been created.
+ */
+export function withTestFile(filespec: string, data: string, action: Function): void {
+    const cleanUpFile = () => {
+        if (fs.existsSync(filespec)) {
+            fs.rmSync(filespec);
+        }
+    };
+
+    try {
+        fs.writeFileSync(filespec, data);
+        action();
+        cleanUpFile();
+    } catch (error) {
+        cleanUpFile();
+    }
+}
