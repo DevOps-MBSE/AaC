05625042979fc31542bf0d50bf20ced3f9e637a1
Alex Monnet <alexcmonnet@gmail.com>
2022-03-22 18:45:53 +0000

    Update log format

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -4,7 +4,7 @@ import logging
 __version__ = "0.0.5"
 
 logging.basicConfig(
-    format="%(asctime)s::%(name)s::%(levelname)s::%(message)s",
+    format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
     filename="aac.log",
     encoding="utf-8",
     level=logging.DEBUG,
