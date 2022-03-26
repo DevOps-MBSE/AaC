a5e9e3a1c96e79c4b93e7ed6ed5a493c04417f4e
Alex Monnet <alexcmonnet@gmail.com>
2022-03-26 02:27:31 +0000

    Update tests

--- python/tests/plugins/validators/test_defined_references.py
+++ python/tests/plugins/validators/test_defined_references.py
@@ -1,19 +1,24 @@
 from unittest import TestCase
 
+from aac.lang import get_active_context
 from aac.definition_helpers import get_definitions_by_type
 from aac.parser import parse
-from aac.plugins.validators.validator_implementation import get_plugin_aac_definitions, register_validators
+from aac.plugins.validators.defined_references import get_plugin_aac_definitions, register_validators
 from aac.validate import ValidatorPlugin
 
 
-class TestValidationImplementPlugin(TestCase):
+class TestDefinedReferencesPlugin(TestCase):
 
-    def test_validation_module_register_validators(self):
+    def setUp(self) -> None:
+        get_active_context(reload_context=True)
+
+    def test_module_register_validators(self):
         actual_validator_plugin = register_validators()
 
         validation_definitions = get_definitions_by_type(parse(get_plugin_aac_definitions()), "validation")
         self.assertEqual(1, len(validation_definitions))
 
-        expected_validator_plugin = ValidatorPlugin(name="Validation definition has an implementation", definition=validation_definitions[0])
+        expected_validator_plugin = ValidatorPlugin(name="Type references exist", definition=validation_definitions[0])
         self.assertEqual(expected_validator_plugin.name, actual_validator_plugin.name)
         self.assertEqual(expected_validator_plugin.definition, actual_validator_plugin.definition)
+
