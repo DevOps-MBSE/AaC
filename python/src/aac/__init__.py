1df8a53eecc2603f97cfc1c666e1557f3e5afea4
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-04-06 16:52:12 -0500

    Revert removal of filename parameter

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -6,6 +6,7 @@ __version__ = "0.0.5"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
+    filename="aac.log",
     encoding="utf-8",
     level=logging.DEBUG,
     datefmt="%m/%d/%Y %H:%M:%S",
