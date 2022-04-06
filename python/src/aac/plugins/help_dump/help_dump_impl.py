447e303d98183fc11a0a64d33fb60c8d895e6581
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-04-06 15:56:12 -0500

    Implement the help-dump command

--- python/src/aac/plugins/help_dump/help_dump_impl.py
+++ python/src/aac/plugins/help_dump/help_dump_impl.py
@@ -1,6 +1,10 @@
 """AaC Plugin implementation module for the help-dump plugin."""
 
+from iteration_utilities import flatten
+
+from aac.plugins import plugin_manager
 from aac.plugins.plugin_execution import PluginExecutionResult, plugin_result
+from aac.cli.aac_command import AacCommand
 
 plugin_name = "help-dump"
 
@@ -12,8 +16,23 @@ def help_dump() -> PluginExecutionResult:
     Returns:
         help_output (str): The formatted string with all commands, etc.
     """
-    def _implement_and_rename_me():
-        raise NotImplementedError("help_dump is not implemented.")
+    def get_command_info():
+        return "\n".join([_format_command(command) for command in _get_all_commands()])
 
-    with plugin_result(plugin_name, _implement_and_rename_me) as result:
+    with plugin_result(plugin_name, get_command_info) as result:
         return result
+
+
+def _format_command(command: AacCommand) -> str:
+    def format_argument(argument):
+        return f"\n{argument.name}::{argument.description}"
+
+    return (
+        f"{command.name}::{command.description}::{len(command.arguments)}"
+        + "".join(map(format_argument, command.arguments))
+    )
+
+
+def _get_all_commands() -> list[AacCommand]:
+    all_plugins = plugin_manager.get_plugin_manager().get_plugins()
+    return list(flatten([plugin.get_commands() for plugin in all_plugins if hasattr(plugin, "get_commands")]))
--- python/tests/plugins/test_help_dump_impl.py
+++ python/tests/plugins/test_help_dump_impl.py
@@ -1,10 +1,47 @@
 from unittest import TestCase
 
-from aac.plugins.plugin_execution import PluginExecutionStatusCode
-from aac.plugins.help_dump.help_dump_impl import help_dump
+from aac.plugins.help_dump.help_dump_impl import help_dump, _format_command, _get_all_commands
+from aac.cli.aac_command import AacCommand, AacCommandArgument
+
+from tests.helpers.assertion import assert_plugin_success
 
 
 class TestHelpDump(TestCase):
-    def test_help_dump(self):
+    def test_format_command_line(self):
+        command_name = "command-name"
+        command_description = "A command description."
+        command_arguments = [self.create_test_command_argument(f"arg{i}", f"The arg{i} description.") for i in range(3)]
+
+        for i in range(3):
+            expected = self.expected_formatted_command_string(
+                command_name,
+                command_description,
+                i,
+                [self.expected_formatted_command_argument_string(arg.name, arg.description) for arg in command_arguments[:i]]
+            )
+            actual = _format_command(self.create_test_command(command_name, command_description, command_arguments[:i]))
+
+            self.assertEqual(expected, actual)
+
+    def test_help_dump_prints_first_party_commands(self):
         result = help_dump()
-        self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+        assert_plugin_success(result)
+
+        for command_name in [cmd.name for cmd in _get_all_commands()]:
+            self.assertIn(command_name, result.get_messages_as_string())
+
+        for command_description in [cmd.description for cmd in _get_all_commands()]:
+            self.assertIn(command_description, result.get_messages_as_string())
+
+    def create_test_command(self, name, description, command_args):
+        return AacCommand(name, description, lambda: f"Test Command: {name}\n{description}", command_args)
+
+    def create_test_command_argument(self, name, description, number_of_arguments=None):
+        return AacCommandArgument(name, description, number_of_arguments)
+
+    def expected_formatted_command_string(self, command_name, command_description, num_args, command_arguments_strings):
+        command_arguments_string = ("\n" if num_args > 0 else "") + "\n".join(command_arguments_strings)
+        return f"{command_name}::{command_description}::{num_args}{command_arguments_string}"
+
+    def expected_formatted_command_argument_string(self, argument_name, argument_description):
+        return f"{argument_name}::{argument_description}"
