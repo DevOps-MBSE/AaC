2a7cbcbaae09ba4b9a05493ed3d64b30327f26df
Alex Monnet <alexcmonnet@gmail.com>
2022-04-09 22:23:23 +0000

    Bump the version number a minor increment

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -1,7 +1,7 @@
 """The Architecture-as-Code tool."""
 import logging
 
-__version__ = "0.0.5"
+__version__ = "0.1.0"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
