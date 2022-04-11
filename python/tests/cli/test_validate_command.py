00f564cdcaea178af1763c656d50c9743c6ae6b8
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-04-10 19:59:10 -0500

    Update test_validate_command.py
    Check the result messages to make sure we aren't getting false positives.

--- python/tests/cli/test_validate_command.py
+++ python/tests/cli/test_validate_command.py
@@ -16,10 +16,15 @@ class TestValidateCommand(TestCase):
         valid_model = create_model_definition("Valid Model", "A valid model.")
         with temporary_test_file(valid_model.content) as valid_model_file:
             result = validate(valid_model_file.name)
+
             assert_plugin_success(result)
+            self.assertIn(f"{valid_model_file.name} is valid", result.get_messages_as_string())
 
     def test_validate_command_fails_with_invalid_definition(self):
-        invalid_model = create_model_definition("Invalid Model", "An invalid model.", [create_behavior_entry("a behavior with no type")])
+        invalid_model = create_model_definition("Invalid Model", "An invalid model.", [create_behavior_entry("a behavior with an invalid type", "invalid")])
         with temporary_test_file(invalid_model.content) as invalid_model_file:
             result = validate(invalid_model_file.name)
+
             assert_validation_failure(result)
+            self.assertIn("Failed to validate", result.get_messages_as_string())
+            self.assertIn("Undefined type 'invalid'", result.get_messages_as_string())
