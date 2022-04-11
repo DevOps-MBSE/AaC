fac81cf893bdd5a247761586445e460c86f86c8b
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-04-10 19:44:41 -0500

    Update test_validate_command.py
    Try resetting the active context to make if the tests work in the pipeline.

--- python/tests/cli/test_validate_command.py
+++ python/tests/cli/test_validate_command.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from aac.cli.builtin_commands.validate.validate_impl import validate
+from aac.lang.active_context_lifecycle_manager import get_active_context
 
 from tests.helpers.assertion import assert_plugin_success, assert_validation_failure
 from tests.helpers.parsed_definitions import create_behavior_entry, create_model_definition
@@ -8,6 +9,9 @@ from tests.helpers.io import temporary_test_file
 
 
 class TestValidateCommand(TestCase):
+    def setUp(self):
+        get_active_context(reload_context=True)
+
     def test_validate_command_succeeds_with_valid_definition(self):
         valid_model = create_model_definition("Valid Model", "A valid model.")
         with temporary_test_file(valid_model.content) as valid_model_file:
@@ -15,7 +19,7 @@ class TestValidateCommand(TestCase):
             assert_plugin_success(result)
 
     def test_validate_command_fails_with_invalid_definition(self):
-        invalid_model = create_model_definition("Invalid Model", "An invalid model.", [create_behavior_entry("not a component")])
+        invalid_model = create_model_definition("Invalid Model", "An invalid model.", [create_behavior_entry("a behavior with no type")])
         with temporary_test_file(invalid_model.content) as invalid_model_file:
             result = validate(invalid_model_file.name)
             assert_validation_failure(result)
