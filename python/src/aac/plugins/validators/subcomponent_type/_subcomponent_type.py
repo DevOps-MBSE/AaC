b410de2a4dbdb7697c41f70bc23f538b010dd331
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-04-12 12:39:23 -0500

    Implement the validator function

--- python/src/aac/plugins/validators/subcomponent_type/_subcomponent_type.py
+++ python/src/aac/plugins/validators/subcomponent_type/_subcomponent_type.py
@@ -1,16 +1,18 @@
+import logging
+
 from aac.lang.language_context import LanguageContext
 from aac.lang.definitions.definition import Definition
 from aac.lang.definitions.structure import get_substructures_by_type
 from aac.plugins.validators import ValidatorResult
 
 
-def validate_subcomponent_types(definition_under_test: Definition, target_sub_definition: Definition, active_context: LanguageContext) -> ValidatorResult:
+def validate_subcomponent_types(definition_under_test: Definition, target_schema_definition: Definition, active_context: LanguageContext) -> ValidatorResult:
     """
     Validate that the subcomponents of the definition are models.
 
     Args:
         definition_under_test (Definition): The definition that's being validated. (Root validation definitions)
-        target_sub_definition (Definition): A definition with applicable validation. ("Validation" definition)
+        target_schema_definition (Definition): A definition with applicable validation. ("Validation" definition)
         active_context (LanguageContext): The active context.
 
     Returns:
@@ -18,11 +20,24 @@ def validate_subcomponent_types(definition_under_test: Definition, target_sub_de
     """
     error_messages = []
 
-    def validate_model_subcomponents(dict_to_validate: dict) -> list[str]:
-        print(dict_to_validate)
-        return []
+    def validate_model_subcomponents(dict_to_validate: dict):
+        subcomponents = dict_to_validate.get("components", [])
 
-    dicts_to_test = get_substructures_by_type(definition_under_test, target_sub_definition, active_context)
+        for component in subcomponents:
+            component_type = component.get("type")
+            definition = active_context.get_definition_by_name(component_type)
+
+            expected_type = "model"
+            actual_type = definition and definition.get_root_key()
+            if definition and actual_type != expected_type:
+                incorrect_subcomponent_type = (
+                    f"Expected '{expected_type}' as the subcomponent type but found '{component_type}' with type "
+                    f"'{actual_type}' in: {dict_to_validate}"
+                )
+                error_messages.append(incorrect_subcomponent_type)
+                logging.debug(incorrect_subcomponent_type)
+
+    dicts_to_test = get_substructures_by_type(definition_under_test, target_schema_definition, active_context)
     list(map(validate_model_subcomponents, dicts_to_test))
 
     return ValidatorResult(error_messages, len(error_messages) == 0)
