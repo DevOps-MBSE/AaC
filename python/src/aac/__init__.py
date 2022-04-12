1e031daced6bc7c8af097b8add22a6eed433f2d2
Alex Monnet <60155541+Coffee2Bits@users.noreply.github.com>
2022-04-12 15:39:16 -0500

    Validator overhaul minor version bump to 0.1.0 (#294)

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -2,7 +2,7 @@
 
 import logging
 
-__version__ = "0.0.5"
+__version__ = "0.1.0"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
