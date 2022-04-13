31aa7dd9d19642df4659f1cd160b382dc1c6f05d
Alex Monnet <alexcmonnet@gmail.com>
2022-04-13 23:53:49 +0000

    Renamed variable per PR comment

--- python/src/aac/lang/lsp/code_completion_provider.py
+++ python/src/aac/lang/lsp/code_completion_provider.py
@@ -19,18 +19,18 @@ class CodeCompletionProvider:
     """Resolve various code completion triggers to specific code completion functions.
 
     Attributes:
-        registered_completion_callbacks (dict[str, Callable]): The dict of trigger characters to corresponding code completion functions
+        completion_callbacks (dict[str, Callable]): The dict of trigger characters to corresponding code completion functions
     """
 
-    registered_completion_callbacks: dict = attrib(default=Factory(dict), validator=validators.instance_of(dict))
+    completion_callbacks: dict = attrib(default=Factory(dict), validator=validators.instance_of(dict))
 
     def __attrs_post_init__(self):
         """Post init hook for attrs classes."""
-        self.registered_completion_callbacks[SPACE_TRIGGER] = _handle_space_code_completion
+        self.completion_callbacks[SPACE_TRIGGER] = _handle_space_code_completion
 
     def get_trigger_characters(self) -> list[str]:
         """Return a list of the currently registered trigger characters."""
-        return list(self.registered_completion_callbacks.keys())
+        return list(self.completion_callbacks.keys())
 
     def handle_code_completion(self, ls: LanguageServer, params: CompletionParams):
         """Resolve the trigger to the corresponding code completion function, then execute it."""
@@ -40,11 +40,11 @@ class CodeCompletionProvider:
             source_document_content = ls.workspace.documents.get(params.text_document.uri).source.split(os.linesep)
             trigger_character = source_document_content[params.position.line][params.position.character - 1]
 
-        callback_function = self.registered_completion_callbacks.get(trigger_character)
+        callback_function = self.completion_callbacks.get(trigger_character)
         if callback_function:
             return callback_function(ls, params)
         else:
-            logging.debug(f"Failed to find corresponding code completion function for the registered trigger: {trigger_character} in registered callbacks: {self.registered_completion_callbacks}")
+            logging.debug(f"Failed to find corresponding code completion function for the registered trigger: {trigger_character} in registered callbacks: {self.completion_callbacks}")
 
 
 def _handle_space_code_completion(ls: LanguageServer, params: CompletionParams):
