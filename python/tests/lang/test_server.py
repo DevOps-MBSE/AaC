569b502cf0c03c0b30d417b4a1dccb24cc8a8d6e
Alex Monnet <alexcmonnet@gmail.com>
2022-04-14 20:17:41 +0000

    Fixed flake issues.

--- python/tests/lang/test_server.py
+++ python/tests/lang/test_server.py
@@ -1,20 +1,14 @@
 from unittest import TestCase
 
-from aac.lang.lsp.server import start_lsp
-from aac.lang.active_context_lifecycle_manager import get_active_context
 from pygls import uris
 from pygls.lsp import methods
 from pygls.lsp.types import (
     ClientCapabilities,
-    CompletionItem,
-    CompletionParams,
     Hover,
     HoverParams,
     InitializeParams,
     Position,
 )
-
-from aac.lang.active_context_lifecycle_manager import get_active_context
 
 from tests.lang.LspTestClient import LspTestClient
 
@@ -25,7 +19,9 @@ class TestLspServer(TestCase):
         self.client.start()
         res = self.client.send_request(
             methods.INITIALIZE,
-            InitializeParams(process_id=12345, root_uri="file://", capabilities=ClientCapabilities()),
+            InitializeParams(
+                process_id=12345, root_uri="file://", capabilities=ClientCapabilities()
+            ),
         )
 
         self.assertIn("capabilities", res)
@@ -36,7 +32,10 @@ class TestLspServer(TestCase):
     def test_handles_hover_request(self):
         res: Hover = self.client.send_request(
             methods.HOVER,
-            HoverParams(text_document={"uri": TEST_DOCUMENT_URI}, position=Position(line=0, character=0)),
+            HoverParams(
+                text_document={"uri": TEST_DOCUMENT_URI},
+                position=Position(line=0, character=0),
+            ),
         )
 
         self.assertSequenceEqual(list(res.keys()), ["contents"])
