4557368eb8b1974681f1cb8ca17bb971b80376b8
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-04-21 18:53:33 -0500

    Update extension tests

--- vscode_extension/src/test/suite/aacExecutableWrapper.test.ts
+++ vscode_extension/src/test/suite/aacExecutableWrapper.test.ts
@@ -4,14 +4,20 @@ import * as aacWrapper from "../../aacExecutableWrapper";
 import * as helpers from "../../helpers";
 
 suite("AaC Executable Wrapper Test Suite", () => {
-    test("we can get version of aac tool", async () => {
+    test("it should get the version of the aac tool", async () => {
         await aacWrapper.getAaCVersion()
             .then(value => assert.strictEqual(value, helpers.getConfigurationItem("version")))
             .catch(reason => assert.fail(reason));
     });
 
-    test("we can execute a command with no arguments", async () => {
-        await aacWrapper.executeCommandWithArguments("version")
+    test("it should execute a command with no arguments", async () => {
+        const versionCommand = {
+            name: "version",
+            description: "",
+            arguments: [],
+        };
+
+        await aacWrapper.executeCommandWithArguments(versionCommand)
             .then((output: string) => assert.ok(output.includes(helpers.getConfigurationItem("version"))))
             .catch(reason => assert.fail(reason));
     });
