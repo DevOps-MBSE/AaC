34a5861b8923e0c178204ba9be79b2f362223b63
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-04-21 15:28:27 -0500

    Temporarily remove filename parameter to enable testing

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -6,7 +6,6 @@ __version__ = "0.1.0"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
-    filename="aac.log",
     encoding="utf-8",
     level=logging.DEBUG,
     datefmt="%m/%d/%Y %H:%M:%S",
