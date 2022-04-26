b5ced0eaaac8f60039252c6286f7d9397b329202
Cameron Chaparro <cvchaparro@users.noreply.github.com>
2022-04-25 21:40:00 -0400

    Update the VSCode extension to leverage the help-dump command (#312)
    * Temporarily remove filename parameter to enable testing
    
    * Use absolute file path for filename
    
    I don't know why this didn't work before but it seems to be working okay, now.
    
    * Update aacExecutableWrapper.ts
    
    Use the help-dump command to get commands and descriptions, etc.
    
    * Update extension tests
    
    * Remove nested quotes
    
    These are causing issues in the extension when trying to parse the JSON.

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -1,12 +1,13 @@
 """The Architecture-as-Code tool."""
 
 import logging
+import os
 
 __version__ = "0.1.0"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
-    filename="aac.log",
+    filename=os.path.join(os.path.dirname(__file__), "aac.log"),
     encoding="utf-8",
     level=logging.DEBUG,
     datefmt="%m/%d/%Y %H:%M:%S",
