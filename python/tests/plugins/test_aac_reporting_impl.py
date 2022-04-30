d7c098b5192a3ba3a979e795a8c05c428bdec904
jondavid-black <black.jondavid@gmail.com>
2022-04-30 12:58:06 +0000

    initial report plugin idea

---
+++ python/tests/plugins/test_aac_reporting_impl.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from aac.plugins.plugin_execution import PluginExecutionStatusCode
+from aac.plugins.report.aac_reporting_impl import report, report_csv
+
+
+class TestAacReporting(TestCase):
+    def test_report(self):
+        # TODO: Write tests for report
+
+        architecture_file = str()
+        output_file = str()
+        
+        result = report(architecture_file=architecture_file, output_file=output_file)
+        self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+    
+    def test_report_csv(self):
+        # TODO: Write tests for report_csv
+
+        architecture_file = str()
+        output_file = str()
+        
+        result = report_csv(architecture_file=architecture_file, output_file=output_file)
+        self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+    
