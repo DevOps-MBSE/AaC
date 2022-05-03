2c3f46291f06355a1bd17da5586e839eb0e5f9a5
Alex Monnet <alexcmonnet@gmail.com>
2022-05-03 05:11:25 +0000

    Added text sync to the LSP server

--- python/src/aac/lang/lsp/language_server.py
+++ python/src/aac/lang/lsp/language_server.py
@@ -1,6 +1,6 @@
 """The Architecture-as-Code Language Server."""
 
-from attr import attrib, attrs, validators
+from attr import Factory, attrib, attrs, validators
 import logging
 from typing import Optional
 from pygls.server import LanguageServer
@@ -9,17 +9,17 @@ from pygls.lsp import (
     CompletionParams,
     Hover,
     HoverParams,
-    InitializeParams,
-    InitializeResult,
-    ServerCapabilities,
+    TextDocumentSyncKind,
     DidChangeTextDocumentParams,
     DidCloseTextDocumentParams,
     DidOpenTextDocumentParams,
     methods
 )
 
+from aac.parser import parse
 from aac.lang.active_context_lifecycle_manager import get_initialized_language_context
 from aac.lang.language_context import LanguageContext
+from aac.lang.lsp.managed_workspace_file import ManagedWorkspaceFile
 from aac.lang.lsp.code_completion_provider import CodeCompletionProvider
 
 
@@ -36,6 +36,7 @@ class AacLanguageServer:
     language_server: Optional[LanguageServer] = attrib(default=None, validator=validators.instance_of((type(None), LanguageServer)))
     language_context: Optional[LanguageContext] = attrib(default=None, validator=validators.instance_of((type(None), LanguageContext)))
     code_completion_provider: Optional[CodeCompletionProvider] = attrib(default=None, validator=validators.instance_of((type(None), CodeCompletionProvider)))
+    workspace_files: dict[str, ManagedWorkspaceFile] = attrib(default=Factory(dict), validator=validators.instance_of(dict))
 
     def __attrs_post_init__(self):
         """Post init hook for attrs classes."""
@@ -48,31 +49,44 @@ class AacLanguageServer:
         self.code_completion_provider = self.code_completion_provider or CodeCompletionProvider()
         self.setup_features()
 
-        logging.debug("AaC LSP initialized.")
-
     def setup_features(self) -> None:
         """Configure the server with the supported features."""
         server = self.language_server
-
-        @server.feature(methods.TEXT_DOCUMENT_DID_CHANGE)
-        def did_change(ls, params: DidChangeTextDocumentParams):
-            """Text document did change notification."""
-            logging.debug(f"Text document changed {params.text_document.uri}.")
-
-        @server.feature(methods.TEXT_DOCUMENT_DID_CLOSE)
-        def did_close(server: LanguageServer, params: DidCloseTextDocumentParams):
-            """Text document did close notification."""
-            logging.debug(f"Text document closed {params.text_document.uri}.")
+        server.sync_kind = TextDocumentSyncKind.FULL
 
         @server.feature(methods.TEXT_DOCUMENT_DID_OPEN)
         async def did_open(ls, params: DidOpenTextDocumentParams):
             """Text document did open notification."""
-            logging.debug(f"Text document opened {params.text_document.uri}.")
+            file_uri = params.text_document.uri
+            managed_file = self.workspace_files.get(file_uri)
 
-        @server.feature(methods.INITIALIZE)
-        async def handle_initialize(ls: LanguageServer, params: InitializeParams):
-            """Handle initialize request."""
-            return InitializeResult(capabilities=ServerCapabilities(hover_provider=True))
+            if not managed_file:
+                managed_file = ManagedWorkspaceFile(file_uri)
+                self.workspace_files[file_uri] = managed_file
+
+            managed_file.is_client_managed = True
+            _, file_path = file_uri.split("file://")
+            self.language_context.add_definitions_to_context(parse(file_path))
+            logging.info(f"Text document opened by LSP client {file_uri}.")
+
+        @server.feature(methods.TEXT_DOCUMENT_DID_CLOSE)
+        def did_close(server: LanguageServer, params: DidCloseTextDocumentParams):
+            """Text document did close notification."""
+            file_uri = params.text_document.uri
+            managed_file = self.workspace_files.get(file_uri)
+            managed_file.is_client_managed = False
+            logging.info(f"Text document closed by LSP client {file_uri}.")
+
+        @server.feature(methods.TEXT_DOCUMENT_DID_CHANGE)
+        def did_change(ls, params: DidChangeTextDocumentParams):
+            """Text document did change notification."""
+            file_uri = params.text_document.uri
+            _, file_path = file_uri.split("file://")
+            affected_definitions = self.language_context.get_definitions_by_file_uri(file_path)
+            altered_definition_names = [definition.name for definition in affected_definitions]
+            logging.info(f"Definitions identified to update due to doc change: {altered_definition_names}")
+
+            logging.info(f"Text document altered by LSP client {params.text_document.uri}.")
 
         @server.feature(methods.HOVER)
         async def handle_hover(ls: LanguageServer, params: HoverParams):
