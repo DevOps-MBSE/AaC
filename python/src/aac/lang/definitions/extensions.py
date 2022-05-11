175f7f03af92abf76abd6531b65be2cc9fca61a1
Alex Monnet <alexcmonnet@gmail.com>
2022-05-11 16:52:59 +0000

    Improved naming and readability for extensions module

--- python/src/aac/lang/definitions/extensions.py
+++ python/src/aac/lang/definitions/extensions.py
@@ -13,18 +13,18 @@ def apply_extension_to_definition(extension_definition: Definition, target_defin
         extension_definition (Definition): The extension definition to apply to the context.
         target_definition (Definition): The definition to in the context to which to apply the extension.
     """
-    target_definition_extension_sub_dict = target_definition.get_fields()
-    extension_subtype_sub_dict = _get_extension_subtype_dict(extension_definition)
+    target_definition_fields = target_definition.get_fields()
+    extension_additional_content = _get_extension_additional_content_dict(extension_definition)
     extension_field_name = _get_extension_field_name(extension_definition)
 
-    if target_definition_extension_sub_dict.get(extension_field_name):
-        target_definition_extension_sub_dict[extension_field_name] += extension_subtype_sub_dict.get("add")
+    if target_definition_fields.get(extension_field_name):
+        target_definition_fields[extension_field_name] += extension_additional_content.get("add")
     else:
         logging.error(
             f"Attempted to apply an extension to the incorrect target. Extension name '{extension_definition.name}' target definition '{target_definition.name}'"
         )
 
-    _add_extension_required_fields_to_defintion(target_definition_extension_sub_dict, extension_subtype_sub_dict)
+    _add_extension_required_fields_to_defintion(target_definition_fields, extension_additional_content)
 
 
 def remove_extension_from_definition(extension_definition: Definition, target_definition: Definition) -> None:
@@ -35,26 +35,27 @@ def remove_extension_from_definition(extension_definition: Definition, target_de
         extension_definition (Definition): The extension definition to apply to the context.
         target_definition (Definition): The extension definition to apply to the context.
     """
-    target_definition_extension_sub_dict = target_definition.get_fields()
-    extension_subtype_sub_dict = _get_extension_subtype_dict(extension_definition)
+    target_definition_fields = target_definition.get_fields()
+    extension_additional_content = _get_extension_additional_content_dict(extension_definition)
     extension_field_name = _get_extension_field_name(extension_definition)
 
-    if target_definition_extension_sub_dict.get(extension_field_name):
-        elements_to_remove = extension_subtype_sub_dict.get("add")
+    if target_definition_fields.get(extension_field_name):
+        elements_to_remove = extension_additional_content.get("add")
         if not isinstance(elements_to_remove, list):
             elements_to_remove = [elements_to_remove]
 
         for element_to_remove in elements_to_remove:
-            target_definition_extension_sub_dict[extension_field_name].remove(element_to_remove)
+            target_definition_fields[extension_field_name].remove(element_to_remove)
     else:
         logging.error(
             f"Attempted to remove a missing extension field from target. Extension name '{extension_definition.name}' target definition '{target_definition.name}'"
         )
 
-    _remove_extension_required_fields_to_defintion(target_definition_extension_sub_dict, extension_subtype_sub_dict)
+    _remove_extension_required_fields_to_defintion(target_definition_fields, extension_additional_content)
 
 
-def _get_extension_subtype_dict(extension_definition: Definition) -> dict:
+def _get_extension_additional_content_dict(extension_definition: Definition) -> dict:
+    """Return the extension's additive information fields based on the extension's sub-type (enumExt/dataExt)."""
     extension_definition_fields = extension_definition.get_fields()
     extension_type = "enum" if extension_definition.is_enum_extension() else "data"
     ext_type = f"{extension_type}Ext"
@@ -62,28 +63,30 @@ def _get_extension_subtype_dict(extension_definition: Definition) -> dict:
 
 
 def _get_extension_field_name(extension_definition: Definition) -> str:
+    """Get the appropriate field name (values/fields) for the extension's additional content."""
     return "values" if extension_definition.is_enum_extension() else "fields"
 
 
-def _add_extension_required_fields_to_defintion(target_definition_sub_dict, extension_dictionary_sub_dict):
-    if "required" in extension_dictionary_sub_dict:
+def _add_extension_required_fields_to_defintion(target_definition_fields: dict, extension_additional_content_fields: dict) -> None:
+    """Add any additional required fields from the extension to the target definition."""
+    extension_required_fields = extension_additional_content_fields.get("required")
 
-        if "required" not in target_definition_sub_dict:
-            target_definition_sub_dict["required"] = []
+    if extension_required_fields:
+        if "required" not in target_definition_fields:
+            target_definition_fields["required"] = []
 
-        target_definition_sub_dict["required"] += extension_dictionary_sub_dict.get("required") or []
+        target_definition_fields["required"] += extension_required_fields
 
 
-def _remove_extension_required_fields_to_defintion(target_definition_sub_dict, extension_dictionary_sub_dict):
-    if "required" in extension_dictionary_sub_dict:
-        required_fields = extension_dictionary_sub_dict.get("required") or []
+def _remove_extension_required_fields_to_defintion(target_definition_fields: dict, extension_additional_content_fields: dict) -> None:
+    """Remove the extension's required fields from the target definition's required fields."""
+    extension_required_fields = extension_additional_content_fields.get("required") or []
+    target_required_fields = target_definition_fields.get("required") or []
 
-        for required_field in required_fields:
-            target_definition_required_fields = target_definition_sub_dict.get("required")
-
-            if required_field in target_definition_required_fields:
-                target_definition_required_fields.remove(required_field)
-            else:
-                logging.error(
-                    f"Extension-applied required field '{required_field}' is not present in target dictionary: '{target_definition_sub_dict}'"
-                )
+    for required_field in extension_required_fields:
+        if required_field in target_required_fields:
+            target_required_fields.remove(required_field)
+        else:
+            logging.error(
+                f"Extension-applied required field '{required_field}' is not present in target dictionary: '{target_definition_fields}'"
+            )
