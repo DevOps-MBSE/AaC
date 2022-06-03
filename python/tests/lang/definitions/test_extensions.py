af9c55f51e0dcf58860fe3fbc9d69f9c493ef808
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-06-02 20:58:50 -0500

    Rename references to definition -> schema

--- python/tests/lang/definitions/test_extensions.py
+++ python/tests/lang/definitions/test_extensions.py
@@ -15,14 +15,14 @@ from tests.helpers.parsed_definitions import (
 class TestDefinitionExtensions(TestCase):
 
     def test_apply_and_remove_extension_from_definition(self):
-        test_definition_field = create_field_entry("TestField", "string")
-        test_definition_name = "myDef"
-        test_definition = create_schema_definition(test_definition_name, fields=[test_definition_field])
+        test_schema_field = create_field_entry("TestField", "string")
+        test_schema_name = "myDef"
+        test_schema = create_schema_definition(test_schema_name, fields=[test_schema_field])
 
-        data_ext_field_name = "extField"
-        data_ext_field_type = "ExtField"
-        ext_field = create_field_entry(data_ext_field_name, data_ext_field_type)
-        test_definition_ext = create_schema_ext_definition("myDefExt", test_definition_name, fields=[ext_field], required=[data_ext_field_name])
+        schema_ext_field_name = "extField"
+        schema_ext_field_type = "ExtField"
+        ext_field = create_field_entry(schema_ext_field_name, schema_ext_field_type)
+        test_schema_ext = create_schema_ext_definition("mySchemaExt", test_schema_name, fields=[ext_field], required=[schema_ext_field_name])
 
         enum_val1 = "val1"
         enum_val2 = "val2"
@@ -33,36 +33,36 @@ class TestDefinitionExtensions(TestCase):
         test_enum_ext = create_enum_ext_definition("myEnumExt", test_enum_name, values=[test_enum_ext_value])
 
         language_context = get_initialized_language_context(core_spec_only=True)
-        language_context.add_definitions_to_context([test_definition, test_enum])
+        language_context.add_definitions_to_context([test_schema, test_enum])
 
-        self.assertIn(test_definition, language_context.definitions)
+        self.assertIn(test_schema, language_context.definitions)
         self.assertIn(test_enum, language_context.definitions)
 
-        self.assertEqual(1, len(test_definition.structure["schema"]["fields"]))
-        self.assertNotIn(REQUIRED_FIELDS_VALIDATION_STRING, test_definition.structure["schema"]["validation"])
+        self.assertEqual(1, len(test_schema.structure["schema"]["fields"]))
+        self.assertNotIn(REQUIRED_FIELDS_VALIDATION_STRING, test_schema.structure["schema"]["validation"])
         self.assertEqual(2, len(test_enum.structure["enum"]["values"]))
 
-        apply_extension_to_definition(test_definition_ext, test_definition)
+        apply_extension_to_definition(test_schema_ext, test_schema)
         apply_extension_to_definition(test_enum_ext, test_enum)
 
         # Assert Altered Extension State
-        self.assertEqual(2, len(test_definition.structure["schema"]["fields"]))
-        self.assertIn(REQUIRED_FIELDS_VALIDATION_STRING, test_definition.structure["schema"]["validation"])
-        self.assertEqual(1, len(test_definition.structure["schema"]["validation"][REQUIRED_FIELDS_VALIDATION_STRING]["arguments"]))
-        self.assertIn(data_ext_field_name, test_definition.to_yaml())
-        self.assertIn(data_ext_field_type, test_definition.to_yaml())
+        self.assertEqual(2, len(test_schema.structure["schema"]["fields"]))
+        self.assertIn(REQUIRED_FIELDS_VALIDATION_STRING, test_schema.structure["schema"]["validation"])
+        self.assertEqual(1, len(test_schema.structure["schema"]["validation"][REQUIRED_FIELDS_VALIDATION_STRING]["arguments"]))
+        self.assertIn(schema_ext_field_name, test_schema.to_yaml())
+        self.assertIn(schema_ext_field_type, test_schema.to_yaml())
 
         self.assertEqual(3, len(test_enum.structure["enum"]["values"]))
         self.assertIn(test_enum_ext_value, test_enum.to_yaml())
 
-        remove_extension_from_definition(test_definition_ext, test_definition)
+        remove_extension_from_definition(test_schema_ext, test_schema)
         remove_extension_from_definition(test_enum_ext, test_enum)
 
         # Assert Removed Extension State
-        self.assertEqual(1, len(test_definition.structure["schema"]["fields"]))
-        self.assertNotIn(REQUIRED_FIELDS_VALIDATION_STRING, test_definition.structure["schema"]["validation"])
-        self.assertNotIn(data_ext_field_name, test_definition.to_yaml())
-        self.assertNotIn(data_ext_field_type, test_definition.to_yaml())
+        self.assertEqual(1, len(test_schema.structure["schema"]["fields"]))
+        self.assertNotIn(REQUIRED_FIELDS_VALIDATION_STRING, test_schema.structure["schema"]["validation"])
+        self.assertNotIn(schema_ext_field_name, test_schema.to_yaml())
+        self.assertNotIn(schema_ext_field_type, test_schema.to_yaml())
 
         self.assertEqual(2, len(test_enum.structure["enum"]["values"]))
         self.assertNotIn(test_enum_ext_value, test_enum.to_yaml())
