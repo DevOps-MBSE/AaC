2f2a526088141495c95ca2a31f93295c23407dcf
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-06-06 23:14:59 -0500

    Rename required_fields.yaml -> required_fields.aac

--- python/src/aac/plugins/validators/required_fields/__init__.py
+++ python/src/aac/plugins/validators/required_fields/__init__.py
@@ -6,7 +6,7 @@ from aac.plugins import hookimpl
 from aac.plugins.validators import ValidatorPlugin, get_validation_definition_from_plugin_definitions
 from aac.plugins.validators.required_fields._validate_required_fields import validate_required_fields
 
-PLUGIN_YAML_FILE = "required_fields.yaml"
+PLUGIN_YAML_FILE = "required_fields.aac"
 
 
 @hookimpl
