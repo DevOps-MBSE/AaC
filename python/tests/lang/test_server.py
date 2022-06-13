b82aee0bbe255613421e8e89de6eedb6dfca8637
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-06-12 19:17:39 -0500

    Minor updates

--- python/tests/lang/test_server.py
+++ python/tests/lang/test_server.py
@@ -1,12 +1,13 @@
 from pygls.lsp import methods
 from pygls.lsp.types import Hover, HoverParams, Position
+from pygls.lsp.types.language_features.completion import CompletionContext, CompletionParams, CompletionTriggerKind
 
 from tests.lang.base_lsp_test_case import BaseLspTestCase
 
 
 class TestLspServer(BaseLspTestCase):
     def test_handles_hover_request(self):
-        res: Hover = self.client.send_request(
+        res: Hover = self.send_request(
             methods.HOVER,
             HoverParams(
                 text_document={"uri": self.get_document("test.aac")},
