4718174594bbbe87e97de67ee5540cb0e28ae2ad
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-06-14 17:39:58 -0500

    Update BaseLspTestCase
    Add functions to allow creating, closing, reading, and writing virtual documents
    for use in LSP tests.

--- python/tests/lang/base_lsp_test_case.py
+++ python/tests/lang/base_lsp_test_case.py
@@ -1,16 +1,23 @@
 from typing import Optional
-from unittest import TestCase
+from attr import attrib, attrs
+from attr.validators import instance_of
 
 from pygls import uris
 from pygls.lsp import methods
 from pygls.lsp.types import ClientCapabilities, InitializeParams
-from pygls.lsp.types.basic_structures import Model
+from pygls.lsp.types.basic_structures import Position, TextDocumentIdentifier, TextDocumentItem, VersionedTextDocumentIdentifier
+from pygls.lsp.types.language_features.hover import HoverParams
+from pygls.lsp.types.workspace import DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams
 
-from tests.lang.lsp_test_client import LspTestClient, DEFAULT_TIMEOUT_IN_SECONDS
+from tests.base_test_case import BaseTestCase
+from tests.helpers.lsp.text_document import TextDocument
+from tests.lang.lsp_test_client import LspTestClient
 
 
-class BaseLspTestCase(TestCase):
+class BaseLspTestCase(BaseTestCase):
     """Base test case providing set up and tear down for LSP tests."""
+
+    document: Optional[TextDocument] = None
 
     def setUp(self) -> None:
         super().setUp()
@@ -19,42 +26,83 @@ class BaseLspTestCase(TestCase):
         self.client.start()
         res = self.client.send_request(
             methods.INITIALIZE,
-            InitializeParams(
-                process_id=12345, root_uri=self.get_document("root.aac"), capabilities=ClientCapabilities()
-            ),
+            InitializeParams(process_id=12345, root_uri=self.to_uri("root.aac"), capabilities=ClientCapabilities()),
         )
 
         self.assertIn("capabilities", res)
 
     def tearDown(self):
+        self.close_document()
         self.client.stop()
 
-    def get_document(self, file_name: str) -> Optional[str]:
-        """Return a virtual document URI."""
+    def create_document(self, file_name: str, content: str = "") -> TextDocument:
+        """
+        Create a virtual document with the provided contents.
+
+        Args:
+            file_name (str): The name of the file for the virtual document.
+            content (str): The contents to write to the virtual document.
+
+        Returns:
+            The virtual document
+        """
+        self.document = TextDocument(file_name=file_name, content=content)
+
+        uri = self.to_uri(file_name)
+        self.client.send_notification(
+            methods.TEXT_DOCUMENT_DID_OPEN,
+            DidOpenTextDocumentParams(
+                text_document=TextDocumentItem(uri=uri, language_id="aac", version=self.document.version, text=content)
+            )
+        )
+
+        return self.document
+
+    def close_document(self) -> None:
+        """Close the virtual document."""
+        if self.document:
+            self.client.send_notification(
+                methods.TEXT_DOCUMENT_DID_CLOSE,
+                DidCloseTextDocumentParams(text_document=TextDocumentIdentifier(uri=self.to_uri(self.document.file_name)))
+            )
+        else:
+            raise LspTestCaseError("Could not close virtual document because there is no document.")
+
+    def write_document(self, content: str) -> None:
+        """
+        Write the provided content to the virtual document.
+
+        Args:
+            content (str): The content to write to the virtual document.
+        """
+        if self.document:
+            self.document.version += 1
+            self.document.write(content)
+            self.client.send_notification(
+                methods.TEXT_DOCUMENT_DID_CHANGE,
+                DidChangeTextDocumentParams(
+                    text_document=VersionedTextDocumentIdentifier(
+                        uri=self.to_uri(self.document.file_name), version=self.document.version
+                    ),
+                    content_changes=[{"text": content}]
+                )
+            )
+        else:
+            raise LspTestCaseError("Could not write content to virtual document because there is no document.")
+
+    def read_document(self) -> str:
+        """Return the document text."""
+        if self.document:
+            return self.document.read()
+        raise LspTestCaseError("Could not read content from virtual document because there is no document.")
+
+    def to_uri(self, file_name: str) -> Optional[str]:
+        """Return file_name as a file URI."""
         return uris.from_fs_path(file_name)
 
-    def send_request(self, method: str, params: Optional[Model] = None, timeout: int = DEFAULT_TIMEOUT_IN_SECONDS):
-        """
-        Send an LSP request to the server via the LSP test client.
 
-        Args:
-            method (str): The LSP method to use for the request.
-            params (Model): The parameters to send with the request. (optional)
-            timeout (int): The timeout to use when waiting for a result. If not provided, DEFAULT_TIMEOUT_IN_SECONDS is used.
+@attrs(slots=True)
+class LspTestCaseError(RuntimeError):
+    """An error caused by an issue with the test case."""
 
-        Returns:
-            The LSP response for the sent request.
-        """
-        return self.client.send_request(method, params, timeout)
-
-    def send_notification(self, method: str):
-        """
-        Send an LSP notification to the server via the LSP test client.
-
-        Args:
-            method (str): The LSP method to use for the notification.
-
-        Returns:
-            The LSP response for the sent notification.
-        """
-        return self.client.send_notification(method)
+    message: str = attrib(validator=instance_of(str))
