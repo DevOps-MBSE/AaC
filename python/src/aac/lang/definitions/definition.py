62ef6a605c05359ff7b3592a83ef2bd6e0db0a83
Alex Monnet <60155541+Coffee2Bits@users.noreply.github.com>
2022-06-14 13:09:22 -0500

    RESTful API Support for CRUD operations on Definitions (#361)
    * Added RESTful API Support for CRUD operations on Definitions
    * Added a new CLI command to generate a JSON OpenAPI v3 specification file of the API.

--- python/src/aac/lang/definitions/definition.py
+++ python/src/aac/lang/definitions/definition.py
@@ -14,6 +14,7 @@ class Definition:
     Attributes:
         name (str): The name of the definition
         content (str): The original source textual representation of the definition.
+        source_uri (str): The URI for the document containing the definition.
         lexemes (list[Lexeme]): A list of lexemes for each item in the parsed definition.
         structure (dict): The dictionary representation of the definition.
     """
@@ -30,7 +31,7 @@ class Definition:
 
     def get_top_level_fields(self) -> dict[str, dict]:
         """
-        Return a dictionary of the top-level fields that are populated in the defintion where the key is the field name.
+        Return a dictionary of the top-level fields that are populated in the definition where the key is the field name.
 
         Schema/data definitions will return their top-level fields, including a "fields" field. Because schema/data
         is self-defining, it may be easy to confuse the intention of this function and assume that it will returns the
