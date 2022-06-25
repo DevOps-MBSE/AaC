18603835ba5357bba65c98e9d65f8b7d9a70fc3a
Alex Monnet <alexcmonnet@gmail.com>
2022-06-25 00:23:34 +0000

    Update definition attribute accessor

--- python/src/aac/plugins/rest_api/models/definition_model.py
+++ python/src/aac/plugins/rest_api/models/definition_model.py
@@ -16,12 +16,12 @@ class DefinitionModel(BaseModel):
 def to_definition_model(definition: Definition) -> DefinitionModel:
     """Return a DefinitionModel representation from a Definition object."""
     return DefinitionModel(
-        name=definition.name, content=definition.content, source_uri=definition.source_uri, structure=definition.structure
+        name=definition.name, content=definition.content, source_uri=definition.source.uri, structure=definition.structure
     )
 
 
 def to_definition_class(definition_model: DefinitionModel) -> Definition:
     """Return a Definition object from a DefinitionModel object."""
-    definition = Definition(name=definition_model.name, content="", source_uri="", structure=definition_model.structure)
+    definition = Definition(name=definition_model.name, content="", source="", structure=definition_model.structure)
     definition.content = definition.to_yaml()
     return definition
