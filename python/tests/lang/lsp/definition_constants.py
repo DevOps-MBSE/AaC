a13a4cbbec76f66dfff0ffabdf46a723c2aed28d
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2022-07-10 13:57:52 -0500

    Refactor definition_constants.py
    Use tests.helpers.parsed_definitions functions to create definition helpers
    instead of hard-coding strings. Additionally, update tests to account for these
    changes.

--- python/tests/lang/lsp/definition_constants.py
+++ python/tests/lang/lsp/definition_constants.py
@@ -1,112 +1,92 @@
-TEST_DOCUMENT_NAME = "test.aac"
-TEST_DOCUMENT_MODEL_NAME = "ServiceOne"
-TEST_DOCUMENT_SCHEMA_NAME = "DataA"
-TEST_ADDITIONAL_SCHEMA_NAME = "DataC"
-TEST_ADDITIONAL_MODEL_NAME = "ServiceTwo"
-TEST_ENUM_NAME = "Options"
+from aac.parser._parse_source import YAML_DOCUMENT_SEPARATOR
 
-TEST_DOCUMENT_CONTENT = f"""
-schema:
-  name: {TEST_DOCUMENT_SCHEMA_NAME}
-  fields:
-  - name: msg
-    type: string
----
-schema:
-  name: DataB
-  fields:
-  - name: msg
-    type: string
----
-model:
-  name: {TEST_DOCUMENT_MODEL_NAME}
-  behavior:
-    - name: Process DataA Request
-      type: request-response
-      description: Process a DataA request and return a DataB response
-      input:
-        - name: in
-          type: {TEST_DOCUMENT_SCHEMA_NAME}
-      output:
-        - name: out
-          type: DataB
-      acceptance:
-        - scenario: Receive DataA request and return DataB response
-          given:
-            - ServiceOne is running
-          when:
-            - ServiceOne receives a DataA request
-          then:
-            - ServiceOne processes the request into a DataB response
-            - ServiceOne returns the DataB response
-        - scenario: Receive invalid request
-          given:
-            - ServiceOne is running
-          when:
-            - ServiceOne receives request that isn't a DataA request
-          then:
-            - ServiceOne returns an error response code
-"""
-TEST_DOCUMENT_ADDITIONAL_CONTENT = f"""
-schema:
-  name: {TEST_ADDITIONAL_SCHEMA_NAME}
-  fields:
-  - name: msg
-    type: string
----
-model:
-  name: {TEST_ADDITIONAL_MODEL_NAME}
-  behavior:
-    - name: Process DataB Request
-      type: request-response
-      description: Process a DataB request and return a DataC response
-      input:
-        - name: in
-          type: DataB
-      output:
-        - name: out
-          type: DataC
-      acceptance:
-        - scenario: Receive DataB request and return DataC response
-          given:
-            - ServiceTwo is running
-          when:
-            - ServiceTwo receives a DataB request
-          then:
-            - ServiceTwo processes the request into a DataC response
-            - ServiceTwo returns the DataC response
-"""
+from tests.helpers.parsed_definitions import (
+    create_enum_definition,
+    create_schema_definition,
+    create_field_entry,
+    create_model_definition,
+    create_behavior_entry,
+    create_scenario_entry,
+)
+
+TEST_ENUM = create_enum_definition("Options", ["one", "two", "three"])
+TEST_SCHEMA_A = create_schema_definition("DataA", fields=[create_field_entry("msg", "string")])
+TEST_SCHEMA_B = create_schema_definition("DataB", fields=[create_field_entry("msg", "string")])
+TEST_SCHEMA_C = create_schema_definition("DataC", fields=[create_field_entry("msg", "string")])
+
+TEST_PARTIAL_CONTENT_NAME = "Partial"
 TEST_PARTIAL_CONTENT = f"""
 schema:
-  name: {TEST_ADDITIONAL_SCHEMA_NAME}
+  name: {TEST_PARTIAL_CONTENT_NAME}
   fields:
   - name: msg
     type:\
 """
-TEST_ADDITIONAL_MODEL_CONTENT = f"""
-model:
-  name: ServiceThree
-  behavior:
-    - name: Pass through
-      type: request-response
-      input:
-        - name: in
-          type: {TEST_ADDITIONAL_SCHEMA_NAME}
-      output:
-        - name: out
-          type: {TEST_ADDITIONAL_SCHEMA_NAME}
-      acceptance:
-        - scenario: Pass the data through, untouched
-          when:
-            - ServiceThree receives a {TEST_ADDITIONAL_SCHEMA_NAME} request
-          then:
-            - ServiceThree returns the {TEST_ADDITIONAL_SCHEMA_NAME} data in the response, untouched
-"""
-TEST_ENUM_CONTENT = f"""
-enum:
-  name: {TEST_ENUM_NAME}
-  values:
-    - one
-    - two
-    - three
-"""
+
+TEST_SERVICE_ONE_NAME = "ServiceOne"
+TEST_SERVICE_ONE_BEHAVIOR = create_behavior_entry(
+    f"Process {TEST_SCHEMA_A.name} Request",
+    "request-response",
+    f"Process a {TEST_SCHEMA_A.name} request and return a {TEST_SCHEMA_B.name} response",
+    input=[create_field_entry("in", TEST_SCHEMA_A.name)],
+    output=[create_field_entry("out", TEST_SCHEMA_B.name)],
+    acceptance=[
+        create_scenario_entry(
+            f"Receive {TEST_SCHEMA_A.name} request and return {TEST_SCHEMA_B.name} response",
+            given=[f"{TEST_SERVICE_ONE_NAME} is running"],
+            when=[f"{TEST_SERVICE_ONE_NAME} receives a {TEST_SCHEMA_A.name} request"],
+            then=[
+                f"{TEST_SERVICE_ONE_NAME} processes the request into a {TEST_SCHEMA_B.name} response",
+                f"{TEST_SERVICE_ONE_NAME} returns the {TEST_SCHEMA_B.name} response",
+            ],
+        ),
+        create_scenario_entry(
+            "Receive invalid request",
+            given=[f"{TEST_SERVICE_ONE_NAME} is running"],
+            when=[f"{TEST_SERVICE_ONE_NAME} receives request that isn't a {TEST_SCHEMA_A.name} request"],
+            then=[f"{TEST_SERVICE_ONE_NAME} returns an error response code"],
+        ),
+    ]
+)
+TEST_SERVICE_ONE = create_model_definition(TEST_SERVICE_ONE_NAME, behavior=[TEST_SERVICE_ONE_BEHAVIOR])
+
+TEST_SERVICE_TWO_NAME = "ServiceTwo"
+TEST_SERVICE_TWO_BEHAVIOR = create_behavior_entry(
+    f"Process {TEST_SCHEMA_B.name} Request",
+    "request-response",
+    f"Process a {TEST_SCHEMA_B.name} request and return a DataC response",
+    input=[create_field_entry("in", TEST_SCHEMA_B.name)],
+    output=[create_field_entry("out", TEST_SCHEMA_C.name)],
+    acceptance=[
+        create_scenario_entry(
+            f"Receive {TEST_SCHEMA_B.name} request and return {TEST_SCHEMA_C.name} response",
+            given=[f"{TEST_SERVICE_TWO_NAME} is running"],
+            when=[f"{TEST_SERVICE_TWO_NAME} receives a {TEST_SCHEMA_B.name} request"],
+            then=[
+                f"{TEST_SERVICE_TWO_NAME} processes the request into a DataC response",
+                f"{TEST_SERVICE_TWO_NAME} returns the DataC response",
+            ],
+        )
+    ]
+)
+TEST_SERVICE_TWO = create_model_definition(TEST_SERVICE_TWO_NAME, behavior=[TEST_SERVICE_TWO_BEHAVIOR])
+
+TEST_SERVICE_THREE_NAME = "ServiceThree"
+TEST_SERVICE_THREE_BEHAVIOR = create_behavior_entry(
+    "Pass through",
+    "request-response",
+    f"Process a {TEST_SCHEMA_C.name} request and return a {TEST_SCHEMA_C.name} response",
+    input=[create_field_entry("in", TEST_SCHEMA_C.name)],
+    output=[create_field_entry("out", TEST_SCHEMA_C.name)],
+    acceptance=[
+        create_scenario_entry(
+            "Pass the data through, untouched",
+            when=[f"{TEST_SERVICE_THREE_NAME} receives a {TEST_SCHEMA_C.name} request"],
+            then=[f"{TEST_SERVICE_THREE_NAME} returns the {TEST_SCHEMA_C.name} data in the response, untouched"],
+        )
+    ]
+)
+TEST_SERVICE_THREE = create_model_definition(TEST_SERVICE_THREE_NAME, behavior=[TEST_SERVICE_THREE_BEHAVIOR])
+
+TEST_DOCUMENT_NAME = "test.aac"
+TEST_DOCUMENT_CONTENT = f"{YAML_DOCUMENT_SEPARATOR}\n".join([TEST_SCHEMA_A.to_yaml(), TEST_SCHEMA_B.to_yaml(), TEST_SERVICE_ONE.to_yaml()])
