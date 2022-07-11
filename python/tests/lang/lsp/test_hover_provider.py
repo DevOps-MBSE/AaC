a8e35ec6a89c773bc652aa166f0444e3ed61ce85
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-07-11 11:57:11 -0500

    Fix tests to account for changes to definition_constants.py

--- python/tests/lang/lsp/test_hover_provider.py
+++ python/tests/lang/lsp/test_hover_provider.py
@@ -3,11 +3,12 @@ from unittest.async_case import IsolatedAsyncioTestCase
 from pygls.lsp import methods
 from pygls.lsp.types.language_features.hover import HoverParams
 
-from aac.lang.active_context_lifecycle_manager import get_active_context
-
 from tests.helpers.lsp.responses.hover_response import HoverResponse
 from tests.lang.lsp.base_lsp_test_case import BaseLspTestCase
-from tests.lang.lsp.definition_constants import TEST_DOCUMENT_NAME
+from tests.lang.lsp.definition_constants import (
+    TEST_DOCUMENT_CONTENT,
+    TEST_DOCUMENT_NAME,
+)
 
 
 class TestHoverProvider(BaseLspTestCase, IsolatedAsyncioTestCase):
@@ -31,9 +32,10 @@ class TestHoverProvider(BaseLspTestCase, IsolatedAsyncioTestCase):
         )
 
     async def test_handles_hover_request(self):
-        res: HoverResponse = await self.hover(TEST_DOCUMENT_NAME, line=1)
-        self.assertEqual(res.get_content(), get_active_context().get_definition_by_name("schema").content)
+        res: HoverResponse = await self.hover(TEST_DOCUMENT_NAME)
+        self.assertEqual(res.get_content(), self.active_context.get_definition_by_name("schema").content)
 
     async def test_no_hover_when_nothing_under_cursor(self):
+        await self.write_document(TEST_DOCUMENT_NAME, f"\n{TEST_DOCUMENT_CONTENT}")
         res: HoverResponse = await self.hover(TEST_DOCUMENT_NAME)
         self.assertIsNone(res.get_content())
