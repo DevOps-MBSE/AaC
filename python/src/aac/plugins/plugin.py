96733a0aac9349b8967af46824c9c99440fd45eb
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-07-18 09:37:05 -0500

    Add Plugin metadata class

---
+++ python/src/aac/plugins/plugin.py
@@ -0,0 +1,25 @@
+"""Plugin metadata class."""
+
+from typing import Optional
+from attr import attrib, attrs, validators
+
+from aac.plugins.contribution_points import ContributionPoints
+
+
+@attrs(hash=False)
+class Plugin:
+    """
+    A class that contains information relevant to AaC Plugins.
+
+    Attributes:
+        name: A string with the name of the plugin.
+        contributions: A ContributionPoints object containing plugin contributions.
+    """
+
+    name: str = attrib(validator=validators.instance_of(str))
+    contributions: Optional[ContributionPoints] = attrib(
+        default=None, validator=validators.instance_of((ContributionPoints, type(None)))
+    )
+
+    def __hash__(self) -> int:
+        return hash(self.name)
