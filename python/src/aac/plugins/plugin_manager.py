4f909c02a2fc1bb6e8893d6bc76b10a554b66316
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-07-22 12:11:53 -0500

    Update plugin_manager.py

--- python/src/aac/plugins/plugin_manager.py
+++ python/src/aac/plugins/plugin_manager.py
@@ -1,9 +1,9 @@
 """Provide access to plugins and plugin data."""
 
+from importlib import import_module
 from iteration_utilities import flatten
 from pluggy import PluginManager
 
-from aac.io import parser
 from aac.lang.definitions.definition import Definition
 from aac.plugins import hookspecs, PLUGIN_PROJECT_NAME
 from aac.plugins.plugin import Plugin
@@ -17,71 +17,54 @@ def get_plugin_manager() -> PluginManager:
     Returns:
         The plugin manager.
     """
-    # Import built-in commands
-    from aac.cli.builtin_commands import (
-        validate,
-        version,
-    )
-
-    # Import plugins within the function to prevent circular imports and partial initialization
-    from aac.plugins import (
-        gen_json,
-        gen_plugin,
-        gen_protobuf,
-        gen_design_doc,
-        gen_gherkin_behaviors,
-        gen_plant_uml,
-        specifications,
-        print_spec,
-        start_lsp,
-        help_dump,
-        material_model,
-        rest_api,
-    )
-
-    # Import Validation Plugins
-    from aac.plugins.validators import (
-        defined_references,
-        required_fields,
-        validator_implementation,
-        exclusive_fields,
-        subcomponent_type,
-        root_keys,
-        reference_fields,
-        reference_targets,
-    )
-
     plugin_manager = PluginManager(PLUGIN_PROJECT_NAME)
     plugin_manager.add_hookspecs(hookspecs)
     plugin_manager.load_setuptools_entrypoints(PLUGIN_PROJECT_NAME)
 
     # register "built-in" plugins
-    plugin_manager.register(gen_json)
-    plugin_manager.register(gen_plugin)
-    plugin_manager.register(gen_protobuf)
-    plugin_manager.register(gen_design_doc)
-    plugin_manager.register(gen_gherkin_behaviors)
-    plugin_manager.register(gen_plant_uml)
-    plugin_manager.register(specifications)
-    plugin_manager.register(print_spec)
-    plugin_manager.register(start_lsp)
-    plugin_manager.register(material_model)
-    plugin_manager.register(help_dump)
-    plugin_manager.register(rest_api)
+    first_party_plugins_package = "aac.plugins"
+    first_party_plugins = [
+        "gen_json",
+        "gen_plugin",
+        "gen_protobuf",
+        "gen_design_doc",
+        "gen_gherkin_behaviors",
+        "gen_plant_uml",
+        "specifications",
+        "print_spec",
+        "start_lsp",
+        "material_model",
+        "help_dump",
+        "rest_api",
+    ]
 
     # register "built-in" commands
-    plugin_manager.register(validate)
-    plugin_manager.register(version)
+    builtin_command_plugins_package = "aac.cli.builtin_commands"
+    builtin_command_plugins = [
+        "validate",
+        "version",
+    ]
 
     # register "built-in" validation plugins
-    plugin_manager.register(defined_references)
-    plugin_manager.register(required_fields)
-    plugin_manager.register(validator_implementation)
-    plugin_manager.register(exclusive_fields)
-    plugin_manager.register(subcomponent_type)
-    plugin_manager.register(root_keys)
-    plugin_manager.register(reference_fields)
-    plugin_manager.register(reference_targets)
+    validator_plugins_package = "aac.plugins.validators"
+    validator_plugins = [
+        "defined_references",
+        "required_fields",
+        "validator_implementation",
+        "exclusive_fields",
+        "subcomponent_type",
+        "root_keys",
+        "reference_fields",
+        "reference_targets",
+    ]
+
+    def register_plugin(plugin, package):
+        plugin_module = import_module(f"{package}.{plugin}")
+        plugin_manager.register(plugin_module)
+
+    [register_plugin(name, first_party_plugins_package) for name in first_party_plugins]
+    [register_plugin(name, builtin_command_plugins_package) for name in builtin_command_plugins]
+    [register_plugin(name, validator_plugins_package) for name in validator_plugins]
 
     return plugin_manager
 
@@ -93,20 +76,13 @@ def get_plugin_definitions() -> list[Definition]:
     Returns:
         A list of parsed definitions from all active plugins.
     """
+
     def set_files_to_not_user_editable(definition):
         definition.source.is_user_editable = False
         return definition
 
-    plugin_manager = get_plugin_manager()
-    plugin_definitions_as_yaml = plugin_manager.hook.get_plugin_aac_definitions()
-    plugin_definitions = []
-    for plugin_definition in plugin_definitions_as_yaml:
-        if len(plugin_definition) > 0:
-            # Temporary setting until plugins can be updated to provide better contextual information
-            plugin_file_source_uri = "plugins.yaml"
-            plugin_definitions.append(parser.parse(plugin_definition, plugin_file_source_uri))
-
-    flattened_definition_list = list(flatten(plugin_definitions))
+    plugins = get_plugin_manager().hook.get_plugin()
+    flattened_definition_list = list(flatten(filter(lambda x: x, [plugin.get_definitions() for plugin in plugins])))
     return list(map(set_files_to_not_user_editable, flattened_definition_list))
 
 
@@ -118,25 +94,6 @@ def get_validator_plugins() -> list[ValidatorPlugin]:
         A list of validator plugins that are currently registered.
     """
     return get_plugin_manager().hook.register_validators()
-
-
-def get_plugin_model_definitions() -> dict:
-    """
-    Get a collection of all the plugin-defined AaC models and definitions.
-
-    Returns:
-        A collection of plugin defined models.
-    """
-    plugin_manager = get_plugin_manager()
-    plugin_models_yaml = plugin_manager.hook.get_plugin_aac_definitions()
-    plugin_extensions = {}
-    for plugin_ext in plugin_models_yaml:
-        if len(plugin_ext) > 0:
-            parsed_definitions = parser.parse(plugin_ext)
-            definitions_dict = dict(map(lambda definition: (definition.name, definition.structure), parsed_definitions))
-            plugin_extensions = definitions_dict | plugin_extensions
-
-    return plugin_extensions
 
 
 def get_plugins() -> list[Plugin]:
