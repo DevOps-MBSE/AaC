424ccd96bed96baac4e9a2897a42326a103b8dc1
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-09-01 17:21:41 -0500

    Use ValidatorFindings in a ValidationResult

--- python/src/aac/validate/_validation_result.py
+++ python/src/aac/validate/_validation_result.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 from attr import attrib, attrs, validators, Factory
 
-from aac.plugins.validators._validator_finding import Severity
+from aac.plugins.validators._validator_findings import ValidatorFindings
 
 
 @attrs(slots=True, auto_attribs=True)
@@ -12,20 +12,19 @@ class ValidationResult:
     """Represents the result of the validation of a single definition.
 
     Attributes:
-        definitions (list): Definitions that were part of the validated content
-        messages (dict[Severity, list]): A dictionary of messages of different severities.
+        findings (ValidatorFindings): A collection of findings that resulted from running validation
+                                      on the Definition.
         is_valid (Callable): A function that accepts a ValidationResult and returns True if it
                              should be considered valid or False if it should be considered invalid.
     """
 
-    def _is_valid(self):
+    def _is_valid_if_no_errors(self):
         """
         Return True if there are no error messages on the validation result; False, otherwise.
 
         This is a default implementation for determining if a ValidationResult is valid, or not.
         """
-        return len(self.messages.get(Severity.ERROR, [])) == 0
+        return len(self.findings.get_error_findings()) == 0
 
-    definitions: list = attrib(validator=validators.instance_of(list))
-    messages: dict[Severity, list[str]] = attrib(default=Factory(dict), validator=validators.instance_of(dict))
-    is_valid: Callable = attrib(default=_is_valid, validator=validators.is_callable())
+    findings: ValidatorFindings = attrib(default=Factory(list), validator=validators.instance_of(list))
+    is_valid: Callable = attrib(default=_is_valid_if_no_errors, validator=validators.is_callable())
