141dd240cdeced35b063a4846ed7569c91dd7b92
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-09-04 12:11:55 -0500

    Add the finding location to the validator finding

--- python/src/aac/plugins/validators/_validator_finding.py
+++ python/src/aac/plugins/validators/_validator_finding.py
@@ -8,6 +8,8 @@ is.
 """
 
 from enum import Enum, auto
+from aac.io.files.aac_file import AaCFile
+from aac.lang.definitions.source_location import SourceLocation
 
 from attr import attrib, attrs, validators
 
@@ -22,6 +24,22 @@ class FindingSeverity(Enum):
 
 
 @attrs(slots=True)
+class FindingLocation:
+    """
+    A representation of the location of a validator finding.
+
+    Attributes:
+        validation_name (str): The name of the validator plugin that made the finding.
+        source_file (AaCFile): The file in which the finding occurred.
+        location (SourceLocation): The source location of the finding.
+    """
+
+    validation_name: str = attrib(validator=validators.instance_of(str))
+    source_file: AaCFile = attrib(validator=validators.instance_of(AaCFile))
+    location: SourceLocation = attrib(validator=validators.instance_of(SourceLocation))
+
+
+@attrs(slots=True)
 class ValidatorFinding:
     """
     A finding made in a validator plugin.
@@ -30,8 +48,11 @@ class ValidatorFinding:
         definition (Definition): The definition on which the finding was made.
         severity (FindingSeverity): The severity of the finding.
         message (str): A message for the user to know how to address the finding, if needed.
+        location (FindingLocation): An object containing information about the location of the
+                                    finding.
     """
 
     definition: Definition = attrib(validator=validators.instance_of(Definition))
     severity: FindingSeverity = attrib(validator=validators.instance_of(FindingSeverity))
     message: str = attrib(validator=validators.instance_of(str))
+    location: (FindingLocation) = attrib(validator=validators.instance_of(FindingLocation))
