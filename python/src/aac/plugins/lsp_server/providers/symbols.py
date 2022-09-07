3f28be121320eaa93806bb314e3b31d52a36f529
Alex Monnet <alexcmonnet@gmail.com>
2022-09-07 04:35:09 +0000

    Added additional provider functionality

--- python/src/aac/plugins/lsp_server/providers/symbols.py
+++ python/src/aac/plugins/lsp_server/providers/symbols.py
@@ -1,8 +1,44 @@
 """Module contain common functionality shared by the various LSP providers."""
-import logging
+from enum import Enum
+
+from aac.lang.language_context import LanguageContext
 
 
-def get_symbol_at_position(content: str, offset: int) -> str:
+class SymbolType(Enum):
+    """Enums classifying the symbol type. Leveraged by providers to tailor responses."""
+    DEFINITION_NAME = 1
+    ENUM_VALUE_TYPE = 2
+    ROOT_KEY_NAME = 3
+
+
+def get_symbol_type(name: str, language_context: LanguageContext) -> list[SymbolType]:
+    """
+    Return a list of enums indicating what kind of symbol this may be based on the language context.
+
+    Args:
+        name (str): The name or symbol to lookup
+        language_context (LanguageContext): The language context to search for context for the symbol.
+
+    Returns:
+        A list of enums indicating a relationship between the name and content in the language context.
+    """
+
+    symbol_types = []
+
+    if language_context.is_definition_type(name):
+        symbol_types.append(SymbolType.DEFINITION_NAME)
+
+    enum_values_in_context = [value for values in [definition.get_top_level_fields().get("values") for definition in language_context.get_definitions_by_root_key("enum")] for value in values]
+    if name in enum_values_in_context:
+        symbol_types.append(SymbolType.ENUM_VALUE_TYPE)
+
+    if name in language_context.get_root_keys():
+        symbol_types.append(SymbolType.ROOT_KEY_NAME)
+
+    return symbol_types
+
+
+def get_symbol_at_position(content: str, line: int, column: int) -> str:
     """
     Return the word at or adjacent to the offset location.
 
@@ -15,29 +51,30 @@ def get_symbol_at_position(content: str, offset: int) -> str:
         A list of Locations at which the item at `position` is defined. If there is nothing
         found at the specified position, an empty list is returned.
     """
-    at_end_of_file = offset >= len(content) - 1
-    at_beginning_of_file = offset == 0
-    on_symbol = not content[:offset].isspace()
-
-    is_offset_preceded_by_a_space = (offset > 0 and content[offset - 1] == " ")
-    at_beginning_of_symbol = (at_beginning_of_file or is_offset_preceded_by_a_space) and not at_end_of_file
-
-    is_offset_a_space = (offset < len(content) and content[offset] == " ")
-    at_end_of_symbol = (at_end_of_file or is_offset_a_space) and not at_beginning_of_file
+    content_lines = content.splitlines()
+    line_with_symbol = content_lines[line]
+    adjusted_column = column if column < len(line_with_symbol) else column - 1
+    on_symbol = not line_with_symbol[adjusted_column].isspace()
 
     symbol = ""
     if on_symbol:
+
         symbol_start = 0
-        symbol_end = len(content) - 1
-        try:
-            symbol_start = content.rindex(" ", 0, offset)
-            symbol_end = content.index(" ", offset, len(content))
-        except Exception as error:
-            logging.info(f"Soft-error when attempting to locate symbol for LSP provider request: {error}")
+        # Reversed range from the adjusted column to the 0 element in the line
+        for i in range(adjusted_column, -1, -1):
+            if line_with_symbol[i].isspace():
+                break
 
-        start_index = offset if at_beginning_of_symbol else symbol_start
-        end_index = offset if at_end_of_symbol else symbol_end
+            symbol_start = i
 
-        symbol = content[start_index:end_index].strip()
+        symbol_end = len(line_with_symbol)
+        for i in range(adjusted_column, len(line_with_symbol)):
+            if line_with_symbol[i].isspace():
+                break
+
+            symbol_end = i
+
+        # Have to add 1 to the end index since slice ends are exclusive.
+        symbol = line_with_symbol[symbol_start:symbol_end + 1].strip()
 
     return symbol
