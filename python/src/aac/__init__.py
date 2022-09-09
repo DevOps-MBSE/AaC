b44cba0ab0a35ceb46152d136ffa1f7f3cf3ea63
Alex Monnet <alexcmonnet@gmail.com>
2022-09-09 20:07:16 +0000

    Another debugging attempt

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -10,14 +10,15 @@ if sys.version_info < (3, 9):
     exit(1)
 
 import logging
-import os
+# import os noqa E800
 
 __version__ = "0.1.2"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
-    filename=os.path.join(os.path.dirname(__file__), "aac.log"),
+    # filename=os.path.join(os.path.dirname(__file__), "aac.log"),  noqa E800
     encoding="utf-8",
     level=logging.DEBUG,
     datefmt="%m/%d/%Y %H:%M:%S",
+    stream=sys.stdout
 )
--- python/src/aac/io/parser/_parse_source.py
+++ python/src/aac/io/parser/_parse_source.py
@@ -40,11 +40,6 @@ def parse(source: str, source_uri: Optional[str] = None) -> list[Definition]:
         if path.lexists(sanitized_source):
             is_file = True
 
-    if is_file:
-        print(f"Is file: {sanitized_source}")
-    else:
-        print(f"Is not a file: {source}")
-
     return _parse_file(sanitized_source) if is_file else _parse_str(source_uri or DEFAULT_SOURCE_URI, source)
 
 
