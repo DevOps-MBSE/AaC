47378dfcd50f0a0c05fba45bf6c28984412d38e3
Alex Monnet <alexcmonnet@gmail.com>
2022-09-14 20:35:09 +0000

    Revert logging stream change

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -10,15 +10,14 @@ if sys.version_info < (3, 9):
     exit(1)
 
 import logging
-# import os noqa E800
+import os
 
 __version__ = "0.1.2"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
-    # filename=os.path.join(os.path.dirname(__file__), "aac.log"),  noqa E800
+    filename=os.path.join(os.path.dirname(__file__), "aac.log"),
     encoding="utf-8",
     level=logging.DEBUG,
-    datefmt="%m/%d/%Y %H:%M:%S",
-    stream=sys.stdout
+    datefmt="%m/%d/%Y %H:%M:%S"
 )
