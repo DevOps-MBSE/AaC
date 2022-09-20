3c97aea699ab8be74c6aff3dc2877a6c929f3a20
Alex Monnet <60155541+Coffee2Bits@users.noreply.github.com>
2022-09-20 12:51:27 -0500

    LSP Find References (#426)
    * Added LSP Find References functionality
    * Plugged in the LSP functionality to the Language Context instead of relying on the IDE's LSP client for accessible documents
    * Updated the parse to preserve lexeme line count for the whole document, not just the section containing the definition

--- python/src/aac/io/parser/_parse_source.py
+++ python/src/aac/io/parser/_parse_source.py
@@ -5,18 +5,19 @@ the caller with a dictionary of the content keyed by the named type.  This allow
 to find a certain type in a model by just looking for that key.
 """
 
-from os import path
+from os import path, linesep
 from typing import Optional
 from yaml.parser import ParserError as YAMLParserError
+import logging
 import yaml
 
-from aac.io.files.aac_file import AaCFile
 from aac.io.constants import YAML_DOCUMENT_SEPARATOR, DEFAULT_SOURCE_URI
+from aac.io.files.aac_file import AaCFile
+from aac.io.parser._parser_error import ParserError
 from aac.io.paths import sanitize_filesystem_path
 from aac.lang.definitions.definition import Definition
 from aac.lang.definitions.lexeme import Lexeme
 from aac.lang.definitions.source_location import SourceLocation
-from aac.io.parser._parser_error import ParserError
 
 
 def parse(source: str, source_uri: Optional[str] = None) -> list[Definition]:
@@ -30,12 +31,16 @@ def parse(source: str, source_uri: Optional[str] = None) -> list[Definition]:
         A list of Definition objects containing the internal representation of the definition and metadata
         associated with the definition.
     """
-    sanitized_source = sanitize_filesystem_path(source)
-    return (
-        _parse_file(sanitized_source)
-        if path.lexists(sanitized_source)
-        else _parse_str(sanitize_filesystem_path(source_uri or DEFAULT_SOURCE_URI), source)
-    )
+    # Linesep provides a quick and cheap filter for filepaths. A valid filepath won't have newlines.
+    sanitized_source = source
+    is_file = False
+
+    if linesep not in source:
+        sanitized_source = sanitize_filesystem_path(source)
+        if path.lexists(sanitized_source):
+            is_file = True
+
+    return _parse_file(sanitized_source) if is_file else _parse_str(source_uri or DEFAULT_SOURCE_URI, source)
 
 
 def _parse_file(arch_file: str) -> list[Definition]:
@@ -69,40 +74,60 @@ def _parse_str(source: str, model_content: str) -> list[Definition]:
     def mark_to_source_location(start: yaml.error.Mark, end: yaml.error.Mark) -> SourceLocation:
         return SourceLocation(start.line, start.column, start.index, end.column - start.column)
 
-    def get_lexemes_for_definition(contents):
-        yaml_source = path.abspath(source) if path.lexists(source) else DEFAULT_SOURCE_URI
-        lexemes = []
-        tokens = [token for token in yaml.scan(contents, Loader=yaml.SafeLoader) if hasattr(token, "value")]
-        for token in tokens:
+    def get_lexemes_for_definition(value_tokens, content_start, content_end) -> list[Lexeme]:
+        definition_tokens = [token for token in value_tokens if is_token_between_locations(token, content_start, content_end)]
+        definition_lexemes = []
+        for token in definition_tokens:
             location = mark_to_source_location(token.start_mark, token.end_mark)
-            lexemes.append(Lexeme(location, yaml_source, token.value))
-        return lexemes
+            definition_lexemes.append(Lexeme(location, source, token.value))
+        return definition_lexemes
+
+    def is_token_between_locations(token, inclusive_line_start: int, inclusive_line_end: int) -> list[Lexeme]:
+        return token.start_mark.line >= inclusive_line_start and token.end_mark.line <= inclusive_line_end
+
+    yaml_tokens = list(yaml.scan(model_content, Loader=yaml.SafeLoader))
+    doc_start_token = [token for token in yaml_tokens if isinstance(token, yaml.tokens.StreamStartToken)]
+    doc_end_token = [token for token in yaml_tokens if isinstance(token, yaml.tokens.StreamEndToken)]
+    doc_segment_tokens = [token for token in yaml_tokens if isinstance(token, yaml.tokens.DocumentStartToken)]
+    value_tokens = [token for token in yaml_tokens if hasattr(token, "value")]
+    doc_tokens = [*doc_start_token, *doc_segment_tokens, *doc_end_token]
 
     source_files: dict[str, AaCFile] = {}
     definitions: list[Definition] = []
-    for doc in model_content.split(YAML_DOCUMENT_SEPARATOR):
-        for root in _parse_yaml(source, doc):
-            if "import" in root:
-                del root["import"]
+    for doc_token_index in range(0, len(doc_tokens) - 1):
+        start_doc_token = doc_tokens[doc_token_index]
+        end_doc_token = doc_tokens[doc_token_index + 1]
 
-            root_type, *_ = root.keys()
-            root_name = root.get(root_type).get("name")
-            contents = _add_yaml_document_separator(doc) if _has_document_separator(model_content, doc) else doc
-            lexemes = get_lexemes_for_definition(contents)
+        content_start_line = start_doc_token.start_mark.line
+        content_end_line = end_doc_token.end_mark.line
+
+        end_of_file_offset = (1 if isinstance(end_doc_token, yaml.tokens.StreamEndToken) else 0)
+
+        yaml_text = linesep.join(model_content.splitlines()[content_start_line:content_end_line + end_of_file_offset])
+        yaml_text += linesep
+
+        if yaml_text.strip():
+            definition_lexemes = get_lexemes_for_definition(value_tokens, content_start_line, content_end_line)
+            root_yaml, *_ = _parse_yaml(source, yaml_text)
+
+            if "import" in root_yaml:
+                del root_yaml["import"]
+
+            root_type, *_ = root_yaml.keys()
+            definition_name = root_yaml.get(root_type, {}).get("name")
             source_file = source_files.get(source)
 
             if not source_file:
                 source_file = AaCFile(source, True, False)
                 source_files[source] = source_file
 
-            definitions.append(Definition(root_name, contents, source_file, lexemes, root))
+            definitions.append(Definition(definition_name, yaml_text, source_file, definition_lexemes, root_yaml))
+        else:
+            logging.info(f"Skipping empty content between {start_doc_token}:L{content_start_line} and {end_doc_token}:L{content_end_line} in source {source}")
+            logging.debug(f"Source: {source} Content:{model_content}")
+            logging.debug(f"Content lines:{model_content.splitlines()}")
 
     return definitions
-
-
-def _has_document_separator(model_content: str, document: str) -> bool:
-    before, _, _ = model_content.partition(document)
-    return before.endswith(YAML_DOCUMENT_SEPARATOR)
 
 
 def _parse_yaml(source: str, content: str) -> list[dict]:
@@ -125,10 +150,10 @@ def _parse_yaml(source: str, content: str) -> list[dict]:
         _error_if_not_yaml(source, content, models)
         _error_if_not_complete(source, content, models)
         return models
-    except Exception as error:
-        raise ParserError(source, [f"Failed to parse file, invalid YAML {error}", content])
     except YAMLParserError as error:
         raise ParserError(source, [f"Failed to parse file, invalid YAML {error.context} {error.problem}", content])
+    except Exception as error:
+        raise ParserError(source, [f"Failed to parse file, invalid YAML {error}", content])
 
 
 def _error_if_not_yaml(source, content, models):
