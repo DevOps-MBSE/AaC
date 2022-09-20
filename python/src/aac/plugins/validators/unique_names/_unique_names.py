fbcacced2e6ab5d86e69d89b49bb706e083ff2ef
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-09-19 23:50:44 -0500

    Simplify

--- python/src/aac/plugins/validators/unique_names/_unique_names.py
+++ python/src/aac/plugins/validators/unique_names/_unique_names.py
@@ -14,8 +14,7 @@ def validate_unique_names(
     """Search through the language context ensuring that all definition names are unique."""
     findings = ValidatorFindings()
 
-    all_definition_names = {d.name for d in language_context.definitions}
-    if definition_under_test.name in all_definition_names:
+    if definition_under_test.name in language_context.get_defined_types():
         duplicate_name_message = f"Definition name {definition_under_test.name} was duplicated in the language context."
         findings.add_error_finding(definition_under_test, duplicate_name_message, PLUGIN_NAME, 0, 0, 0, 0)
         logging.debug(duplicate_name_message)
