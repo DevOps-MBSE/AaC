e298157a7149c93c5cf0160d60d79c520cfbdcd0
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-09-19 23:30:51 -0500

    Add unique name validator plugin

--- python/src/aac/plugins/validators/unique_names/_unique_names.py
+++ python/src/aac/plugins/validators/unique_names/_unique_names.py
@@ -1,3 +1,5 @@
+import logging
+
 from aac.lang.definitions.definition import Definition
 from aac.lang.language_context import LanguageContext
 from aac.plugins.validators._validator_result import ValidatorFindings, ValidatorResult
@@ -6,6 +8,16 @@ from aac.plugins.validators._validator_result import ValidatorFindings, Validato
 PLUGIN_NAME = "Unique definition names"
 
 
-def validate_unique_names(definition_under_test: Definition, target_schema_definition: Definition, language_context: LanguageContext, *validation_args) -> ValidatorResult:
+def validate_unique_names(
+    definition_under_test: Definition, target_schema_definition: Definition, language_context: LanguageContext, *validation_args
+) -> ValidatorResult:
+    """Search through the language context ensuring that all definition names are unique."""
     findings = ValidatorFindings()
+
+    all_definition_names = {d.name for d in language_context.definitions}
+    if definition_under_test.name in all_definition_names:
+        duplicate_name_message = f"Definition name {definition_under_test.name} was duplicated in the language context."
+        findings.add_error_finding(definition_under_test, duplicate_name_message, PLUGIN_NAME, 0, 0, 0, 0)
+        logging.debug(duplicate_name_message)
+
     return ValidatorResult(definition_under_test, findings)
---
+++ python/tests/plugins/validators/test__unique_names.py
@@ -0,0 +1,48 @@
+from aac.lang.active_context_lifecycle_manager import get_active_context
+from aac.plugins.validators.unique_names._unique_names import validate_unique_names
+
+from tests.active_context_test_case import ActiveContextTestCase
+from tests.helpers.assertion import assert_validator_result_failure, assert_validator_result_success
+from tests.helpers.parsed_definitions import create_enum_definition, create_schema_definition
+
+
+class TestValidateUniqueNames(ActiveContextTestCase):
+    def test_validation_succeeds_with_no_definitions(self):
+        new_definition = create_schema_definition("New")
+        actual_result = validate_unique_names(new_definition, new_definition, get_active_context())
+
+        assert_validator_result_success(actual_result)
+
+    def test_validation_succeeds_with_no_duplicated_names(self):
+        test_active_context = get_active_context()
+        test_active_context.add_definitions_to_context([create_schema_definition(f"Test{i}") for i in range(1, 100)])
+
+        new_definition = create_schema_definition("Test0")
+        unused_definition = create_schema_definition("unused")
+        actual_result = validate_unique_names(new_definition, unused_definition, test_active_context)
+
+        assert_validator_result_success(actual_result)
+
+    def test_validation_fails_with_duplicated_names_of_same_type(self):
+        test_active_context = get_active_context()
+
+        definition = create_schema_definition("Test")
+        test_active_context.add_definition_to_context(definition)
+
+        new_definition = create_schema_definition("Test")
+        unused_definition = create_schema_definition("unused")
+        actual_result = validate_unique_names(new_definition, unused_definition, test_active_context)
+
+        assert_validator_result_failure(actual_result)
+
+    def test_validation_succeeds_with_no_duplicated_names_of_different_types(self):
+        test_active_context = get_active_context()
+
+        definition = create_schema_definition("Test")
+        test_active_context.add_definition_to_context(definition)
+
+        new_definition = create_enum_definition("Test", ["a", "b", "c"])
+        unused_definition = create_schema_definition("unused")
+        actual_result = validate_unique_names(new_definition, unused_definition, test_active_context)
+
+        assert_validator_result_failure(actual_result)
