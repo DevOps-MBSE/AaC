8341f88c46765359f8629061f57622cf80089ee3
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-09-26 23:16:14 -0500

    Fix test

--- python/tests/plugins/validators/test__unique_names.py
+++ python/tests/plugins/validators/test__unique_names.py
@@ -1,3 +1,4 @@
+from aac.io.parser import parse
 from aac.lang.active_context_lifecycle_manager import get_active_context
 from aac.plugins.validators.unique_names._unique_names import validate_unique_names
 
@@ -37,11 +38,9 @@ class TestValidateUniqueNames(ActiveContextTestCase):
 
     def test_validation_fails_with_duplicated_names_of_same_type(self):
         test_active_context = get_active_context()
+        test_active_context.add_definitions_to_context(parse(TEST_CONTENT, __file__))
 
-        definition = create_schema_definition("Test")
-        test_active_context.add_definition_to_context(definition)
-
-        new_definition = create_schema_definition("Test")
+        new_definition = create_schema_definition("Test1")
         unused_definition = create_schema_definition("unused")
         actual_result = validate_unique_names(new_definition, unused_definition, test_active_context)
 
@@ -67,3 +66,18 @@ class TestValidateUniqueNames(ActiveContextTestCase):
         actual_result = validate_unique_names(new_definition, unused_definition, test_active_context)
 
         assert_validator_result_failure(actual_result)
+
+
+TEST_CONTENT = """
+schema:
+  name: Test0
+  fields:
+    - name: a
+      type: string
+---
+schema:
+  name: Test1
+  fields:
+    - name: b
+      type: int
+"""
