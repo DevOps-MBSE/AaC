b3aa005590d09195b9565fbc43c39e7df827a8fe
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-09-26 23:14:18 -0500

    Update _unique_names.py
    Update how we determine if two definitions are the same.
    
    Update how we determine the position of a duplicate definition.

--- python/src/aac/plugins/validators/unique_names/_unique_names.py
+++ python/src/aac/plugins/validators/unique_names/_unique_names.py
@@ -1,7 +1,9 @@
 import logging
 from os import linesep
+from typing import Optional
 
 from aac.lang.definitions.definition import Definition
+from aac.lang.definitions.lexeme import Lexeme
 from aac.lang.language_context import LanguageContext
 from aac.plugins.validators._validator_result import ValidatorFindings, ValidatorResult
 
@@ -15,7 +17,9 @@ def validate_unique_names(
     """Search through the language context ensuring that all definition names are unique."""
 
     def is_duplicate_name(definition1: Definition, definition2: Definition) -> bool:
-        return definition1.name == definition2.name and definition1.uid != definition2.uid
+        lexeme1 = _get_lexeme_for_defined_name(definition1.name, definition1.lexemes)
+        lexeme2 = _get_lexeme_for_defined_name(definition2.name, definition2.lexemes)
+        return definition1.name == definition2.name and None not in [lexeme1, lexeme2] and lexeme1 != lexeme2
 
     findings = ValidatorFindings()
 
@@ -32,10 +36,22 @@ def validate_unique_names(
 
 
 def _build_duplicate_name_message(definition: Definition, definitions: list[Definition]) -> str:
-    def _get_position(other_definition: Definition) -> str:
-        lexeme, *_ = [lexeme for lexeme in other_definition.lexemes if lexeme.value == definition.name]
-        return f"{lexeme.location.line + 1}:{lexeme.location.column}"
-
     message = [f"Definition '{definition.name}' is defined in:"]
     message += [f"  {definition.source.uri} at {_get_position(definition)}" for definition in definitions]
     return linesep.join(message)
+
+
+def _get_position(definition: Definition) -> str:
+    lexeme = _get_lexeme_for_defined_name(definition.name, definition.lexemes)
+    return f"{lexeme.location.line + 1}:{lexeme.location.column}" if lexeme else "unknown location"
+
+
+def _get_lexeme_for_defined_name(name: str, definition_lexemes: list[Lexeme]) -> Optional[Lexeme]:
+    is_name_next = False
+    for lexeme in definition_lexemes:
+        if lexeme.value == "name":
+            is_name_next = True
+        elif lexeme.value == name and is_name_next:
+            return lexeme
+        else:
+            is_name_next = False
