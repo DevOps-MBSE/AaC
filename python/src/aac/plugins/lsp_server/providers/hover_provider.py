ef59724d0b6707cde2790e3864d88f62fbcd7eb3
Alex Monnet <60155541+Coffee2Bits@users.noreply.github.com>
2022-09-30 11:42:58 -0500

    LSP Rename Symbol (#440)
    * Added support for the LSP Rename feature for Definition Names
    * Added support for the LSP Prepare Rename feature
    * Fixed an issue where LSP-updated definitions didn't have their source file
    
    Co-authored-by: Cameron Chaparro <cvchaparro@users.noreply.github.com>

--- python/src/aac/plugins/lsp_server/providers/hover_provider.py
+++ python/src/aac/plugins/lsp_server/providers/hover_provider.py
@@ -2,8 +2,7 @@
 
 from typing import Optional
 
-from pygls.lsp.types.basic_structures import MarkupContent, MarkupKind
-from pygls.lsp.types.language_features.hover import Hover, HoverParams
+from pygls.lsp.types import MarkupContent, MarkupKind, Hover, HoverParams
 from pygls.server import LanguageServer
 from pygls.workspace import Document
 
@@ -15,13 +14,15 @@ from aac.plugins.lsp_server.providers.lsp_provider import LspProvider
 class HoverProvider(LspProvider):
     """Provide useful contextual information for the named item being hovered over."""
 
-    def handle_request(self, ls: LanguageServer, params: HoverParams) -> Optional[Hover]:
+    def handle_request(self, language_server: LanguageServer, params: HoverParams) -> Optional[Hover]:
         """Return the YAML representation of the item at the specified position."""
-        document: Optional[Document] = ls.workspace.documents.get(params.text_document.uri)
+        document: Optional[Document] = language_server.workspace.documents.get(params.text_document.uri)
         position = params.position
         symbol = get_symbol_at_position(document.source, position.line, position.character)
 
         if symbol is not None:
             name = remove_list_type_indicator(symbol).strip(":")
-            definition = ls.language_context.get_definition_by_name(name)
-            return definition and Hover(contents=MarkupContent(kind=MarkupKind.Markdown, value=f"```\n{definition.content}\n```"))
+            definition = language_server.language_context.get_definition_by_name(name)
+            return definition and Hover(
+                contents=MarkupContent(kind=MarkupKind.Markdown, value=f"```\n{definition.content}\n```")
+            )
