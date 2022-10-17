b6e1ce349db65207ffef599fe6eb209e3eb1e6ad
Charles Blackard <charles.blackard@zadentech.com>
2022-10-17 14:41:31 -0500

    Updated tests for rename of enums

--- python/tests/plugins/lsp_server/providers/test_prepare_rename_provider.py
+++ python/tests/plugins/lsp_server/providers/test_prepare_rename_provider.py
@@ -43,6 +43,18 @@ class TestPrepareRenameProvider(BaseLspTestCase, IsolatedAsyncioTestCase):
         self.assertEqual(actual_range.start.character, expected_range_character_start)
         self.assertEqual(actual_range.end.character, expected_range_character_end)
 
+    async def test_prepare_rename_enum_request(self):
+        expected_selection = "Options"
+        expected_range_line = 19
+        expected_range_character_start = 8
+        expected_range_character_end = expected_range_character_start + len(expected_selection)
+        res = await self.prepare_rename(TEST_DOCUMENT__WITH_ENUM_NAME, 19, 8)
+        actual_range: Range = res.get_range()
+
+        self.assertEqual(actual_range.start.line, expected_range_line)
+        self.assertEqual(actual_range.start.character, expected_range_character_start)
+        self.assertEqual(actual_range.end.character, expected_range_character_end)
+    
     async def test_no_prepare_rename_when_nothing_under_cursor(self):
         await self.write_document(TEST_DOCUMENT_NAME, f"\n{TEST_DOCUMENT_CONTENT}")
         res: PrepareRenameResponse = await self.prepare_rename(TEST_DOCUMENT_NAME)
--- python/tests/plugins/lsp_server/providers/test_rename_provider.py
+++ python/tests/plugins/lsp_server/providers/test_rename_provider.py
@@ -8,6 +8,8 @@ from tests.plugins.lsp_server.base_lsp_test_case import BaseLspTestCase
 from tests.plugins.lsp_server.definition_constants import (
     TEST_DOCUMENT_CONTENT,
     TEST_DOCUMENT_NAME,
+    TEST_DOCUMENT_WITH_ENUM_NAME,
+    TEST_DOCUMENT_WITH_ENUM_CONTENT, 
 )
 
 
@@ -43,10 +45,11 @@ class TestRenameProvider(BaseLspTestCase, IsolatedAsyncioTestCase):
 
     async def test_rename_enum_request(self):
         expected_new_enum = "OptionsNew"
-        res: RenameResponse = await self.rename(TEST_DOCUMENT_NAME, expected_new_enum, 19, 8)
+        await self.create_document(TEST_DOCUMENT_WITH_ENUM_NAME, f"\n{TEST_DOCUMENT_WITH_ENUM_CONTENT}")
+        res: RenameResponse = await self.rename(TEST_DOCUMENT_WITH_ENUM_NAME, expected_new_enum, 20, 8)
         actual_text_edits = res.get_all_text_edits()
         expected_test_document_edits = res.get_workspace_edit()
-        self.assertIn(TEST_DOCUMENT_NAME, list(expected_test_document_edits.keys())[0])
+        self.assertIn(TEST_DOCUMENT_WITH_ENUM_NAME, list(expected_test_document_edits.keys())[0])
         self.assertIn(expected_new_enum, actual_text_edits[0].get("newText"))
         self.assertEqual(1, len(actual_text_edits))
 
