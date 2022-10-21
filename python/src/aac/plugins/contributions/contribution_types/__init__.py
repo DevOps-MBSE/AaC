4e1ffb11a37ca0c8c5e587b28ce7b51e6f7d0adc
Alex Monnet <alexcmonnet@gmail.com>
2022-10-21 21:22:24 +0000

    updated new contribution point type file names

--- python/src/aac/plugins/contributions/contribution_types/__init__.py
+++ python/src/aac/plugins/contributions/contribution_types/__init__.py
@@ -1,6 +1,6 @@
 """Module for contribution-type classes."""
-from aac.plugins.contributions.contribution_types.rule_validation import DefinitionValidationContribution
-from aac.plugins.contributions.contribution_types.type_validation import PrimitiveValidationContribution
+from aac.plugins.contributions.contribution_types.definition_validation_contribution import DefinitionValidationContribution
+from aac.plugins.contributions.contribution_types.primitive_validation_contribution import PrimitiveValidationContribution
 from aac.plugins.contributions.contribution_types.contribution_type import ContributionType
 
 
