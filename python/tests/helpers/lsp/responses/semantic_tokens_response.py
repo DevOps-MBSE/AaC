59ddf2ebd7be2b21789d0f4a0e1d2c3c5452a86a
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-10-25 12:17:27 -0500

    Clarify what 5 has to do with semantic tokens

--- python/tests/helpers/lsp/responses/semantic_tokens_response.py
+++ python/tests/helpers/lsp/responses/semantic_tokens_response.py
@@ -1,10 +1,15 @@
 from tests.helpers.lsp.responses.lsp_response import LspResponse
 
 
+# Refer here to see why 5 is significant:
+#  https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_semanticTokens:~:text=relative%27%3B-,Integer%20Encoding%20for%20Tokens
+NUM_INTEGERS_TO_ENCODE_TOKEN = 5
+
+
 class SemanticTokensResponse(LspResponse):
     def get_default_data(self) -> list[int]:
         return self.response.get("data", [])
 
     def get_semantic_tokens(self) -> list[list[int]]:
         data = self.get_default_data()
-        return [data[i:i + 5] for i in range(0, len(data), 5)]
+        return [data[i:i + NUM_INTEGERS_TO_ENCODE_TOKEN] for i in range(0, len(data), NUM_INTEGERS_TO_ENCODE_TOKEN)]
