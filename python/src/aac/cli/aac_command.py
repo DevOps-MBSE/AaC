a94da02e9d61cd852c00dc32edfd45a3f4d55a96
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-10-27 11:33:41 -0500

    Update aac_command.py
    Update the AacCommandArgument and AacCommand classes to align better with click
    commands/options.

--- python/src/aac/cli/aac_command.py
+++ python/src/aac/cli/aac_command.py
@@ -1,49 +1,49 @@
 """A simple data class to collect AaC command info from plugins."""
+
+from typing import Any, Callable, Union
+
 from attr import Factory, attrib, attrs, validators
 
 
 @attrs
 class AacCommandArgument:
     """
-    A class used as a struct to hold a command argument name and description.
+    A class used as represent a command argument for an AaC command.
 
     Attributes:
-        name: A string with the name of the command argument
-        description: A string with the command argument description -- will provide a description about this argument when the
-                         help command is invoked.
-        data_type: The data type of the command argument -- will provide validation information for command arguments.
-        number_of_arguments: Number of entries that the argument can take. Maps to the argsparse module's nargs command.
-        choices: The set of allowable values for the argument. Maps to the argparse module's choices argument.
-        action: An action to take with the argument. Maps to the argparse module's action argument.
+        name: One or more strings with the name of the command argument.
+        description: A string with the command argument description.
+        data_type: The data type of the command argument.
+        number_of_arguments: The number of entries that the argument can take. (default: 1)
+        default: The default value for the current argument. (default: None)
     """
 
-    name = attrib(validator=validators.instance_of(str))
-    description = attrib(validator=validators.instance_of(str))
-    data_type = attrib(default="", validator=validators.instance_of(str))
-    number_of_arguments = attrib(default=None, validator=validators.instance_of((str, type(None))))
-    choices = attrib(default=None, validator=validators.instance_of((list, type(None))))
-    action = attrib(default=None, validator=validators.instance_of((str, type(None))))
+    name: Union[str, list[str]] = attrib(validator=validators.instance_of((list, str)))
+    description: str = attrib(validator=validators.instance_of(str))
+    data_type: str = attrib(validator=validators.instance_of(str))
+    number_of_arguments: int = attrib(default=1, validator=validators.instance_of(int))
+    default: Any = attrib(default=None)
 
 
 @attrs(hash=False)
 class AacCommand:
     """
-    A class used as a struct to hold a command name, command description, and callback.
+    A class used to represent a command in AaC.
 
-    The command name is added to the argument parser with help set to teh command description.
+    The command name is added to the argument parser with help set to the command description.
     The callback is invoked if the user specifies the command.
 
     Attributes:
-        name: A string with the name of the command_description
-        description: a string with the command description -- will be provided with the help command
-        callback: A Callable function that's executed when the user runs the AaC command
-        arguments: A List of AacCommandArgument containing argument information about the command. Defaults to an empty list.
+        name: A string with the name of the command_description.
+        description: A string with the command description -- will be used to build the help command.
+        callback: A function that's executed when the user runs the AaC command.
+        arguments: A list of AacCommandArgument containing argument information about the command. (default: [])
     """
 
-    name = attrib(validator=validators.instance_of(str))
-    description = attrib(validator=validators.instance_of(str))
-    callback = attrib(validator=validators.is_callable())
-    arguments = attrib(default=Factory(list), validator=validators.instance_of(list))
+    name: str = attrib(validator=validators.instance_of(str))
+    description: str = attrib(validator=validators.instance_of(str))
+    callback: Callable[..., Any] = attrib(validator=validators.is_callable())
+    arguments: list[AacCommandArgument] = attrib(default=Factory(list), validator=validators.instance_of(list))
 
     def __hash__(self) -> int:
         """Return the hash of this AacCommand."""
