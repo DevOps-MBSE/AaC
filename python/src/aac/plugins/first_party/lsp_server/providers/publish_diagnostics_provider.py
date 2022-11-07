a8d7bcf7b099ad61fa767e87cf6aec72f8dcff16
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-11-07 13:19:15 -0600

    Update PublishDiagnosticsProvider and it's tests

--- python/src/aac/plugins/first_party/lsp_server/providers/publish_diagnostics_provider.py
+++ python/src/aac/plugins/first_party/lsp_server/providers/publish_diagnostics_provider.py
@@ -3,43 +3,34 @@
 from aac.io.parser import parse
 from typing import Optional
 
-from pygls.lsp import Diagnostic, DiagnosticSeverity, Position, PublishDiagnosticsParams, Range
+from pygls.lsp import Diagnostic, DiagnosticSeverity, PublishDiagnosticsParams
 from pygls.server import LanguageServer
 from pygls.uris import to_fs_path
 
+from aac.plugins.first_party.lsp_server.conversion_helpers import source_location_to_range
 from aac.plugins.first_party.lsp_server.providers.lsp_provider import LspProvider
-from aac.plugins.validators import ValidatorResult
-from aac.plugins.validators._validator_finding import FindingSeverity
+from aac.plugins.validators._validator_finding import ValidatorFinding, FindingSeverity
 from aac.validate._validate import _validate_definitions
 
 
 class PublishDiagnosticsProvider(LspProvider):
     """Handler for Publishing Diagnostics for AaC LSP."""
 
-    async def handle_request(self, ls: LanguageServer, params: PublishDiagnosticsParams) -> list[Diagnostic]:
+    async def handle_request(self, _: LanguageServer, params: PublishDiagnosticsParams) -> list[Diagnostic]:
         """Handle publishing validation findings as diagnostics."""
-        diagnostics = []
-        validation_result: ValidatorResult = _validate_definitions(parse(to_fs_path(params.uri)), validate_context=True)
 
-        for finding in validation_result.findings.findings:
-            finding_location = finding.location.location
-
-            range_start = Position(line=finding_location.line, character=finding_location.position)
-            range_end = Position(line=finding_location.line, character=finding_location.position + finding_location.span)
-
+        def finding_to_diagnostic(finding: ValidatorFinding):
             severity = self.finding_severity_to_diagnostic_severity(finding.severity)
-
-            diagnostics.append(
-                Diagnostic(
-                    range=Range(start=range_start, end=range_end),
-                    severity=severity.value if severity else None,
-                    code=finding.location.validation_name,
-                    source="aac",
-                    message=finding.message,
-                )
+            return Diagnostic(
+                range=source_location_to_range(finding.location.location),
+                severity=severity.value if severity else None,
+                code=finding.location.validation_name,
+                source="aac",
+                message=finding.message,
             )
 
-        return diagnostics
+        result = _validate_definitions(parse(to_fs_path(params.uri)), validate_context=True)
+        return [finding_to_diagnostic(finding) for finding in result.findings.get_all_findings()]
 
     def finding_severity_to_diagnostic_severity(self, finding_severity: FindingSeverity) -> Optional[DiagnosticSeverity]:
         """Return the DiagnosticSeverity that corresponds most closely to the correpsonding FindingSeverity."""
--- python/tests/plugins/lsp_server/providers/test_publish_diagnostics_provider.py
+++ python/tests/plugins/lsp_server/providers/test_publish_diagnostics_provider.py
@@ -1,11 +1,15 @@
 from typing import Optional
-from pygls.lsp import Diagnostic, PublishDiagnosticsParams, methods
+from pygls.lsp import Diagnostic, DiagnosticSeverity, Position, PublishDiagnosticsParams, Range, methods
 from unittest.async_case import IsolatedAsyncioTestCase
 
+from aac.io.constants import DEFINITION_SEPARATOR
 from aac.plugins.first_party.lsp_server.providers.publish_diagnostics_provider import PublishDiagnosticsProvider
+from aac.plugins.validators.defined_references import PLUGIN_NAME as DEFINED_REFERENCES_VALIDATOR
+from aac.plugins.validators.required_fields import PLUGIN_NAME as REQUIRED_FIELDS_VALIDATOR
+from aac.plugins.validators.root_keys import PLUGIN_NAME as ROOT_KEYS_VALIDATOR
 
-from tests.helpers.parsed_definitions import create_enum_definition
-from tests.plugins.lsp_server.definition_constants import TEST_DOCUMENT_NAME
+from tests.helpers.parsed_definitions import create_enum_definition, create_field_entry, create_schema_definition
+from tests.plugins.lsp_server.definition_constants import TEST_DOCUMENT_NAME, TEST_ENUM, TEST_SERVICE_ONE
 from tests.plugins.lsp_server.base_lsp_test_case import BaseLspTestCase
 
 
@@ -41,5 +45,36 @@ class TestPublishDiagnosticsProvider(BaseLspTestCase, IsolatedAsyncioTestCase):
 
         await self.create_document(invalid_enum_document_name, invalid_enum.to_yaml())
 
-        diagnostics = await self.publish_diagnostics(invalid_enum_document_name)
-        self.assertEqual(1, len(diagnostics))
+        diagnostic, *_ = await self.publish_diagnostics(invalid_enum_document_name)
+        self.assertEqual(0, len(_))
+        self.assertEqual(DiagnosticSeverity.Error, diagnostic.severity)
+        self.assertEqual(Range(start=Position(line=0, character=0), end=Position(line=0, character=4)), diagnostic.range)
+        self.assertEqual(REQUIRED_FIELDS_VALIDATOR, diagnostic.code)
+        self.assertRegexpMatches(diagnostic.message.lower(), "required.*values.*missing")
+
+    async def test_get_diagnostics_for_document_with_invalid_schema(self):
+        invalid_schema_document_name = "schema.aac"
+        fields = [create_field_entry("name", "invalid")]
+        invalid_schema = create_schema_definition("InvalidSchema", "a schema with an invalid field", fields)
+        await self.create_document(invalid_schema_document_name, invalid_schema.to_yaml())
+
+        diagnostic, *_ = await self.publish_diagnostics(invalid_schema_document_name)
+        self.assertEqual(0, len(_))
+        self.assertEqual(DiagnosticSeverity.Error, diagnostic.severity)
+        self.assertEqual(Range(start=Position(line=5, character=10), end=Position(line=5, character=17)), diagnostic.range)
+        self.assertEqual(DEFINED_REFERENCES_VALIDATOR, diagnostic.code)
+        self.assertRegexpMatches(diagnostic.message.lower(), "undefined.*invalid.*reference")
+
+    async def test_get_diagnostics_for_document_with_multiple_definitions(self):
+        TEST_SERVICE_ONE.structure["service"] = TEST_SERVICE_ONE.structure["model"]
+        TEST_SERVICE_ONE.structure.pop("model")
+
+        content = DEFINITION_SEPARATOR.join([TEST_ENUM.to_yaml(), TEST_SERVICE_ONE.to_yaml()])
+        await self.write_document(TEST_DOCUMENT_NAME, content)
+
+        diagnostic, *_ = await self.publish_diagnostics(TEST_DOCUMENT_NAME)
+        self.assertEqual(0, len(_))
+        self.assertEqual(DiagnosticSeverity.Error, diagnostic.severity)
+        self.assertEqual(Range(start=Position(line=7, character=0), end=Position(line=7, character=7)), diagnostic.range)
+        self.assertEqual(ROOT_KEYS_VALIDATOR, diagnostic.code)
+        self.assertRegexpMatches(diagnostic.message.lower(), "undefined.*root.*service")
