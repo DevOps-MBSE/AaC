10a2560e37f3285ffa36c00b01a9fb9847076fb5
Charles Blackard <charles.blackard@zadentech.com>
2022-12-06 13:48:41 -0600

    Added test for the boolean validator

--- python/src/aac/plugins/first_party/primitive_type_check/validators/bool_validator.py
+++ python/src/aac/plugins/first_party/primitive_type_check/validators/bool_validator.py
@@ -17,33 +17,24 @@ def get_validator() -> PrimitiveValidationContribution:
 def validate_bool(definition: Definition, value_to_validate: Any) -> Optional[ValidatorFinding]:
     """
     Returns a Validation finding if the type isn't valid, otherwise None.
-    
-    This function is intended to be used in the Validation apparatus, and for this result
-        to be collated with other finding into a larger ValidatorResult.
-        
-        Arge:
-            definition (Definition): The definition that the value belongs to.
-            value_to_validate (Any): The value to be tested.
-        
-        Returns:
-            A ValidatorFinding if the value is not a boolean or None.
-    """
-    is_invalid = False
-    try:
-        type_casted_bool = bool(value_to_validate)
-        # asset that the conversoin didn't alter the contents. 
-        is_invalid = (str(type_casted_bool) != str(value_to_validate))
-    except Exception as e:
-        is_invalid = True
-        logging.debug(f"{PRIMITIVE_TYPE_BOOL} validation failed for value {value_to_validate} with error: \n{e}")
-        pass
 
+    `This function is intended to be used in the Validation apparatus, and for this result
+    to be collated with other finding into a larger ValidatorResult.
+
+    Arge:
+        definition (Definition): The definition that the value belongs to.
+        value_to_validate (Any): The value to be tested.
+
+    Returns:
+        `A ValidatorFinding if the value is not a boolean or None.
+    """
+
+    is_invalid = not isinstance(value_to_validate, bool)
     finding = None
     if is_invalid:
-        finding_message = f"{value_to_validate} is invalid value for boolean type {PRIMITIVE_TYPE_BOOL}"
-        # Cast the value_to_validate to string since lexeme values will always be strings.
         lexeme, *_ = [lexeme for lexeme in definition.lexemes if lexeme.value == str(value_to_validate)]
+        finding_message = f"{value_to_validate} is not a valid value for boolean type {PRIMITIVE_TYPE_BOOL}"
         finding_location = get_finding_location(BOOL_VALIDATION_NAME, lexeme)
         finding = ValidatorFinding(definition, FindingSeverity.ERROR, finding_message, finding_location)
-    
+
     return finding
