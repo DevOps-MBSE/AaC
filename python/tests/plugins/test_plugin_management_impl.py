f26917b9dacf18c173db48ab6372105e5b36837b
Cameron Chaparro <cameron.chaparro@zadentech.com>
2022-12-19 14:55:34 -0600

    Add generated plugin-manangement plugin

---
+++ python/tests/plugins/test_plugin_management_impl.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+from aac.plugins.plugin_execution import PluginExecutionStatusCode
+from aac.plugins.first_party.plugin_management.plugin_management_impl import list_plugins, activate_plugin, deactivate_plugin
+
+class TestPluginManagement(TestCase):
+    def test_list_plugins(self):
+        # TODO: Write tests for list_plugins
+
+        all = bool()
+        active = bool()
+        inactive = bool()
+
+        result = list_plugins(all=all, active=active, inactive=inactive)
+        self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+
+    def test_activate_plugin(self):
+        # TODO: Write tests for activate_plugin
+
+        plugin_name = str()
+
+        result = activate_plugin(plugin_name=plugin_name)
+        self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+
+    def test_deactivate_plugin(self):
+        # TODO: Write tests for deactivate_plugin
+
+        plugin_name = str()
+
+        result = deactivate_plugin(plugin_name=plugin_name)
+        self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
+
