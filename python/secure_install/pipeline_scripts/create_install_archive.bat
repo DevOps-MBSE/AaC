63d34c130dfb3fedc4af80e4363cecf2b8d9b9fd
Alex Monnet <alexcmonnet@gmail.com>
2022-12-19 07:33:26 +0000

    Added more debug

--- python/secure_install/pipeline_scripts/create_install_archive.bat
+++ python/secure_install/pipeline_scripts/create_install_archive.bat
@@ -3,10 +3,12 @@ REM https://daniel-sc.github.io/bash-shell-to-bat-converter/
 
 cd /D "%~dp0"
 
+echo "%cd%"
 SET install_script_dir="%cd%"
 cd "%install_script_dir%"
 cd "../"
-SET version=%aac version%
+SET version=%python -m aac version%
+echo "Version %version%"
 SET install_dir="aac_secure_install_%version%"
 IF EXIST "%install_dir%" (
   DEL /S "%install_dir%"
@@ -15,8 +17,7 @@ mkdir "%install_dir%"
 COPY  "install_scripts\*" "%install_dir%"
 COPY  "README.md" "%install_dir%"
 cd "%install_dir%"
-python -m pip list
-python -m pip "wheel" "%CD%\..\..\"
+python -m pip "wheel" "-e" "%CD%\..\..\"
 python -m pip-compile "--generate-hashes" "%CD%\..\..\setup.py"
 mv "%CD%\..\..\requirements.txt" "."
 REM UNKNOWN: {"type":"Pipeline","commands":[{"type":"Command","name":{"text":"pip","type":"Word"},"suffix":[{"text":"hash","type":"Word"},{"text":"aac-*.whl","type":"Word"}]},{"type":"Command","name":{"text":"sed","type":"Word"},"suffix":[{"text":"-r","type":"Word"},{"text":"s/l:/l \\/g","type":"Word"}]},{"type":"Command","name":{"text":"sed","type":"Word"},"suffix":[{"text":"-r","type":"Word"},{"text":"s/--hash/    --hash/g","type":"Word"},{"type":"Redirect","op":{"text":">>","type":"dgreat"},"file":{"text":"requirements.txt","type":"Word"}}]}]}
