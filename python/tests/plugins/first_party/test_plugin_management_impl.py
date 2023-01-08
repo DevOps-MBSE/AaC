830ed8564dd4b5760e7a4652a25799f66971848b
Cameron Chaparro <cameron.chaparro@zadentech.com>
2023-01-07 18:58:44 -0600

    Refactor plugin_management_impl.py

--- python/tests/plugins/first_party/test_plugin_management_impl.py
+++ python/tests/plugins/first_party/test_plugin_management_impl.py
@@ -22,26 +22,38 @@ class TestPluginManagement(TestCase):
             result.messages[0], "\n".join([plugin.name for plugin in get_active_context().get_inactive_plugins()])
         )
 
-        with self.assertRaises(PluginError):
+        with self.assertRaises(PluginError) as cm:
             list_plugins(all=True, active=True, inactive=False)
 
-        with self.assertRaises(PluginError):
+        exception = cm.exception
+        self.assertRegexpMatches(exception.message, "Multiple.*options.*all=True.*active=True")
+
+        with self.assertRaises(PluginError) as cm:
             list_plugins(all=True, active=False, inactive=True)
 
-        with self.assertRaises(PluginError):
+        exception = cm.exception
+        self.assertRegexpMatches(exception.message, "Multiple.*options.*all=True.*inactive=True")
+
+        with self.assertRaises(PluginError) as cm:
             list_plugins(all=False, active=True, inactive=True)
 
-        with self.assertRaises(PluginError):
+        exception = cm.exception
+        self.assertRegexpMatches(exception.message, "Multiple.*options.*active=True.*inactive=True")
+
+        with self.assertRaises(PluginError) as cm:
             list_plugins(all=True, active=True, inactive=True)
+
+        exception = cm.exception
+        self.assertRegexpMatches(exception.message, "Multiple.*options.*all=True.*active=True.*inactive=True")
 
     def test_activate_plugin(self):
         plugin_name = str()
 
-        result = activate_plugin(plugin_name=plugin_name)
+        result = activate_plugin(name=plugin_name)
         self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
 
     def test_deactivate_plugin(self):
         plugin_name = str()
 
-        result = deactivate_plugin(plugin_name=plugin_name)
+        result = deactivate_plugin(name=plugin_name)
         self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
