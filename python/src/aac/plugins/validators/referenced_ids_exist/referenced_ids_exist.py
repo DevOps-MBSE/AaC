9995f8a68354aa1b564b00046a6536ce66f817f9
jondavid-black <black.jondavid@gmail.com>
2023-01-29 20:54:18 +0000

    req reference validator now works

--- python/src/aac/plugins/validators/referenced_ids_exist/referenced_ids_exist.py
+++ python/src/aac/plugins/validators/referenced_ids_exist/referenced_ids_exist.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 from aac.lang.definitions.definition import Definition
 from aac.lang.language_context import LanguageContext
 from aac.lang.definitions.structure import get_substructures_by_type
@@ -5,6 +7,8 @@ from aac.plugins.validators import ValidatorFindings, ValidatorResult
 
 
 SPEC_REF_ID_VALIDATOR_NAME = "Referenced IDs exist"
+
+ALL_REQ_IDS: Set[str] = set()
 
 
 def validate_referenced_ids(
@@ -27,8 +31,24 @@ def validate_referenced_ids(
     """
     findings = ValidatorFindings()
 
-    dicts_to_test = get_substructures_by_type(definition_under_test, target_schema_definition, language_context)
-    if dicts_to_test:
-        print(f"Specification -> Referenced IDs Exist on {definition_under_test.name}, {target_schema_definition.name} {dicts_to_test}")
+    _get_all_requirement_ids(target_schema_definition, language_context)
+
+    for req_ref_dict in get_substructures_by_type(definition_under_test, target_schema_definition, language_context):
+        for req_dict in get_substructures_by_type(definition_under_test, target_schema_definition, language_context):
+            for id in req_dict["ids"]:
+                if id not in ALL_REQ_IDS:
+                    findings.add_error_finding(
+                        definition_under_test, f"Referenced requirement '{id}' is not defined", SPEC_REF_ID_VALIDATOR_NAME, definition_under_test.get_lexeme_with_value(id)
+                    )
 
     return ValidatorResult([definition_under_test], findings)
+
+
+def _get_all_requirement_ids(target_schema_definition, language_context):
+    if len(ALL_REQ_IDS) == 0:  # don't repeat this for every validator invocation
+        requirement_definition = language_context.get_definition_by_name("Requirement")
+
+        spec_roots = language_context.get_definitions_by_root_key("spec")
+        for spec in spec_roots:
+            for req_dict in get_substructures_by_type(spec, requirement_definition, language_context):
+                ALL_REQ_IDS.add(req_dict["id"])
