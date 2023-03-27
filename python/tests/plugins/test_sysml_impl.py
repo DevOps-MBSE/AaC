e45ece6bf7f28edf38cf0ceb9afa2ccaf1cc7e98
Alex Monnet <alexcmonnet@gmail.com>
2023-03-27 18:15:59 +0000

    Added initial structure and registration of sysml plugin

---
+++ python/tests/plugins/test_sysml_impl.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+from aac.plugins.plugin_execution import PluginExecutionStatusCode
+from aac.plugins.first_party.sysml.sysml_impl import 
+
+class TestSysml(TestCase):
