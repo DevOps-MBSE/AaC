dd31f0aea6d3bf6bc59422b1f01248fc451332c4
Alex Monnet <alexcmonnet@gmail.com>
2023-04-05 06:00:28 +0000

    Fixed flake issues

--- python/tests/plugins/test_sysml_impl.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-from aac.plugins.plugin_execution import PluginExecutionStatusCode
-from aac.plugins.first_party.sysml.sysml_impl import 
-
-class TestSysml(TestCase):
