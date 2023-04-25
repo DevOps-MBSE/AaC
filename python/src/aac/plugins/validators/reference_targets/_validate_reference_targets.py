2db6cc32467654227dfc4ad5b3f05463260268a5
Cameron Chaparro <cameron.chaparro@zadentech.com>
2023-04-25 15:09:27 -0500

    Replace hard-coded string values with constants

--- python/src/aac/plugins/validators/reference_targets/_validate_reference_targets.py
+++ python/src/aac/plugins/validators/reference_targets/_validate_reference_targets.py
@@ -1,5 +1,6 @@
 import logging
 
+from aac.lang.constants import DEFINITION_FIELD_FIELDS, DEFINITION_FIELD_NAME, DEFINITION_FIELD_TYPE, PRIMITIVE_TYPE_REFERENCE
 from aac.lang.definitions.definition import Definition
 from aac.lang.definitions.references import get_reference_target_definitions, is_reference_format_valid
 from aac.lang.definitions.structure import get_substructures_by_type
@@ -31,16 +32,16 @@ def validate_reference_targets(
     findings = ValidatorFindings()
 
     reference_field_names = validation_args
-    schema_defined_fields_as_list = target_schema_definition.get_top_level_fields().get("fields") or []
-    schema_defined_fields_as_dict = {field.get("name"): field for field in schema_defined_fields_as_list}
+    schema_defined_fields_as_list = target_schema_definition.get_top_level_fields().get(DEFINITION_FIELD_FIELDS) or []
+    schema_defined_fields_as_dict = {field.get(DEFINITION_FIELD_NAME): field for field in schema_defined_fields_as_list}
 
     def validate_dict(dict_to_validate: dict) -> None:
         for reference_field_name in reference_field_names:
             field_value = dict_to_validate.get(reference_field_name)
-            field_type = schema_defined_fields_as_dict.get(reference_field_name, {}).get("type")
+            field_type = schema_defined_fields_as_dict.get(reference_field_name, {}).get(DEFINITION_FIELD_TYPE)
 
             # field type must be reference
-            if field_type != "reference":
+            if field_type != PRIMITIVE_TYPE_REFERENCE:
                 non_reference_field = f"Reference format validation cannot be performed on non-reference field '{reference_field_name}'.  Type is '{field_type}'"
                 reference_field_name_lexeme = definition_under_test.get_lexeme_with_value(reference_field_name)
                 findings.add_error_finding(
