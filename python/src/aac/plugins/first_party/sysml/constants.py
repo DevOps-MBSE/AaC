9e888b047b983bb61b512846907c29ad10ae1ae2
Alex Monnet <alexcmonnet@gmail.com>
2023-05-16 17:48:05 +0000

    Added a constants file specifically for SysML plugin

---
+++ python/src/aac/plugins/first_party/sysml/constants.py
@@ -0,0 +1,18 @@
+"""Module that provides constants for SysML definitions."""
+
+# SysML Root Keys
+SYSML_ROOT_KEY_BDD = "blockDefinitionDiagram"
+SYSML_ROOT_KEY_BLOCK = "block"
+
+# SysML Definition Names
+SYSML_DEFINITION_NAME_BDD = "BlockDefinitionDiagram"
+SYSML_DEFINITION_NAME_BLOCK = "Block"
+
+SYSML_DEFINITION_NAME_MODEL_METADATA = "SysmlModelMetadata"
+SYSML_DEFINITION_NAME_SCHEMA_METADATA = "SysmlSchemaMetadata"
+
+# SysML Definition Fields
+SYSML_DEFINITION_FIELD_SYSML_ELEMENT = "sysml_element"
+SYSML_DEFINITION_FIELD_DEFAULT_NAMESPACE =  "defaultNamespace"
+SYSML_DEFINITION_FIELD_BLOCKS =  "blocks"
+SYSML_DEFINITION_FIELD_VALUES =  "values"
