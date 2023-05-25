c3b016864331f97223cdc638254d7605a69aa0ce
Alex Monnet <alexcmonnet@gmail.com>
2023-05-25 07:28:15 +0000

    Update type hint for test helper

--- python/tests/helpers/parsed_definitions.py
+++ python/tests/helpers/parsed_definitions.py
@@ -154,7 +154,7 @@ def create_usecase_definition(name: str, description: str, participants: list[di
 
 
 def create_model_definition(
-    name: str, description: str = "", components: list[dict] = [], behavior: list[dict] = [], state: list[str] = []
+    name: str, description: str = "", components: list[str] = [], behavior: list[dict] = [], state: list[str] = []
 ):
     """Return a simulated model definition."""
     definition_dict = {
