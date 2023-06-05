abbc6f446013e3c1338066ae6d3db725d9a36499
lizzcondrey <ashley.condrey@ngc.com>
2023-06-05 15:08:02 -0500

    - Adding traceback to help control traceback printing

--- python/src/aac/io/parser/_yaml.py
+++ python/src/aac/io/parser/_yaml.py
@@ -44,10 +44,11 @@ def parse_yaml(source: str, content: str) -> list[dict]:
                                                               f"Encountered error at line, column: {error.problem_mark.line+1}, {error.problem_mark.column+1}",
                                                               f"Context of the error: {error.context}"])
     except Exception as error:
+        traceback.print_last()
         # print(f"Failed to parse file: {source}", f"Encountered the following error: {error}")
-        logging.error(f"Error: {error}. Encountered in: {source}"))
+        logging.error(f"Error: {error}. Encountered in: {source}")
         logging.error(f"Content of error: {content}")
-        raise ParserError(f"Failed to parse file: {source}", [f"Encountered the following error: {error}"]) from None
+        raise ParserError(f"Failed to parse file: {source}", [f"Encountered the following error: {error}"])
 
 
 def _error_if_not_yaml(source, content, models):
