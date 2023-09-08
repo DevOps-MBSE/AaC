b489bd133a5532e06a854a863f61abb9bbcbf501
Cameron Chaparro <cameron.chaparro@zadentech.com>
2023-09-08 14:33:30 -0400

    Swap test cases

--- python/tests/plugins/validators/test__validate_usecase_participants.py
+++ python/tests/plugins/validators/test__validate_usecase_participants.py
@@ -12,6 +12,22 @@ from tests.helpers.parsed_definitions import (
 
 
 class TestUsecaseParticipants(ActiveContextTestCase):
+    def test_validate_usecase_with_referenced_participants(self):
+        test_participant_1 = create_field_entry("user1", "SomeModel")
+        test_participant_2 = create_field_entry("user2", "SomeOtherModel")
+        test_step = create_step_entry(
+            "step1", test_participant_1.get(DEFINITION_FIELD_NAME), test_participant_2.get(DEFINITION_FIELD_NAME), "action"
+        )
+        test_usecase = create_usecase_definition("TestUsecase", "", [test_participant_1, test_participant_2], [test_step])
+
+        test_active_context = get_active_context(reload_context=True)
+        test_active_context.add_definition_to_context(test_usecase)
+        target_schema_definition = get_definition_by_name(DEFINITION_NAME_USECASE, test_active_context.definitions)
+
+        result = validate_usecase_participants(test_usecase, target_schema_definition, test_active_context)
+
+        self.assertTrue(result.is_valid())
+
     def test_usecase_with_step_endpoint_does_not_reference_participant_fails(self):
         test_participant = create_field_entry("user1", "SomeModel")
         test_step = create_step_entry("step1", "notUser1", "alsoNotUser1", "action")
@@ -30,22 +46,6 @@ class TestUsecaseParticipants(ActiveContextTestCase):
 
         target = test_step.get(DEFINITION_FIELD_TARGET)
         self.assertRegexpMatches(result.get_messages_as_string(), f".*{target}.*not.*participant.*")
-
-    def test_validate_usecase_with_referenced_participants(self):
-        test_participant_1 = create_field_entry("user1", "SomeModel")
-        test_participant_2 = create_field_entry("user2", "SomeOtherModel")
-        test_step = create_step_entry(
-            "step1", test_participant_1.get(DEFINITION_FIELD_NAME), test_participant_2.get(DEFINITION_FIELD_NAME), "action"
-        )
-        test_usecase = create_usecase_definition("TestUsecase", "", [test_participant_1, test_participant_2], [test_step])
-
-        test_active_context = get_active_context(reload_context=True)
-        test_active_context.add_definition_to_context(test_usecase)
-        target_schema_definition = get_definition_by_name(DEFINITION_NAME_USECASE, test_active_context.definitions)
-
-        result = validate_usecase_participants(test_usecase, target_schema_definition, test_active_context)
-
-        self.assertTrue(result.is_valid())
 
     def test_validate_usecase_with_valid_source_invalid_target(self):
         test_participant = create_field_entry("user1", "SomeModel")
