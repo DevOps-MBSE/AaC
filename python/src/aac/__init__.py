954c8b75d7269fd9f60992c195b23a6200b74bb2
lizzcondrey <ashley.condrey@ngc.com>
2023-09-11 11:21:06 -0500

    - bumping version for proper order

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -12,7 +12,7 @@ if sys.version_info < (3, 9):
 import logging
 import os
 
-__version__ = "0.2.17"
+__version__ = "0.2.19"
 __log_file_name__ = os.path.join(os.path.dirname(__file__), "aac.log")
 
 logging.basicConfig(
