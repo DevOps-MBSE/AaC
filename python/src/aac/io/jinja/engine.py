f333a26712c003621339385f47921b703652d8da
JD Black <black.jondavid@gmail.com>
2023-09-24 20:57:15 -0500

    minor progress on gen-plugin

---
+++ python/src/aac/io/jinja/engine.py
@@ -0,0 +1,171 @@
+"""This module provides a common set of templating and generation functions."""
+
+from __future__ import annotations
+import os
+from typing import Callable
+
+from attr import attrib, attrs, validators
+from jinja2 import Environment, PackageLoader, Template, TemplateError
+
+from aac.io.writer import write_file
+from aac.io.jinja.error import AacTemplateError
+
+
+def load_templates(package_name: str, template_directory: str = "templates") -> list[Template]:
+    """
+    Load templates from a `templates` directory within a package.
+
+    Args:
+        group_dir_name: name of the templates sub-directory to load templates from
+        template_directory: the directory within the package containing the templates. Defaults to 'templates'
+
+    Returns:
+        list of loaded templates
+    """
+
+    def _load_templates_from_env(env) -> list:
+        filtered_templates = list(filter(_is_template, env.list_templates()))
+        return list(map(env.get_template, filtered_templates))
+
+    env = Environment(
+        loader=PackageLoader(package_name, template_directory),
+        autoescape=True,
+        lstrip_blocks=True,
+        trim_blocks=True,
+    )
+
+    return _handle_template_error(
+        f"The error occurred while loading the templates from '{template_directory}/' for the '{package_name}' package",
+        _load_templates_from_env,
+        env,
+    )
+
+
+def _is_template(filename: str) -> bool:
+    return filename.endswith("jinja2")
+
+
+def generate_templates(
+    templates: list[Template], output_directories: dict[str, str], properties: dict[str, str]
+) -> dict[str, TemplateOutputFile]:
+    """
+    Compile a list of Jinja2 Templates with a dict of template properties into a dict of template name to compiled template content.
+
+    Args:
+        templates: list of Jinja2 templates to compile.
+        output_directories: The directories in which to generate the files.
+        properties: Dict of properties to use when compiling the templates.
+
+    Returns:
+        Dict of template names to TemplateOutputFile objects
+    """
+    generated_templates = {}
+    for template in templates:
+        template_id = template.name
+        generated_templates[template_id] = generate_template(template, output_directories.get(template_id), properties)
+
+    return generated_templates
+
+
+def generate_templates_as_strings(templates: list[Template], properties: dict[str, str]) -> dict[str, str]:
+    """
+    Compile a list of Jinja2 Templates with a dict of template properties into a dict of template name to compiled template content.
+
+    Args:
+        templates: list of Jinja2 templates to compile.
+        properties: Dict of properties to use when compiling the templates
+
+    Returns:
+        Dict of template names to template content strings
+    """
+    generated_templates = {}
+    for template in templates:
+        template_id = template.name
+        generated_templates[template_id] = generate_template_as_string(template, properties)
+
+    return generated_templates
+
+
+def generate_template(template: Template, output_directory: str, properties: dict[str, str]) -> TemplateOutputFile:
+    """
+    Compile a Jinja2 Template object to a TemplateOutputFile object.
+
+    Args:
+        template (Template): Jinja2 template to compile.
+        output_directory (str): The directory in which to generate the gherkin file.
+        properties (dict[str, str]): Dict of properties to use when compiling the template.
+
+    Returns:
+        Compiled/Rendered template as a TemplateOutputFile object
+    """
+    return TemplateOutputFile(
+        output_directory=output_directory,
+        template_name=template.name,
+        content=generate_template_as_string(template, properties),
+        overwrite=False,
+    )
+
+
+def generate_template_as_string(template: Template, properties: dict[str, str]) -> str:
+    """
+    Compile a Jinja2 Template object to a string.
+
+    Args:
+        template: Jinja2 template to compile.
+        properties: Dict of properties to use when compiling the template
+
+    Returns:
+        Compiled/Rendered template as a string
+    """
+    return _handle_template_error(f"The error occurred while rendering {template.filename}", template.render, properties)
+
+
+def write_generated_templates_to_file(generated_files: list[TemplateOutputFile]) -> None:
+    """
+    Write a list of generated files to the target directory.
+
+    Args:
+        generated_files: list of generated files to write to the filesystem
+    """
+    for generated_file in generated_files:
+        file_uri = os.path.join(generated_file.output_directory, generated_file.file_name)
+        write_file(
+            file_uri,
+            generated_file.content,
+            generated_file.overwrite,
+        )
+
+
+def _handle_template_error(error_reason: str, callback: Callable, *args, **kwargs):
+    try:
+        return callback(*args, **kwargs)
+    except TemplateError as template_error:
+        error_message = f"{error_reason}\n{template_error}"
+
+        if hasattr(template_error, "filename"):
+            error_message = f"{error_reason}\n{os.path.basename(template_error.filename)}:L{template_error.lineno} {template_error.message}"
+
+        raise AacTemplateError(
+            f"{template_error.__class__.__name__} occurred running {__package__}.{callback.__name__}:\n{error_message}"
+        )
+
+
+@attrs(slots=True, auto_attribs=True)
+class TemplateOutputFile:
+    """
+    Class containing all of the relevant information necessary to handle writing templates to files.
+
+    Attributes:
+        output_directory (str): The directory in which to generate the file.
+        template_name (str): The name of the jinja2 template the generated content is based on
+        content (str): The generated content
+        overwrite (bool): A boolean to indicate if this template output should overwrite any existing files with the same name.
+
+        file_name: This attribute is not exposed in the constructor. It's up to the user to set the filename.
+    """
+
+    output_directory: str = attrib(validator=validators.instance_of(str))
+    template_name: str = attrib(validator=validators.instance_of(str))
+    file_name: str = attrib(validator=validators.instance_of(str), default="", init=False)
+    content: str = attrib(validator=validators.instance_of(str))
+    overwrite: bool = attrib(validator=validators.instance_of(bool))
--- python/src/aac/plugins/gen_plugin/gen_plugin_impl.py
+++ python/src/aac/plugins/gen_plugin/gen_plugin_impl.py
@@ -1,6 +1,9 @@
 """AaC Plugin implementation module for the Version plugin."""
 
-from aac.execute.aac_execution_result import ExecutionResult, ExecutionStatus
+from os import path
+from typing import Callable
+from aac.execute.aac_execution_result import ExecutionResult, ExecutionStatus, OperationCancelled, ExecutionError
+from aac.context.language_context import LanguageContext
 
 plugin_name = "Generate Plugin"
 
@@ -8,6 +11,68 @@ plugin_name = "Generate Plugin"
 def gen_plugin(aac_plugin_file: str, code_output: str, test_output: str, no_prompt: bool, no_setup: bool) -> ExecutionResult:
     """Print the AaC package version."""
 
-    print(f"Running the AaC Gen-Plugin with:\n   aac_plugin_file: {aac_plugin_file}\n   code_output: {code_output}\n   test_output: {test_output}\n   no_prompt: {no_prompt}\n   no_setup: {no_setup}")
+    print(f"DEBUG: Running the AaC Gen-Plugin with:\n   aac_plugin_file: {aac_plugin_file}\n   code_output: {code_output}\n   test_output: {test_output}\n   no_prompt: {no_prompt}\n   no_setup: {no_setup}")
+
+    # setup directories
+    code_out_dir, test_out_dir = get_output_directories(aac_plugin_file, code_output, test_output, no_prompt)
+
+    print(f"DEBUG: code_out_dir: {code_out_dir} test_out_dir: {test_out_dir}")
+
+    # build out properties
+    # the templates need data from the plugin model to generate code
+    code_gen_props = {}
+    context = LanguageContext()
+    parsed_definitions = context.parse_and_load(aac_plugin_file)
+    plugin_definitions = [definition for definition in parsed_definitions if definition.get_root_key == "plugin"]
+    if not plugin_definitions:
+        return ExecutionResult(plugin_name, "gen-plugin", ExecutionStatus.GENERAL_FAILURE, [f"Could not load plugin definition from {aac_plugin_file}"])
+    if len(plugin_definitions) == 0:
+        return ExecutionResult(plugin_name, "gen-plugin", ExecutionStatus.GENERAL_FAILURE, [f"Could not load plugin definition from {aac_plugin_file}"])
+    if len(plugin_definitions) > 1:
+        return ExecutionResult(plugin_name, "gen-plugin", ExecutionStatus.GENERAL_FAILURE, [f"Found multiple plugin definitions in {aac_plugin_file}"])
+    the_plugin_definition = plugin_definitions[0]
+    
+    code_gen_props["plugin"] = the_plugin_definition.structure["plugin"]
+        
+    # build out jinja2 utility functions
+    def get_python_name(name: str) -> str:
+        return name.replace(" ", "_").replace("-", "_").lower()
+    
+    func_dict: dict[str, Callable] = {
+        "get_python_name": get_python_name
+    }
+    
+    # TODO: figure out how to load func_dict into the jinja2 environment
+
+    # TODO: generate code
+    
+    # TODO: write output to files
     
     return ExecutionResult(plugin_name, "gen-plugin", ExecutionStatus.SUCCESS, [])
+
+def get_output_directories(aac_plugin_file: str, code_output: str, test_output: str, no_prompt: bool) -> tuple[str, str]:
+
+    code_out: str = "."
+    test_out: str = "."
+    
+    if not code_output or len(code_output) == 0:
+        aac_plugin_path = path.abspath(aac_plugin_file)
+        if not path.exists(aac_plugin_file):
+            raise ExecutionError(f"The provided AaC Plugin file does not exist: {aac_plugin_file}")
+        code_out = path.dirname(aac_plugin_path)
+    if not test_output or len(test_output) == 0:
+        test_out = code_out.replace("src", "tests")
+
+    if not no_prompt:
+        print("AaC Gen-Plugin will generate code and tests in the following directories:")
+        print(f"   code: {code_out}")
+        print(f"   tests: {test_out}")
+        approval = "first"
+        while approval not in ["y", "Y", "yes", "Yes", "YES", "n", "N", "no", "No", "NO"]:
+            approval = input("Do you want to continue? (y/n): ")
+        if approval in ["y", "Y", "yes", "Yes", "YES"]:
+            return (code_out, test_out)
+        else:
+            raise OperationCancelled("User cancelled operation.")
+
+    return (code_out, test_out)
