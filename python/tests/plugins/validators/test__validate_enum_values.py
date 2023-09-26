c7a1291c5c62950d6c4704bf34f7069b6ecda8e3
Alex Monnet <alexcmonnet@gmail.com>
2023-09-26 14:51:18 +0000

    Added tests

--- python/tests/plugins/validators/test__validate_enum_values.py
+++ python/tests/plugins/validators/test__validate_enum_values.py
@@ -1,11 +1,9 @@
 from aac.lang.active_context_lifecycle_manager import get_active_context
 from aac.lang.constants import BEHAVIOR_TYPE_REQUEST_RESPONSE, DEFINITION_NAME_MODEL
-from aac.lang.definitions.collections import get_definition_by_name
-from aac.lang.definitions.lexeme import Lexeme
-from aac.lang.definitions.source_location import SourceLocation
-from aac.plugins.contributions.contribution_types import DefinitionValidationContribution
-from aac.plugins.validators import ValidatorFindings, ValidatorResult
-from aac.plugins.validators.defined_references import _get_plugin_definitions, _get_plugin_validations, validate_references
+from aac.plugins.validators import ValidatorResult
+from aac.plugins.validators.enum_values._validate_enums import validate_enums
+from aac.validate import validated_definition, ValidationError
+
 from tests.active_context_test_case import ActiveContextTestCase
 from tests.helpers.assertion import assert_definitions_equal
 from tests.helpers.context import get_core_spec_context
@@ -23,7 +21,7 @@ class TestEnumValuesPlugin(ActiveContextTestCase):
         test_active_context = get_active_context(reload_context=True)
         model_schema_definition = test_active_context.get_definition_by_name(DEFINITION_NAME_MODEL)
 
-        actual_result = validate_references(test_model, model_schema_definition, test_active_context)
+        actual_result = validate_enums(test_model, model_schema_definition, test_active_context)
 
         self.assertEqual(expected_result.is_valid(), actual_result.is_valid())
 
@@ -35,7 +33,25 @@ class TestEnumValuesPlugin(ActiveContextTestCase):
         test_active_context = get_active_context(reload_context=True)
         model_schema_definition = test_active_context.get_definition_by_name(DEFINITION_NAME_MODEL)
 
-        actual_result = validate_references(test_model, model_schema_definition, test_active_context)
+        actual_result = validate_enums(test_model, model_schema_definition, test_active_context)
 
         self.assertFalse(actual_result.is_valid())
 
+    def test_validated_definition_catches_invalid(self):
+        # Create a model with a behavior to test that the validation is valid when a valid BehaviorType enum value is used.
+        bad_enum_value = "NOT_A_VALID_BEHAVIOR_TYPE"
+        test_behavior = create_behavior_entry("TestBehavior", bad_enum_value)
+        test_model = create_model_definition("TestModel", behavior=[test_behavior])
+
+        validation_message = ""
+        try:
+            with validated_definition(test_model):
+                pass
+        except Exception as validation_result:
+            self.assertIsNotNone(validation_result)
+            self.assertTrue(isinstance(validation_result, ValidationError))
+            validation_message = str(validation_result)
+
+        # Setting up an assert outside the exception catch to demonstrate successful exception catching because assertRaise is inadequate.
+        self.assertIn(test_model.name, validation_message)
+        self.assertIn(bad_enum_value, validation_message)
