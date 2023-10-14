854b12a51a560e8253b90d54519459b0c545c26d
JD Black <black.jondavid@gmail.com>
2023-10-14 10:46:45 -0500

    found command gen errors and fixed, regenerated the version command

---
+++ python/tests/aac/plugins/version/version_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+from aac.plugins.version.version_impl import plugin_name, version
+
+
+class Testversion(TestCase):
+    def test_version(self):
+        # TODO: Write tests for version
+        self.fail("Test not yet implemented.")
