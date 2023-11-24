f15cd96d5d94331287101289b9c7bffbfa0ee033
Lizz Condrey <ashley.condrey@ngc.com>
2023-11-24 17:41:23 +0000

    - forgot my semicolons...

--- vscode_extension/src/aacExecutableWrapper.ts
+++ vscode_extension/src/aacExecutableWrapper.ts
@@ -84,9 +84,9 @@ async function getCommandArgUserInput(commandArguments: CommandArgument[]) {
             let fileUris: Uri[] | undefined = await window.showOpenDialog(dialogBoxOptions);
             argumentToPromptUserFor.userResponse = fileUris ? fileUris.map(uri => uri.path).join(",") : "";
         } else {
-            let placeholderText = "Optional Argument. Escape to Skip"
+            let placeholderText = "Optional Argument. Escape to Skip";
 
-            argumentToPromptUserFor.optional ? placeholderText = placeholderText : placeholderText = ""
+            argumentToPromptUserFor.optional ? placeholderText = placeholderText : placeholderText = "";
 
             const inputBoxOptions: InputBoxOptions = {
                 title: argumentToPromptUserFor.name,
