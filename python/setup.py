00d0af83d29582037a211ed11c8b88aef8e032c2
william Bunch <william.bunch@ngc.com>
2024-02-22 16:54:09 +0000

    testing tomli stuff some more

--- python/setup.py
+++ python/setup.py
@@ -46,8 +46,7 @@ runtime_dependencies = [
 development_dependencies = [
     "wheel ~= 0.42.0",
     "pip-tools >= 6.9.0",
-    "tomli < 2.0.0",
-    "tomli >= 1.1.0 ; python_version < '3.11'",
+    'tomli >= 1.1.0 ; python_version < "3.11"',
     "platformdirs >= 2.4",
     "coverage >= 6.0",
     "mccabe >= 0.6.1",
