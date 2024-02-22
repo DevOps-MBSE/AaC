ac3649ae987ae8d968c7ab2cf916567d2ba92544
william Bunch <william.bunch@ngc.com>
2024-02-22 17:53:53 +0000

    tomli version

--- python/setup.py
+++ python/setup.py
@@ -38,7 +38,7 @@ runtime_dependencies = [
 development_dependencies = [
     "wheel ~= 0.42.0",
     "pip-tools >= 6.9.0",
-    "tomli < 2.0.0",
+    "tomli >= 1.1.0",
     "platformdirs >= 2.4",
     "coverage >= 6.0",
     "mccabe >= 0.6.1",
