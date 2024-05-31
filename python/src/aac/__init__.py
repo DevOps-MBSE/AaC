223f86032db1e27ea64df1f52282661036cef068
bunchw <149007807+bunchw@users.noreply.github.com>
2024-05-31 10:31:50 -0500

    Test generation template (#822)
    * test generation template.  imports organized, and added a fail test generation.
    
    * moved aac imports under base python imports
    
    * version bump
    
    * added failure tests for each constraint as well.
    
    * fixed incorrect indentation

--- python/src/aac/__init__.py
+++ python/src/aac/__init__.py
@@ -12,7 +12,7 @@ if sys.version_info < (3, 9):
 import logging
 import os
 
-__version__ = "0.4.7"
+__version__ = "0.4.8"
 __log_file_name__ = os.path.join(os.path.dirname(__file__), "aac.log")
 
 logging.basicConfig(
