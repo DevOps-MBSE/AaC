f281f13b48532d6b477658fa6dd9b870dab2347a
william Bunch <william.bunch@ngc.com>
2024-09-05 22:37:53 +0000

    now it seems like it is finding the names with the spaces, so I am pretty confused myself. Maybe I misdiagnosed the issue.

--- python/tests/test_aac/context/test_definition_parser.py
+++ python/tests/test_aac/context/test_definition_parser.py
@@ -14,7 +14,7 @@ class TestDefinitionParser(TestCase):
         context_definitions = parser.context.get_definitions_by_name(loaded_definitions[0].name)
         name = context_definitions[0].name
 
-        self.assertEqual(name.replace(" ", ""), loaded_definitions[0].name)
+        self.assertEqual(name, loaded_definitions[0].name)
         self.assertIsNotNone(context_definitions[0].instance)
         self.assertEqual(len(context_definitions), 1)
         self.assertEqual(loaded_definitions, context_definitions)
