806507257fe81fd4fcf50d55ed9c7ba51ec77f76
william Bunch <william.bunch@ngc.com>
2024-09-05 15:22:10 +0000

    fixed weird nose2 issue with test_sanitize_filesystem

--- python/tests/test_aac/in_out/test_paths.py
+++ python/tests/test_aac/in_out/test_paths.py
@@ -1,5 +1,5 @@
 import os
-from nose2.tools import params
+# from nose2.tools import params
 from unittest import TestCase
 
 from aac.in_out.paths import sanitize_filesystem_path
@@ -21,7 +21,30 @@ def _get_working_directory_parent_directory(level_of_directories_up: int = 1) ->
 
 
 class TestPaths(TestCase):
-    @params(
+
+    # @params(
+    #     (f"{WORKING_TEST_DIR}/src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/../src/aac/test.py", f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"),
+    #     ("src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
+    #     ("../../src/aac/test.py", f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/../../src/aac/test.py", f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/src/aac/test.exe%00.py", f"{WORKING_TEST_DIR}/src/aac/test.exe.py"),
+    #     (f"{WORKING_TEST_DIR}/%2e%2e%2f/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/%2e%2e//src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/..%2f/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/%2e%2e%5c/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/%2e%2e\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/..%5c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+    #     (f"{WORKING_TEST_DIR}/%252e%252e%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py",),
+    #     (f"{WORKING_TEST_DIR}/..%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+    # )
+    # def test_sanitize_filesystem_path(self, test_path: str, expected_result: str):
+    #     self.assertEqual(sanitize_filesystem_path(test_path), expected_result)
+
+    # Not sure if this is the final fix, leaving original commented above, along with nose2 import
+
+    def test_sanitize_filesystem_path(self):
+        args = [
         (f"{WORKING_TEST_DIR}/src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
         (f"{WORKING_TEST_DIR}/../src/aac/test.py", f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"),
         ("src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
@@ -36,6 +59,6 @@ class TestPaths(TestCase):
         (f"{WORKING_TEST_DIR}/..%5c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
         (f"{WORKING_TEST_DIR}/%252e%252e%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py",),
         (f"{WORKING_TEST_DIR}/..%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    )
-    def test_sanitize_filesystem_path(self, test_path: str, expected_result: str):
-        self.assertEqual(sanitize_filesystem_path(test_path), expected_result)
+        ]
+        for arg in args:
+            self.assertEqual(sanitize_filesystem_path(arg[0]), arg[1])
