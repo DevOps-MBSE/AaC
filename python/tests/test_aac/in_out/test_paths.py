82645a0b99db6225d1a6fd6d1da24cb6885fd01a
jasoncbryant <jason.bryant@ngc.com>
2024-09-25 15:05:55 -0500

    Update test_paths.py

--- python/tests/test_aac/in_out/test_paths.py
+++ python/tests/test_aac/in_out/test_paths.py
@@ -1,7 +1,6 @@
 import os
-# from nose2.tools import params
 from unittest import TestCase
-
+from parameterized import parameterized
 from aac.in_out.paths import sanitize_filesystem_path
 
 WORKING_TEST_DIR = os.getcwd()
@@ -21,9 +20,28 @@ def _get_working_directory_parent_directory(level_of_directories_up: int = 1) ->
 
 
 class TestPaths(TestCase):
-
+    @parameterized.expand([
+            [f"{WORKING_TEST_DIR}/src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py",],
+            [f"{WORKING_TEST_DIR}/../src/aac/test.py", f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"],
+            ["src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"],
+            ["../../src/aac/test.py", f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/../../src/aac/test.py", f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/src/aac/test.exe%00.py", f"{WORKING_TEST_DIR}/src/aac/test.exe.py"],
+            [f"{WORKING_TEST_DIR}/%2e%2e%2f/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/%2e%2e//src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [(f"{WORKING_TEST_DIR}/..%2f/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [(f"{WORKING_TEST_DIR}/%2e%2e%5c/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [(f"{WORKING_TEST_DIR}/%2e%2e\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [(f"{WORKING_TEST_DIR}/..%5c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [(f"{WORKING_TEST_DIR}/%252e%252e%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [(f"{WORKING_TEST_DIR}/..%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+        ])
+    def test_sequence(self, a, b):
+            self.assertEqual(a,b)
+    
     # @params(
     #     (f"{WORKING_TEST_DIR}/src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
+    
     #     (f"{WORKING_TEST_DIR}/../src/aac/test.py", f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"),
     #     ("src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
     #     ("../../src/aac/test.py", f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
@@ -43,22 +61,4 @@ class TestPaths(TestCase):
 
     # Not sure if this is the final fix, leaving original commented above, along with nose2 import
 
-    def test_sanitize_filesystem_path(self):
-        args = [
-        (f"{WORKING_TEST_DIR}/src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/../src/aac/test.py", f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"),
-        ("src/aac/test.py", f"{WORKING_TEST_DIR}/src/aac/test.py"),
-        ("../../src/aac/test.py", f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/../../src/aac/test.py", f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/src/aac/test.exe%00.py", f"{WORKING_TEST_DIR}/src/aac/test.exe.py"),
-        (f"{WORKING_TEST_DIR}/%2e%2e%2f/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/%2e%2e//src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/..%2f/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/%2e%2e%5c/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/%2e%2e\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/..%5c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-        (f"{WORKING_TEST_DIR}/%252e%252e%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py",),
-        (f"{WORKING_TEST_DIR}/..%255c\/src/aac/test.py", f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-        ]
-        for arg in args:
-            self.assertEqual(sanitize_filesystem_path(arg[0]), arg[1])
+#
