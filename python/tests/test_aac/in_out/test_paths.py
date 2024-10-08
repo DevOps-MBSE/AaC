3223dae16296b03a627dffe6ef563e8e4697daf0
jason bryant <jason.bryant@ngc.com>
2024-10-08 14:15:05 +0000

    Capture two approaches pending a decision

--- python/tests/test_aac/in_out/test_paths.py
+++ python/tests/test_aac/in_out/test_paths.py
@@ -1,5 +1,6 @@
 import os
-import pytest
+#import pytest
+from parameterized import parameterized
 from unittest import TestCase
 from aac.in_out.paths import sanitize_filesystem_path
 
@@ -17,22 +18,47 @@ def _get_working_directory_parent_directory(level_of_directories_up: int = 1) ->
 
     return path_to_return
 
-@pytest.mark.parametrize("test_input,expected",[
-    (f"{WORKING_TEST_DIR}/src/aac/test.py",                     f"{WORKING_TEST_DIR}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/../src/aac/test.py",                  f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"),
-    ("src/aac/test.py",                                         f"{WORKING_TEST_DIR}/src/aac/test.py"),
-    ("../../src/aac/test.py",                                   f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/../../src/aac/test.py",               f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/src/aac/test.exe%00.py",              f"{WORKING_TEST_DIR}/src/aac/test.exe.py"),
-    (f"{WORKING_TEST_DIR}/%2e%2e%2f/src/aac/test.py",           f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/%2e%2e//src/aac/test.py",             f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/..%2f/src/aac/test.py",               f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/%2e%2e%5c/src/aac/test.py",           f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/%2e%2e\/src/aac/test.py",             f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/..%5c\/src/aac/test.py",              f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/%252e%252e%255c\/src/aac/test.py",    f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    (f"{WORKING_TEST_DIR}/..%255c\/src/aac/test.py",            f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
-    pytest.param(f"{WORKING_TEST_DIR}/src/aac/AAA.py",          f"{WORKING_TEST_DIR}/src/aac/BBB.py", marks=pytest.mark.xfail) #this test is expected to fail and will show as an 'xfailed'
-    ])
-def test_sequence(test_input, expected):
-    assert sanitize_filesystem_path(test_input) == expected
+class TestPaths(TestCase):
+    @parameterized.expand([
+            [f"{WORKING_TEST_DIR}/src/aac/test.py",                     f"{WORKING_TEST_DIR}/src/aac/test.py",],
+            [f"{WORKING_TEST_DIR}/../src/aac/test.py",                  f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"],
+            ["src/aac/test.py",                                         f"{WORKING_TEST_DIR}/src/aac/test.py"],
+            ["../../src/aac/test.py",                                   f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/../../src/aac/test.py",               f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/src/aac/test.exe%00.py",              f"{WORKING_TEST_DIR}/src/aac/test.exe.py"],
+            [f"{WORKING_TEST_DIR}/%2e%2e%2f/src/aac/test.py",           f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/%2e%2e//src/aac/test.py",             f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/..%2f/src/aac/test.py",               f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/%2e%2e%5c/src/aac/test.py",           f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/%2e%2e\/src/aac/test.py",             f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/..%5c\/src/aac/test.py",              f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/%252e%252e%255c\/src/aac/test.py",    f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            [f"{WORKING_TEST_DIR}/..%255c\/src/aac/test.py",            f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"],
+            # [f"{WORKING_TEST_DIR}/src/aac/AAA.py",                      f"{WORKING_TEST_DIR}/src/aac/BBB.py"],
+        ])
+    def test_sequence(self, test_path, expected_result):
+            # if test_path == f"{WORKING_TEST_DIR}/src/aac/AAA.py":
+            #     pytest.xfail("This test is expected to fail")
+            self.assertEqual(sanitize_filesystem_path(test_path),expected_result)
+
+# Works but pytest and unittest don't have == number of tests
+# @pytest.mark.parametrize("test_input,expected",[
+#     (f"{WORKING_TEST_DIR}/src/aac/test.py",                     f"{WORKING_TEST_DIR}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/../src/aac/test.py",                  f"{os.path.dirname(WORKING_TEST_DIR)}/src/aac/test.py"),
+#     ("src/aac/test.py",                                         f"{WORKING_TEST_DIR}/src/aac/test.py"),
+#     ("../../src/aac/test.py",                                   f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/../../src/aac/test.py",               f"{_get_working_directory_parent_directory(2)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/src/aac/test.exe%00.py",              f"{WORKING_TEST_DIR}/src/aac/test.exe.py"),
+#     (f"{WORKING_TEST_DIR}/%2e%2e%2f/src/aac/test.py",           f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/%2e%2e//src/aac/test.py",             f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/..%2f/src/aac/test.py",               f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/%2e%2e%5c/src/aac/test.py",           f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/%2e%2e\/src/aac/test.py",             f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/..%5c\/src/aac/test.py",              f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/%252e%252e%255c\/src/aac/test.py",    f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     (f"{WORKING_TEST_DIR}/..%255c\/src/aac/test.py",            f"{_get_working_directory_parent_directory(1)}/src/aac/test.py"),
+#     pytest.param(f"{WORKING_TEST_DIR}/src/aac/AAA.py",          f"{WORKING_TEST_DIR}/src/aac/BBB.py", marks=pytest.mark.xfail) #this test is expected to fail and will show as an 'xfailed'
+#     ])
+
+# def test_sanitize_filesystem_path(test_input, expected):
+#     assert sanitize_filesystem_path(test_input) == expected
