19a85db0ade10fc67d2d18939de87e027d171b90
jasoncbryant <jason.bryant@ngc.com>
2024-10-16 16:20:16 -0500

    Improve exclusive fields unit tests (#884)
    * Updated mutually exclusive fields unit tests
    Created feature check for plugins
    
    * Updated feature checks for plugins
    
    * Update exclusive_fields.feature
    
    * Update __init__.py
    
    * Update test_exclusive_fields.py
    
    Modifications for flake8 findings.
    
    * Update python/features/plugins/exclusive_fields.feature
    
    Co-authored-by: lizzcondrey <131189253+lizzcondrey@users.noreply.github.com>
    
    * Update python/src/aac/__init__.py
    
    Co-authored-by: lizzcondrey <131189253+lizzcondrey@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: lizzcondrey <131189253+lizzcondrey@users.noreply.github.com>

--- python/tests/test_aac/plugins/exclusive_fields/test_exclusive_fields.py
+++ python/tests/test_aac/plugins/exclusive_fields/test_exclusive_fields.py
@@ -1,39 +1,30 @@
 from unittest import TestCase
-from typing import Tuple
-from click.testing import CliRunner
-from aac.execute.command_line import cli, initialize_cli
-from aac.execute.aac_execution_result import ExecutionStatus
 from aac.context.language_context import LanguageContext
-from os import linesep
-
-
-from aac.plugins.exclusive_fields.exclusive_fields_impl import plugin_name
-
-
 from aac.plugins.exclusive_fields.exclusive_fields_impl import mutually_exclusive_fields
 
 
 class TestExclusiveFields(TestCase):
     def test_mutually_exclusive_fields(self):
-        
+
         context = LanguageContext()
         one_definition = context.parse_and_load(TEST_EXCLUSIVE_FIELDS)
 
         # good data tests
-        for good_def in [GOOD_DATA_1, GOOD_DATA_2, GOOD_DATA_3]:
+        for good_def in [GOOD_DATA_1, GOOD_DATA_2, GOOD_DATA_3, GOOD_DATA_4, GOOD_DATA_5, GOOD_DATA_6]:
             good = context.parse_and_load(good_def)
             result = mutually_exclusive_fields(good[0].instance, one_definition[0], None, ["alpha", "beta", "gamma"])
-            self.assertTrue(result.is_success(), f"Expected success for {good[0].name}")
+            self.assertTrue(result.is_success(), msg=f"Expected success for {good[0].name}")
             context.remove_definitions(good)
 
         # bad data tests
-        for bad_def in [BAD_DATA_1, BAD_DATA_2, BAD_DATA_3, BAD_DATA_4]:
+        for bad_def in [BAD_DATA_1, BAD_DATA_2, BAD_DATA_3, BAD_DATA_4, BAD_DATA_5, BAD_DATA_6, BAD_DATA_7, BAD_DATA_8]:
             bad = context.parse_and_load(bad_def)
             result = mutually_exclusive_fields(bad[0].instance, one_definition[0], None, ["alpha", "beta", "gamma"])
-            self.assertFalse(result.is_success(), f"Expected failure for {bad[0].name}")
+            self.assertFalse(result.is_success(), msg=f"Expected failure for {bad[0].name}")
             context.remove_definitions(bad)
 
         context.remove_definitions(one_definition)
+
 
 TEST_EXCLUSIVE_FIELDS = """
 schema:
@@ -56,8 +47,7 @@ schema:
             fields:
                 - alpha
                 - beta
-                - gamma  
-
+                - gamma
 """
 
 GOOD_DATA_1 = """
@@ -78,31 +68,79 @@ one:
     gamma: gamma
 """
 
+GOOD_DATA_4 = """
+one:
+    name: GoodFour
+    alpha: alpha
+    alpha: alpha  #  Name & field argument duplication is OK
+"""
+
+GOOD_DATA_5 = """
+one:
+    name: GoodFive
+    alpha: beta
+"""
+
+GOOD_DATA_6 = """
+one:
+    name: GoodSix
+    alpha: beta
+    alpha: gamma
+"""
+
 BAD_DATA_1 = """
 one:
-    name: bad
+    name: BadOne
     alpha: alpha
     beta: beta
 """
 
 BAD_DATA_2 = """
 one:
-    name: bad
+    name: BadTwo
     alpha: alpha
     gamma: gamma
 """
 
 BAD_DATA_3 = """
 one:
-    name: bad
+    name: BadThree
     beta: beta
     gamma: gamma
 """
 
 BAD_DATA_4 = """
 one:
-    name: bad
+    name: BadFour
     alpha: alpha
     beta: beta
     gamma: gamma
+"""
+
+BAD_DATA_5 = """
+one:
+    name: BadFive
+    alpha: alpha
+    alpha: #  This intentionally left as blank/whitespace
+"""
+
+BAD_DATA_6 = """
+one:
+    name: BadSix
+    beta: beta
+    alpha: alpha
+"""
+
+BAD_DATA_7 = """
+one:
+    name: BadSeven
+    alpha: #  This intentionally left as blank/whitespace
+    alpha: #  This intentionally left as blank/whitespace
+"""
+
+BAD_DATA_8 = """
+one:
+    name: BadEight
+    alpha: beta
+    beta: gamma
 """
