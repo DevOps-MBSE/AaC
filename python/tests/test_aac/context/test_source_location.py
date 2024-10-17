b97f11e398fa46afccf71fcff7373b809ac3f17b
william Bunch <william.bunch@ngc.com>
2024-10-17 20:05:07 +0000

    source_location tests fail

--- python/tests/test_aac/context/test_source_location.py
+++ python/tests/test_aac/context/test_source_location.py
@@ -5,5 +5,9 @@ from aac.context.source_location import SourceLocation
 class TestSourceLocation(TestCase):
     def test_source_location(self):
         loc = SourceLocation(1, 2, 3, 4)
-        
+
         self.assertTrue(isinstance(loc.to_tuple(), tuple))
+
+    def test_source_location_incorrect_type(self):
+        with self.assertRaises(TypeError):
+            loc = SourceLocation("1", "2", "3", "4")
