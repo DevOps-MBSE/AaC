2488b6b01ccd92dcde7993eb3c0d006eeea9c555
william Bunch <william.bunch@ngc.com>
2024-10-18 16:49:35 +0000

    source location expansion

--- python/tests/test_aac/context/test_source_location.py
+++ python/tests/test_aac/context/test_source_location.py
@@ -5,8 +5,14 @@ from aac.context.source_location import SourceLocation
 class TestSourceLocation(TestCase):
     def test_source_location(self):
         loc = SourceLocation(1, 2, 3, 4)
-
         self.assertIsInstance(loc.to_tuple(), tuple)
+
+        loc2 = SourceLocation(2, 1, 3, 4)
+        self.assertIsInstance(loc2.to_tuple(), tuple)
+
+        loc3 = SourceLocation(1, 2, 4, 3)
+        self.assertIsInstance(loc3.to_tuple(), tuple)
+        self.assertNotEqual(loc, loc2, loc3)
 
     def test_source_location_incorrect_type(self):
         with self.assertRaises(TypeError):
