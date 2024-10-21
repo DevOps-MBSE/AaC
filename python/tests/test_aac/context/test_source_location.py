a1ea25a10b4c8b6f29193b7403331aa4744d4be7
william Bunch <william.bunch@ngc.com>
2024-10-21 04:34:14 +0000

    location mapping test in source location test

--- python/tests/test_aac/context/test_source_location.py
+++ python/tests/test_aac/context/test_source_location.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from aac.context.source_location import SourceLocation
+from aac.context.language_context import LanguageContext
 
 
 class TestSourceLocation(TestCase):
@@ -14,6 +15,42 @@ class TestSourceLocation(TestCase):
         self.assertIsInstance(loc3.to_tuple(), tuple)
         self.assertNotEqual(loc, loc2, loc3)
 
+    def test_source_location_mapping(self):
+        context = LanguageContext()
+        definition = context.parse_and_load(VALID_AAC_YAML_CONTENT)[0]
+        lexemes = definition.lexemes
+
+        self.assertEqual(lexemes[0].value, "schema")
+        self.assertEqual(lexemes[0].location.to_tuple(), (0, 0, 0, 6))
+
+        self.assertEqual(lexemes[1].value, "name")
+        self.assertEqual(lexemes[1].location.to_tuple(), (1, 2, 10, 4))
+
+
     def test_source_location_incorrect_type(self):
         with self.assertRaises(TypeError):
             loc = SourceLocation("1", "2", "3", "4")  # noqa: F841
+
+VALID_AAC_YAML_CONTENT = """
+schema:
+  name: TestSchema
+  description: |
+    This is a test schema.
+  fields:
+    - name: string_field
+      type: string
+      description: |
+        This is a test field.
+    - name: integer_field
+      type: integer
+      description: |
+        This is a test field.
+    - name: boolean_field
+      type: boolean
+      description: |
+        This is a test field.
+    - name: number_field
+      type: number
+      description: |
+        This is a test field.
+""".strip()
