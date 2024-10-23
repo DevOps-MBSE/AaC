909c5b7974ca69c354e38c44c5add1ae0ce226fa
william Bunch <william.bunch@ngc.com>
2024-10-23 15:03:50 +0000

    added overwrite flag testing

--- python/tests/test_aac/plugins/gen_plugin/test_gen_plugin.py
+++ python/tests/test_aac/plugins/gen_plugin/test_gen_plugin.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from unittest.mock import patch
 from typing import Tuple
 from click.testing import CliRunner
-from os import path, mkdir
+from os import path, mkdir, listdir
 from shutil import copy
 from tempfile import TemporaryDirectory
 from traceback import print_exception
@@ -64,6 +64,45 @@ class TestGenPlugin(TestCase):
             self.assertTrue(path.exists(path.join(package_tests_path, "my_plugin_context_test.feature")))
             self.assertTrue(path.exists(path.join(package_tests_path, "my_plugin_schema_test.feature")))
             self.assertTrue(path.exists(path.join(package_tests_path, "my_plugin_primitive_test.feature")))
+
+    def test_cli_gen_plugin_overwrite(self):
+        # first we need a project to work in, so generate a temporary one
+        with TemporaryDirectory() as temp_dir:
+            # now create an AaC plugin file in the project src directory
+            aac_file_path = path.join(path.dirname(__file__), "my_project.aac")
+            temp_aac_file_path = path.join(temp_dir, "my_project.aac")
+            copy(aac_file_path, temp_aac_file_path)
+
+            proj_args = [temp_aac_file_path, "--output", temp_dir, "--no-prompt"]
+            exit_code, output_message = self.run_gen_project_cli_command_with_args(proj_args)
+
+            package_src_path = path.join(temp_dir, "src", "happy")
+            mkdir(package_src_path)
+            package_tests_path = path.join(temp_dir, "tests", "test_happy")
+            mkdir(package_tests_path)
+            plugin_file_path = path.join(package_src_path, "my_plugin.aac")
+
+            aac_plugin_path = path.join(path.dirname(__file__), "my_plugin.aac")
+            copy(aac_plugin_path, plugin_file_path)
+            plugin_args = [plugin_file_path, "--code-output", path.join(temp_dir, "src"), "--test-output", path.join(temp_dir, "tests"), "--no-prompt"]
+            exit_code, output_message = self.run_gen_plugin_cli_command_with_args(plugin_args)
+            file = open(path.join(package_src_path, "my_plugin_impl.py"), "r")
+            file_read = file.read()
+            self.assertIn("test_command_one(aac_plugin_file: str)", file_read)
+            self.assertNotIn("test_command_one_evaluation(aac_plugin_file: str)", file_read)
+            file.close()
+
+            aac_plugin_path = path.join(path.dirname(__file__), "my_plugin_eval.aac")
+            copy(aac_plugin_path, plugin_file_path)
+            plugin_args = [plugin_file_path, "--code-output", path.join(temp_dir, "src"), "--test-output", path.join(temp_dir, "tests"), "--no-prompt", "--force-overwrite"]
+            exit_code, output_message = self.run_gen_plugin_cli_command_with_args(plugin_args)
+            file = open(path.join(package_src_path, "my_plugin_impl.py"), "r")
+            file_read = file.read()
+            self.assertIn("test_command_1_evaluation(aac_plugin_file: str)", file_read)
+            self.assertNotIn("test_command_one(aac_plugin_file: str)", file_read)
+            file.close()
+
+
 
     def test_cli_gen_plugin_with_incomplete_dirs(self):
         # first we need a project to work in, so generate a temporary one
