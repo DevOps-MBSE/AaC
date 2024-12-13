2faa5a09d8cac851444443e4371208002eed67ec
william Bunch <william.bunch@ngc.com>
2024-12-13 07:21:16 +0000

    modified language error and added language error test

--- python/src/aac/context/language_error.py
+++ python/src/aac/context/language_error.py
@@ -8,3 +8,10 @@ class LanguageError(Exception):
     """A base class representing a language error condition."""
     message: str = attrib(validator=validators.instance_of(str))
     location: Optional[str] = attrib(validator=validators.optional(validators.instance_of(str)))
+
+    def __init__(self, message, location):
+        self.message = message
+        self.location = location
+
+    def __str__(self):
+        return (f"Error: {self.message}. Location: {self.location}")
