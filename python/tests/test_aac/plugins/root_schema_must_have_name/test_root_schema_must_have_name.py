b2e2edd657c60c80b7ad204f157d4a599802cca5
jason bryant <jason.bryant@ngc.com>
2025-01-13 18:07:47 +0000

    Updated and cleaned up Prepping for test read from file

--- python/tests/test_aac/plugins/root_schema_must_have_name/test_root_schema_must_have_name.py
+++ python/tests/test_aac/plugins/root_schema_must_have_name/test_root_schema_must_have_name.py
@@ -21,33 +21,18 @@ from aac.plugins.root_schema_must_have_name.root_schema_must_have_name_impl impo
 
 class TestRootSchemaMustHaveName(TestCase):
 
-    # def test_root_schema_has_name(self):
-    #     print("")
-    #     context = LanguageContext()
-    #     definitions = context.parse_and_load(root_schema_with_name)
-    #     result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
-    #     self.assertTrue(result.is_success())
-    #     context.remove_definitions(definitions)
+    def test_root_schema_has_name(self):
+        context = LanguageContext()
+        definitions = context.parse_and_load(root_schema_with_name)
+        result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
+        self.assertTrue(result.is_success())
+        context.remove_definitions(definitions)
 
-    # def _get_test_fields(schema) -> list[str]:
-    #     """Returns a list of the fields in the parent schema."""
-    #     print("IN _get_test_fields IN")
-    #     context: LanguageContext = LanguageContext()
-    #     fields: list[str] = []
-    #     for field in schema.fields:
-    #         if field.name != 'name':
-    #             fields.append(field.name)
-    #         else:
-    #             print("Excluding name")
-    #     if schema.extends:
-    #         for ext in schema.extends:
-    #             parent_schema = context.get_definitions_by_name(ext.name)
-    #             if len(parent_schema) == 1:
-    #                 fields.extend(_get_fields(parent_schema[0].instance))
-    #     return fields
 
+    #  This test starts with a normal properly formed schema
+    #  It then takes the result from parse_and_load and specifically removes the 'name'
+    #  It then calls root_schema_has_name which will not find the name it is looking for
     def test_root_schema_has_name_fail(self):
-        print("\n\n***\n")
         context = LanguageContext()
         try:
             # Obtain the definitions for a properly formed schema
@@ -60,11 +45,13 @@ class TestRootSchemaMustHaveName(TestCase):
                 instance_fields = list[str]
                 context: LanguageContext = LanguageContext()
                 instance_fields: list[str] = []
+
                 for field in schema.fields:
                     if field.name != 'name':
+                        # Copy the field over if the name is 'name'
+                        # skip if it is 'name' to trigger the check in root_schema_has_name
                         instance_fields.append(field)
-                    # else:
-                        # Excluding name
+
                 if schema.extends:
                     for ext in schema.extends:
                         parent_schema = context.get_definitions_by_name(ext.name)
@@ -76,7 +63,7 @@ class TestRootSchemaMustHaveName(TestCase):
                 traceback.print_exc()
 
             result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
-            index = result.get_messages_as_string().find("must have a field named 'name")
+            index = result.get_messages_as_string().find("must have a field named 'name'") # this is the message from line 61 of root_schema_has_name
             if index != -1:
                 # Name message located
                 self.assertFalse(result.is_success())
@@ -93,99 +80,78 @@ class TestRootSchemaMustHaveName(TestCase):
         except:
             self.assertFalse(True)
 
-    # def test_root_schema_has_name_language_error1(self):
 
-    #     context = LanguageContext()
-    #     result = None
-    #     try:
-    #         definitions = context.parse_and_load(root_schema_without_name_language_error1)
-    #         result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
-    #         self.assertFalse(result.is_success())
-    #         context.remove_definitions(definitions)
-    #         print("Completed try block")
-    #     except ParserError as pe:
-    #         print("ParserError1!")
-    #         print(pe)
-    #         self.assertFalse(True)
-    #     except LanguageError as le:
-    #         print("LanguageError1!")
-    #         #print(le)
-    #         # #printing stack trace
-    #         #traceback.print_exc() # File "/workspace/AaC/python/src/aac/context/definition_parser.py", line 601, in primitive_field_value_check
-    #         self.assertFalse(False)
-    #     except:
-    #         print("Other unplanned error/exception1")
-    #         traceback.print_exc()
-    #         self.assertFalse(True)
+    #  This test should trigger a LanguageError exception from the parse_and_load call
+    #  So the only pass here is the self.assertFalse(False) in the LanguageError catch block
+    def test_root_schema_has_name_language_error1(self):
 
-    # def test_root_schema_has_name_language_error2(self):
-    #     context = LanguageContext()
-    #     result = None
-    #     try:
-    #         definitions = context.parse_and_load(root_schema_without_name_language_error2)
-    #         result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
-    #         self.assertFalse(result.is_success())
-    #         context.remove_definitions(definitions)
-    #         print("Completed try block")
-    #     except ParserError as pe:
-    #         print("ParserError2!")
-    #         print(pe)
-    #         self.assertFalse(True)
-    #     except LanguageError as le:
-    #         print("LanguageError2!")
-    #         # print(le)
-    #         # #printing stack trace
-    #         # traceback.print_exc() # File "/workspace/AaC/python/src/aac/context/definition_parser.py", line 554, in field_instance_check
-    #         self.assertFalse(False)
-    #     except:
-    #         print("Other unplanned error/exception2")
-    #         self.assertFalse(True)
+        context = LanguageContext()
+        result = None
+        try:
+            definitions = context.parse_and_load(root_schema_without_name_language_error1)
+            result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
+            self.assertFalse(True)
+            context.remove_definitions(definitions)
+        except ParserError as pe:
+            self.assertFalse(True)
+        except LanguageError as le:
+            self.assertFalse(False)
+        except:
+            self.assertFalse(True)
 
-    # def test_root_schema_has_name_parser_error3(self):
-    #     context = LanguageContext()
-    #     result = None
-    #     try:
-    #         definitions = context.parse_and_load(root_schema_without_name_parser_error3)
-    #         result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
-    #         self.assertFalse(result.is_success())
-    #         context.remove_definitions(definitions)
-    #         print("Completed try block")
-    #     except ParserError as pe:
-    #         print("ParserError3!")
-    #         # print(pe)
-    #         # printing stack trace
-    #         # traceback.print_exc() # File "/workspace/AaC/python/src/aac/in_out/parser/_yaml.py", line 103, in assert_definition_has_name
-    #         self.assertFalse(False)
-    #     except LanguageError as le:
-    #         print("LanguageError3!")
-    #         print(le)
-    #         self.assertFalse(True)
-    #     except:
-    #         print("Other unplanned error/exception3")
-    #         self.assertFalse(True)
 
-    # def test_root_schema_has_name_parser_error4(self):
-    #     context = LanguageContext()
-    #     result = None
-    #     try:
-    #         definitions = context.parse_and_load(root_schema_without_name_parser_error4)
-    #         result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
-    #         self.assertFalse(result.is_success())
-    #         context.remove_definitions(definitions)
-    #         print("Completed try block")
-    #     except ParserError as pe:
-    #         print("ParserError4!")
-    #         # print(pe)
-    #         # printing stack trace
-    #         # traceback.print_exc() # File "/workspace/AaC/python/src/aac/in_out/parser/_yaml.py", line 103, in assert_definition_has_name
-    #         self.assertFalse(False)
-    #     except LanguageError as le:
-    #         print("LanguageError4!")
-    #         print(le)
-    #         self.assertFalse(True)
-    #     except:
-    #         print("Other unplanned error/exception4")
-    #         self.assertFalse(True)
+    #  This test should trigger a LanguageError exception from the parse_and_load call
+    #  So the only pass here is the self.assertFalse(False) in the LanguageError catch block
+    def test_root_schema_has_name_language_error2(self):
+        context = LanguageContext()
+        result = None
+        try:
+            definitions = context.parse_and_load(root_schema_without_name_language_error2)
+            result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
+            self.assertFalse(True)
+            context.remove_definitions(definitions)
+        except ParserError as pe:
+            self.assertFalse(True)
+        except LanguageError as le:
+            self.assertFalse(False)
+        except:
+            self.assertFalse(True)
+
+
+    #  This test should trigger a ParserError exception from the parse_and_load call
+    #  So the only pass here is the self.assertFalse(False) in the ParserError catch block
+    def test_root_schema_has_name_parser_error3(self):
+        context = LanguageContext()
+        result = None
+        try:
+            definitions = context.parse_and_load(root_schema_without_name_parser_error3)
+            result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
+            self.assertFalse(True)
+            context.remove_definitions(definitions)
+        except ParserError as pe:
+            self.assertFalse(False)
+        except LanguageError as le:
+            self.assertFalse(True)
+        except:
+            self.assertFalse(True)
+
+
+    #  This test should trigger a ParserError exception from the parse_and_load call
+    #  So the only pass here is the self.assertFalse(False) in the ParserError catch block
+    def test_root_schema_has_name_parser_error4(self):
+        context = LanguageContext()
+        result = None
+        try:
+            definitions = context.parse_and_load(root_schema_without_name_parser_error4)
+            result = root_schema_has_name(definitions[0].instance, definitions[0], context.get_definitions_by_name("Schema")[0].instance)
+            self.assertFalse(True)
+            context.remove_definitions(definitions)
+        except ParserError as pe:
+            self.assertFalse(False)
+        except LanguageError as le:
+            self.assertFalse(True)
+        except:
+            self.assertFalse(True)
 
 root_schema_with_name = """
 schema:
